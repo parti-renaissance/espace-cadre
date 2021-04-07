cb57273a0864729f55edd231a7930e3c858db576
XavierLacEM <80252457+XavierLacEM@users.noreply.github.com>
2021-04-07 10:46:56 +0200

    Display dynamically columns titles (#3)
    * Display dynamically columns titles
    
    * Refacto table datas
    
    Co-authored-by: Mickael Labarrere <35226465+mlabarrere@users.noreply.github.com>

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -6,20 +6,27 @@ import ColumnFilter from './ColumnFilter';
 
 export const Contacts = () => {
     const [data, setData] = useState([]);
+    const [columnsTitle, setColumnsTitle] = useState([]);
 
-    // Get the contacts data
+    // Get the data for the table
     useEffect(() => {
-        const getContacts = async () => {
+        const getContactsAndColumnsTitles = async () => {
             const response = await fetch("https://python-api-cloudrun-gcr-staging-x44qrxc7fq-ew.a.run.app/contacts/")
-            const body = await response.json()
-            const contacts = body;
-            setData(contacts)
-        }
-        getContacts()
-    }, []);
+            const body = await response.json();
 
-    // Set the columns headers
-    const columns = useMemo(() => COLUMNS, []);
+            const columnsTitle = (Object.keys(body[0]));
+            const columns = columnsTitle.map((title, i) => {
+                const cleanTitle = title.replace('_', ' ');
+                return {
+                    Header: cleanTitle,
+                    accessor: title
+                }
+            });
+            setColumnsTitle(columns)
+            setData(body)
+        }
+        getContactsAndColumnsTitles()
+    }, []);
 
     // Set the search input to every column
     const defaultColumn = useMemo(() => {
@@ -31,7 +38,7 @@ export const Contacts = () => {
     return (
         <div>
             <TableContainer
-                columns={columns}
+                columns={columnsTitle}
                 data={data}
                 defaultColumn={defaultColumn}
             />
