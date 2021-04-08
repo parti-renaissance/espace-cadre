1c2c0d8b1f7820c01be68858bacd09df53d268b2
noswoscar <32093588+noswoscar@users.noreply.github.com>
2021-04-08 14:17:42 +0100

    blocks de base

---
+++ src/components/Mail/Template/grapesjs/Config.jsx
@@ -0,0 +1,133 @@
+const swv = 'sw-visibility';
+const ful = 'fullscreen';
+const prv = 'preview';
+const obl = 'open-blocks';
+
+const setBlocks = (blockManager) => {
+    blockManager.add("section-block", {
+        id: 'Section',
+        label: '<b>Section</b>',
+        attributes: { class: "gjs-fonts gjs-f-b1 gjs-block gjs-one-bg gjs-four-color-h" },
+        content: `<section><h1>Bonjour,</h1>
+            <div>Ceci est un exemple de Mail</div></section>`,
+    });
+    blockManager.add("text-block", {
+        id: 'text',
+        label: 'Text',
+        attributes: { class: "gjs-fonts gjs-f-text" },
+        content: '<div data-gjs-type="text">Exemple de texte</div>',
+    });
+    blockManager.add("image-block", {
+        id: 'image',
+        label: 'Image',
+        attributes: { class: "gjs-fonts gjs-f-image" },
+        select: true,
+        content: { type: 'image' },
+        activate: true,
+    });
+    blockManager.add("link-block", {
+        id: 'link',
+        label: 'Link',
+        attributes: { class: "fa fa-link gjs-block gjs-one-bg gjs-four-color-h" },
+        select: true,
+        content: {
+          type: 'link',
+          content: 'Link',
+          style: { color: '#d983a6' }
+        },
+        activate: true,
+    });
+    blockManager.add("1/2 section", {
+        id: '1/2 section',
+        label: '1/2 Section',
+        attributes: { class: "gjs-fonts gjs-f-b2 gjs-block gjs-one-bg gjs-four-color-h" },
+        select: true,
+        content: `<div style="display: flex; justify-content: space-between"><section><h1>Section 1 titre</h1>
+        <div>Section1 texte</div></section><section><h1>Section 2 titre</h1>
+        <div>Section2 texte</div></section></div>`,
+        activate: true
+    });
+    blockManager.add("Divider", {
+      id: 'divider',
+      label: 'divider',
+      attributes: { class: "gjs-fonts gjs-f-divider gjs-block gjs-one-bg gjs-four-color-h" },
+      select: true,
+      content: '<hr>',
+      activate: true
+  });
+}
+
+const setConfig = (editor) => {
+    editor.I18n.setLocale("fr");
+        editor.Panels.addPanel({
+            id: 'commands',
+            buttons: [
+            
+            ],
+        });
+        editor.Panels.addPanel(
+        {
+            id: 'options',
+            buttons: [
+              {
+                active: true,
+                id: swv,
+                className: 'fa fa-square-o',
+                command: swv,
+                context: swv,
+                attributes: { title: 'View components' }
+              },
+              {
+                id: prv,
+                className: 'fa fa-eye',
+                command: prv,
+                context: prv,
+                attributes: { title: 'Preview' }
+              },
+              {
+                id: ful,
+                className: 'fa fa-arrows-alt',
+                command: ful,
+                context: ful,
+                attributes: { title: 'Fullscreen' }
+              },
+              {
+                id: 'show-json',
+                className: 'btn-show-json',
+                label: 'JSON',
+                context: 'show-json',
+                command(editor) {
+                  editor.Modal.setTitle('Components JSON')
+                    .setContent(`<textarea style="width:100%; height: 250px;">
+                      ${JSON.stringify(editor.getComponents())}
+                    </textarea>`)
+                    .open();
+                }
+              }
+            ]
+          });
+          editor.Panels.addPanel({
+            id: 'views',
+            buttons: [
+              {
+                id: obl,
+                className: 'fa fa-th-large',
+                command: obl,
+                togglable: 0,
+                attributes: { title: 'Open Blocks' }
+              }
+            ]
+          });
+
+        editor.getConfig().showDevices = 0;
+        editor.Panels.addPanel({ id: "devices-c" }).get("buttons").add([
+            { id: "set-device-desktop", command: function(e) { return e.setDevice("Desktop") }, className: "fa fa-desktop", active: 1},
+            { id: "set-device-tablet", command: function(e) { return e.setDevice("Tablet") }, className: "fa fa-tablet" },
+            { id: "set-device-mobile", command: function(e) { return e.setDevice("Mobile portrait") }, className: "fa fa-mobile" },
+            ]);
+        editor.Panels.render();
+        setBlocks(editor.BlockManager);
+        editor.Panels.getButton('views', 'open-blocks').set('active', true);
+};
+
+export default setConfig;
--- src/components/Mail/Template/grapesjs/Editor.jsx
+++ src/components/Mail/Template/grapesjs/Editor.jsx
@@ -1,11 +1,8 @@
 import 'grapesjs/dist/css/grapes.min.css';
 import grapesjs from 'grapesjs';
-import "./Editor.scss";
 import { useEffect } from 'react';
-const swv = 'sw-visibility';
-const ful = 'fullscreen';
-const prv = 'preview';
-const obl = 'open-blocks';
+import setConfig from './Config';
+import "./Editor.scss";
 
 const Editor = () => {
     useEffect(() => {
@@ -21,127 +18,7 @@ const Editor = () => {
                 defaults: []
             }
         });
-        editor.I18n.setLocale("fr");
-        editor.Panels.addPanel({
-            id: 'commands',
-            buttons: [
-             {
-                id: 'deviceDesktop',
-                command: 'set-device-desktop',
-                className: 'fa fa-desktop',
-                attributes: {title: 'Desktop'},
-                active: 1,
-            }, {
-                id: 'deviceTablet',
-                command: 'set-device-tablet',
-                className: 'fa fa-tablet',
-                attributes: {title:'Tablet'},
-            }, {
-                id: 'deviceMobile',
-                command: 'set-device-mobile',
-                className: 'fa fa-mobile',
-                attributes: {title: 'Mobile'},
-            }
-            ],
-        });
-        editor.Panels.addPanel(
-        {
-            id: 'options',
-            buttons: [
-              {
-                active: true,
-                id: swv,
-                className: 'fa fa-square-o',
-                command: swv,
-                context: swv,
-                attributes: { title: 'View components' }
-              },
-              {
-                id: prv,
-                className: 'fa fa-eye',
-                command: prv,
-                context: prv,
-                attributes: { title: 'Preview' }
-              },
-              {
-                id: ful,
-                className: 'fa fa-arrows-alt',
-                command: ful,
-                context: ful,
-                attributes: { title: 'Fullscreen' }
-              },
-              {
-                id: 'show-json',
-                className: 'btn-show-json',
-                label: 'JSON',
-                context: 'show-json',
-                command(editor) {
-                  editor.Modal.setTitle('Components JSON')
-                    .setContent(`<textarea style="width:100%; height: 250px;">
-                      ${JSON.stringify(editor.getComponents())}
-                    </textarea>`)
-                    .open();
-                }
-              }
-            ]
-          });
-          editor.Panels.addPanel({
-            id: 'views',
-            buttons: [
-              {
-                id: obl,
-                className: 'fa fa-th-large',
-                command: obl,
-                togglable: 0,
-                attributes: { title: 'Open Blocks' }
-              }
-            ]
-          });
-
-        editor.getConfig().showDevices = 0;
-        editor.Panels.addPanel({ id: "devices-c" }).get("buttons").add([
-            { id: "set-device-desktop", command: function(e) { return e.setDevice("Desktop") }, className: "fa fa-desktop", active: 1},
-            { id: "set-device-tablet", command: function(e) { return e.setDevice("Tablet") }, className: "fa fa-tablet" },
-            { id: "set-device-mobile", command: function(e) { return e.setDevice("Mobile portrait") }, className: "fa fa-mobile" },
-            ]);
-        editor.Panels.render();
-        editor.BlockManager.add("section-block", {
-            id: 'Section',
-            label: '<b>Section</b>',
-            attributes: { class: "gjs-fonts gjs-f-b1 gjs-block gjs-one-bg gjs-four-color-h" },
-            content: `<section><h1>Bonjour,</h1>
-                <div>Ceci est un exemple de Mail</div></section>`,
-        });
-        editor.BlockManager.add("text-block", {
-            id: 'text',
-            label: 'Text',
-            attributes: { class: "gjs-fonts gjs-f-text" },
-            content: '<div data-gjs-type="text">Exemple de texte</div>',
-        });
-        editor.BlockManager.add("image-block", {
-            id: 'image',
-            label: 'Image',
-            attributes: { class: "gjs-fonts gjs-f-image" },
-            select: true,
-            content: { type: 'image' },
-            activate: true,
-        });
-        editor.BlockManager.add("link-block", {
-            id: 'link',
-            label: 'Link',
-            attributes: { class: "fa fa-link gjs-block gjs-one-bg gjs-four-color-h" },
-            select: true,
-            content: { type: 'link' },
-            activate: true,
-        });
-        editor.BlockManager.add("1/2 section", {
-            id: '1/2 section',
-            label: '1/2 Section',
-            attributes: { class: "gjs-fonts gjs-f-b2 gjs-block gjs-one-bg gjs-four-color-h" },
-            select: true,
-            content: { type: '1/2 section' },
-        });
-        editor.Panels.getButton('views', 'open-blocks').set('active', true);
+        setConfig(editor);
     }, []);
 
     return (
@@ -150,7 +27,7 @@ const Editor = () => {
             <div className="panel__basic-actions"></div>
         </div>
         <div id="gjs" className="editor">
-              Exemple de mail
+            
         </div>
     </>
     )
