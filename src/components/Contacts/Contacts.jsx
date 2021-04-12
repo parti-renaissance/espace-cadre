c0aa205b4b1e8c902e0bec27cb02c8f490367bca
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-04-12 09:51:20 +0200

    Spinner for Contacts page

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -7,13 +7,18 @@ import ColumnFilter from './ColumnFilter';
 export const Contacts = () => {
     const [data, setData] = useState([]);
     const [columnsTitle, setColumnsTitle] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     // Get the data for the table
     useEffect(() => {
         const getContactsAndColumnsTitles = async () => {
+            setLoading(true);
             const response = await fetch("https://middleware-api-x44qrxc7fq-ew.a.run.app/contacts/");
+            if (!response.ok) {
+                const message = `An error has occured: ${response.status}`;
+                throw new Error(message);
+            }
             const body = await response.json();
-
             const columnsTitle = (Object.keys(body[0]));
             const columns = columnsTitle.map((title, i) => {
                 const cleanTitle = title.replace('_', ' ');
@@ -24,6 +29,7 @@ export const Contacts = () => {
             });
             setColumnsTitle(columns)
             setData(body)
+            setLoading(false);
         }
         getContactsAndColumnsTitles()
     }, []);
@@ -37,11 +43,23 @@ export const Contacts = () => {
 
     return (
         <div>
-            <TableContainer
-                columns={columnsTitle}
-                data={data}
-                defaultColumn={defaultColumn}
-            />
+            {loading ?
+                <div className="d-flex flex-column align-items-center justify-content-center" style={{marginTop:"calc(100vh - 70vh)"}}>
+                    <div className="row mb-2">
+                        <div className="spinner-border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <strong>Page en cours de chargement</strong>
+                    </div>
+                </div>
+                :
+                <TableContainer
+                    columns={columnsTitle}
+                    data={data}
+                    defaultColumn={defaultColumn}
+                />}
         </div>
     )
 }
