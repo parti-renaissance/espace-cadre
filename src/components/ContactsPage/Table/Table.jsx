7401af47ef7fa4d767323f0b9d6f039adb4dd6eb
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-04-19 16:37:11 +0200

    Export all contacts or filtered contacts

--- src/components/ContactsPage/Table/Table.jsx
+++ src/components/ContactsPage/Table/Table.jsx
@@ -8,13 +8,36 @@ import {
     useFilters,
     usePagination,
 } from 'react-table';
-import ExportCSV from '../ExportToExcel/ExportCSV';
+import { useExportData } from 'react-table-plugins';
+import * as XLSX from 'xlsx';
 import GlobalFilter from '../Filters/GlobalFilter';
 
 import './Table.scss';
 
 const Table = ({ columns, data, defaultColumn }) => {
-    const fileName = 'Contacts';
+    function getExportFileBlob({
+        // eslint-disable-next-line no-shadow
+        columns, data, fileName,
+    }) {
+        const header = columns.map((c) => c.exportValue);
+        const compatibleData = data.map((row) => {
+            const obj = {};
+            header.forEach((col, index) => {
+                obj[col] = row[index];
+            });
+            return obj;
+        });
+
+        const wb = XLSX.utils.book_new();
+        const ws1 = XLSX.utils.json_to_sheet(compatibleData, {
+            header,
+        });
+        XLSX.utils.book_append_sheet(wb, ws1, 'React Table Data');
+        XLSX.writeFile(wb, `${fileName}.xlsx`);
+
+        // Returning false as downloading of file is already taken care of
+        return false;
+    }
     const {
         getTableProps,
         getTableBodyProps,
@@ -31,24 +54,33 @@ const Table = ({ columns, data, defaultColumn }) => {
         prepareRow,
         state,
         setGlobalFilter,
+        exportData,
     } = useTable({
         columns,
         data,
         defaultColumn,
         initialState: { pageSize: 40 },
+        getExportFileBlob,
     },
     useGlobalFilter,
     useFilters,
-    usePagination);
+    usePagination,
+    useExportData);
     const { globalFilter, pageIndex, pageSize } = state;
+
     return (
         <>
             <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
             <div className="d-flex paginationTop">
-                <ExportCSV
-                    csvData={data}
-                    fileName={fileName}
-                />
+                <button
+                    type="button"
+                    onClick={() => {
+                        exportData('xlsx', false);
+                    }}
+                    className="btn btn-outline-info btn-sm mx-1"
+                >
+                    Export as XLSX
+                </button>
                 {' '}
                 <span style={{ borderLeft: '1px solid lightgrey', height: '2rem' }} />
                 {' '}
