9fcbac2adb1d5c72d4b3d65d5c81d4a28ef1d721
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-04-29 17:49:00 +0200

    LineChart reusable component

--- src/components/DashboardComponent/Dashboard.jsx
+++ src/components/DashboardComponent/Dashboard.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import LineChartComponent from '../DataVisualization/LineChartComponent';
+import data from './data';
 
-const Home = () => (
-    <div>
-        <p>Home</p>
+const Dashboard = () => (
+    <div className="container dashboardContainer">
+        <div className="row dashboardRow">
+            <div className="col">
+                <LineChartComponent data={data} title="Evolution du nombre d'utilisateurs actifs" />
+            </div>
+        </div>
+        <div className="row dashboardRow">
+            <div className="col">
+                <LineChartComponent data={data} title="Evolution du nombre d'adhérents" />
+            </div>
+        </div>
+        <div className="row dashboardRow">
+            <div className="col">
+                <LineChartComponent data={data} title="Evolution du nombre de téléchargements" />
+            </div>
+        </div>
     </div>
+
 );
 
-export default Home;
+export default Dashboard;
---
+++ src/components/DashboardComponent/data.js
@@ -0,0 +1,20 @@
+const data = [
+    {
+        jour: '25/04/2021',
+        adherents: 120,
+    },
+    {
+        jour: '26/04/2021',
+        adherents: 320,
+    },
+    {
+        jour: '27/04/2021',
+        adherents: 378,
+    },
+    {
+        jour: '28/04/2021',
+        adherents: 931,
+    },
+];
+
+export default data;
