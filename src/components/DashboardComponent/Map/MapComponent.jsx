6c5c18686d24945bbb5c99dad2be90404f16f119
XavierLac <80252457+XavierLac@users.noreply.github.com>
2021-05-10 11:05:40 +0200

    Init geo data (#119)

---
+++ src/components/DashboardComponent/Map/MapComponent.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    MapContainer, TileLayer, Marker,
+} from 'react-leaflet';
+import PropTypes from 'prop-types';
+
+function MapComponent({ mapData }) {
+    return (
+        <MapContainer
+            center={[48.893051, 2.350518]}
+            zoom={16}
+        >
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+            />
+
+            {mapData.map((data) => (
+                <Marker key={data.id} position={[data.latitude, data.longitude]} />
+            ))}
+        </MapContainer>
+    );
+}
+
+export default MapComponent;
+
+MapComponent.propTypes = {
+    mapData: PropTypes.arrayOf(Object).isRequired,
+};
