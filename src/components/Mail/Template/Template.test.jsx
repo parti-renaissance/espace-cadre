930344ca10c2e50763f54131a1adfb37fc4bc4b1
XavierLac <80252457+XavierLac@users.noreply.github.com>
2021-05-18 10:31:40 +0200

    EM-875 Introduce tests (#138)
    * Refacto to textChart
    
    * Bundle size analyzer
    
    * Update eslintrc file
    
    * Charts tests
    
    * Tests for messagerie page
    
    * -
    
    * Remove __test__ folder
    
    Co-authored-by: Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>

---
+++ src/components/Mail/Template/Template.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+
+import Template from './Template';
+
+describe('Behaviour for template block', () => {
+    test('When the user arrives on the page, save and send buttons are disabled', () => {
+        render(
+            <Provider store={configureStore()({
+                template: {
+                    content: null,
+                },
+            })}
+            >
+                <Template />
+            </Provider>,
+        );
+
+        const saveButton = screen.getByRole('button', { name: 'Sauvegarder' });
+        const sendButton = screen.getByRole('button', { name: 'Envoyer e-mail' });
+
+        expect(saveButton).toHaveClass('disabled');
+        expect(sendButton).toHaveClass('disabled');
+    });
+});
