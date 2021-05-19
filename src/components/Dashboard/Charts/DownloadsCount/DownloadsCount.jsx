ecdbc28766e689b685c93115b7875f085610e99e
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-05-19 11:41:13 +0200

    download-card redesigned

--- src/components/Dashboard/Charts/DownloadsCount/DownloadsCount.jsx
+++ src/components/Dashboard/Charts/DownloadsCount/DownloadsCount.jsx
@@ -7,81 +7,82 @@ import {
     XAxis,
     YAxis,
     Tooltip,
-    Legend,
 } from 'recharts';
 import PropTypes from 'prop-types';
 
-function DownloadsCount({ title, data }) {
+function DownloadsCount({ data }) {
+    const count = data[data.length - 1].cumsum;
     return (
-        <div className="area-chart-container with-background">
-            <div className="title">{title}</div>
-            <ResponsiveContainer width="100%" height={250}>
-                <AreaChart
-                    data={data}
-                    margin={{
-                        top: 5, right: 20, bottom: 5, left: 0,
-                    }}
-                >
-                    <defs>
-                        <linearGradient id="colorQuotidien" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-                        </linearGradient>
-                        <linearGradient id="colorCumul" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-                        </linearGradient>
-                    </defs>
-                    <XAxis
-                        dataKey="date"
-                        angle={-14}
-                        tickMargin={8}
-                        interval={4}
-                    />
-                    <YAxis
-                        axisLine={false}
-                        tickLine={false}
-                    />
-                    <CartesianGrid strokeDasharray=".08" />
-                    <Tooltip
-                        contentStyle={{
-                            background: '#fff',
-                            borderColor: '#F0F1F3',
-                            borderRadius: '16px',
+        <div className="with-background">
+            <div className="row p-3">
+                <span className="count-bubble">{count}</span>
+                <div className="col pl-0">
+                    <div className="chart-title">Téléchargements</div>
+                    <div className="chart-subtitle">De l&apos;application Je m&apos;engage sur les stores Android et Apple</div>
+                </div>
+            </div>
+            <div className="row">
+                <ResponsiveContainer width="100%" height={250}>
+                    <AreaChart
+                        data={data}
+                        margin={{
+                            top: 5, right: 20, bottom: 5, left: 0,
                         }}
-                        labelStyle={{
-                            color: '#1A334D',
-                            fontFamily: 'Poppins',
-                            fontWeight: 'bold',
-                            padding: '2px 5px',
-                            textAlign: 'left',
-                        }}
-                        itemStyle={{
-                            fontFamily: 'Poppins',
-                            color: '#0049C6',
-                            padding: '2px 5px',
-                            textAlign: 'left',
-                        }}
-                        cursor={{
-                            stroke: '#0049C6',
-                            strokeWidth: 0.5,
-                        }}
-                    />
-                    <Legend
-                        verticalAlign="bottom"
-                        height={36}
-                        layout="horizontal"
-                    />
-                    <Area
-                        name="Téléchargements par jour"
-                        type="monotone"
-                        dataKey="unique_user"
-                        stroke="#8884d8"
-                        fillOpacity={1}
-                        fill="url(#colorQuotidien)"
-                    />
-                </AreaChart>
-            </ResponsiveContainer>
+                    >
+                        <defs>
+                            <linearGradient id="colorQuotidien" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#0049C6" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#0049C6" stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
+                        <XAxis
+                            dataKey="date"
+                            angle={-14}
+                            tickMargin={8}
+                            interval={4}
+                        />
+                        <YAxis
+                            axisLine={false}
+                            tickLine={false}
+                        />
+                        <CartesianGrid strokeDasharray=".08" />
+                        <Tooltip
+                            contentStyle={{
+                                background: '#fff',
+                                borderColor: '#F0F1F3',
+                                borderRadius: '16px',
+                            }}
+                            labelStyle={{
+                                color: '#1A334D',
+                                fontFamily: 'Poppins',
+                                fontWeight: 'bold',
+                                padding: '2px 5px',
+                                textAlign: 'left',
+                            }}
+                            itemStyle={{
+                                fontFamily: 'Poppins',
+                                color: '#0049C6',
+                                padding: '2px 5px',
+                                textAlign: 'left',
+                            }}
+                            cursor={{
+                                stroke: '#0049C6',
+                                strokeWidth: 0.5,
+                            }}
+                        />
+                        <Area
+                            type="monotone"
+                            dataKey="unique_user"
+                            stroke="#0049C6"
+                            fillOpacity={1}
+                            fill="url(#colorQuotidien)"
+                        />
+                    </AreaChart>
+                </ResponsiveContainer>
+            </div>
+            <div className="row">
+                <li className="legend-chart">Téléchargements par jour </li>
+            </div>
         </div>
     );
 }
@@ -93,6 +94,5 @@ DownloadsCount.defaultProps = {
 };
 
 DownloadsCount.propTypes = {
-    title: PropTypes.string.isRequired,
     data: PropTypes.arrayOf(PropTypes.object),
 };
