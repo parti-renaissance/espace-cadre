500be37502f610b402dca46e5ae57b0374892d69
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-05-19 14:04:31 +0200

    Redesign map card

--- src/components/Dashboard/Map/MapComponent.jsx
+++ src/components/Dashboard/Map/MapComponent.jsx
@@ -7,26 +7,35 @@ import PropTypes from 'prop-types';
 
 function MapComponent({ mapData }) {
     L.Icon.Default.imagePath = 'images/';
+    const count = mapData.survey_datas.length;
     return (
-        <MapContainer
-            center={[mapData.latitude, mapData.longitude]}
-            zoom={8}
-            className="dc-container"
-        >
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-            />
+        <div className="with-background dc-container w-100">
+            <div className="row p-3">
+                <span className="count-bubble ml-3">{count}</span>
+                <div className="col pl-0">
+                    <div className="chart-title">Questionnaires remplis</div>
+                    <div className="chart-subtitle">Répartition géographique dans votre région</div>
+                </div>
+            </div>
+            <MapContainer
+                center={[mapData.latitude, mapData.longitude]}
+                zoom={8}
+            >
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+                />
 
-            {mapData.survey_datas.map((data) => (
-                <Marker key={data.id} position={[data.latitude, data.longitude]}>
-                    <Popup>
-                        <strong>Nom du sondage:</strong> {data.survey.name} <br />
-                        <strong>Répondu le:</strong> {data.survey.created_at}
-                    </Popup>
-                </Marker>
-            ))}
-        </MapContainer>
+                {mapData.survey_datas.map((data) => (
+                    <Marker key={data.id} position={[data.latitude, data.longitude]}>
+                        <Popup>
+                            <strong>Nom du sondage:</strong> {data.survey.name} <br />
+                            <strong>Répondu le:</strong> {data.survey.created_at}
+                        </Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
+        </div>
     );
 }
 
