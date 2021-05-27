96f66b81f6577485b6b721f4910b6b660480cc64
XavierLac <80252457+XavierLac@users.noreply.github.com>
2021-05-27 15:22:21 +0200

    Various improvements (style, issues) for email campaigns (#175)
    * Fixed decimals
    
    * Fixed decimals + change some datas
    
    * Responsive first line email campaign
    
    * responsive for second line email campaigns

--- src/components/Dashboard/Charts/EmailCampaign/EmailCampaign.jsx
+++ src/components/Dashboard/Charts/EmailCampaign/EmailCampaign.jsx
@@ -40,35 +40,41 @@ function EmailCampaign() {
                         <div className="row">
                             <p className="mail-title"><i className="fas fa-envelope mr-2" />Campagnes d&apos;emails</p>
                         </div>
-                        <div className="row mb-3">
-                            <div className="col mr-3 mb-3 mb-md-0 with-background dc-container little-card">
+                        <div className="row">
+                            <div className="col-12 col-md mb-3 mr-md-3 with-background dc-container little-card">
                                 <div className="main-info">{emailCampaign.local.nbCampagnes}</div>
                                 <div className="main-text">Campagnes</div>
                                 <div className="secondary-text">Envoyées en {new Date().getFullYear()}</div>
                             </div>
-                            <div className="col mr-3 mb-3 mb-md-0 with-background dc-container little-card">
-                                <div className="main-info">{emailCampaign.local.txOuverture * 100}%</div>
+                            <div className="col-12 col-md mb-3 mr-md-3 with-background dc-container little-card">
+                                <div className="main-info">{Number((emailCampaign.local.txOuverture * 100).toFixed(2))}%</div>
                                 <div className="main-text">Ouvertures</div>
-                                <div className="secondary-text">{emailCampaign.national.txOuverture * 100}% au national</div>
+                                <div className="secondary-text">{Number((emailCampaign.national.txOuverture * 100).toFixed(2))}% au national</div>
                             </div>
-                            <div className="col mr-3 mb-3 mb-md-0 with-background dc-container little-card">
-                                <div className="main-info">{emailCampaign.local.txClique * 100}%</div>
+                            <div className="col-12 col-md mb-3 mr-md-3 with-background dc-container little-card">
+                                <div className="main-info">{Number((emailCampaign.local.txClique * 100).toFixed(2))}%</div>
                                 <div className="main-text">Clics</div>
-                                <div className="secondary-text">{emailCampaign.national.txClique * 100}% au national</div>
+                                <div className="secondary-text">{Number((emailCampaign.national.txClique * 100).toFixed(2))}% au national</div>
                             </div>
-                            <div className="col mr-0 mb-3 mb-md-0 with-background dc-container little-card">
-                                <div className="main-info">{emailCampaign.local.txDesabonnement * 100}%</div>
+                            <div className="col-12 col-md mb-0 mb-3 with-background dc-container little-card">
+                                <div className="main-info">{Number((emailCampaign.local.txDesabonnement * 100).toFixed(2))}%</div>
                                 <div className="main-text">Désabonnements</div>
-                                <div className="secondary-text">{emailCampaign.national.txDesabonnement * 100}% au national</div>
+                                <div className="secondary-text">{Number((emailCampaign.national.txDesabonnement * 100).toFixed(2))}% au national</div>
                             </div>
                         </div>
                     </>
                 )
-                : <div className="with-background text-center"><Loader /></div>}
+                : (
+                    <div className="row">
+                        <div className="col with-background dc-container text-center mb-3">
+                            <Loader />
+                        </div>
+                    </div>
+                )}
             {emailCampaignReports !== null && emailCampaignReports.campagnes.length > 0
                 ? (
                     <div className="row">
-                        <div className="col-12 col-lg with-background dc-container mr-lg-3 medium-card">
+                        <div className="col-12 col-lg mr-lg-3 with-background dc-container medium-card">
                             <p className="headline">{emailCampaignReports.campagnes[0].titre}</p>
                             <p className="subtitle-text-card">Le {emailCampaignReports.campagnes[0].date}, par {emailCampaignReports.campagnes[0].auteur}</p>
                             <div className="row p-3">
@@ -77,16 +83,16 @@ function EmailCampaign() {
                                     <div className="text-below-info-number">Emails</div>
                                 </div>
                                 <div className="col flash-card mr-3">
-                                    <div className="info-number">{emailCampaignReports.campagnes[0].nbOuvertures}</div>
+                                    <div className="info-number">{Number((emailCampaignReports.campagnes[0].txOuverture).toFixed(2))}</div>
                                     <div className="text-below-info-number">Ouvertures</div>
                                 </div>
                                 <div className="col flash-card">
-                                    <div className="info-number">{emailCampaignReports.campagnes[0].nbCliques}</div>
+                                    <div className="info-number">{Number((emailCampaignReports.campagnes[0].txClique).toFixed(2))}</div>
                                     <div className="text-below-info-number">Clics</div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-12 col-lg with-background dc-container mr-lg-3 medium-card">
+                        <div className="col-12 col-lg with-background dc-container medium-card">
                             <p className="headline">{emailCampaignReports.campagnes[1].titre}</p>
                             <p className="subtitle-text-card">Le {emailCampaignReports.campagnes[1].date}, par {emailCampaignReports.campagnes[1].auteur}</p>
                             <div className="row p-3">
@@ -95,11 +101,11 @@ function EmailCampaign() {
                                     <div className="text-below-info-number">Emails</div>
                                 </div>
                                 <div className="col flash-card mr-3">
-                                    <div className="info-number">{emailCampaignReports.campagnes[1].nbOuvertures}</div>
+                                    <div className="info-number">{Number((emailCampaignReports.campagnes[1].txOuverture).toFixed(2))}</div>
                                     <div className="text-below-info-number">Ouvertures</div>
                                 </div>
                                 <div className="col flash-card">
-                                    <div className="info-number">{emailCampaignReports.campagnes[1].nbCliques}</div>
+                                    <div className="info-number">{Number((emailCampaignReports.campagnes[1].txClique).toFixed(2))}</div>
                                     <div className="text-below-info-number">Clics</div>
                                 </div>
                             </div>
