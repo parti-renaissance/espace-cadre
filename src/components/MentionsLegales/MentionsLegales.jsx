2ff2e2e72865bbc422ec7e90fb0a26d651e95d4e
XavierLac <80252457+XavierLac@users.noreply.github.com>
2021-06-02 10:17:20 +0200

    Mentions légales (#190)

---
+++ src/components/MentionsLegales/MentionsLegales.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function MentionsLegales() {
+    return (
+        <div className="mentionsLegales">
+            <div className="version">Version 0.1.12</div>
+            <ul className="mentionLegalesList">
+                <li>
+                    <a href="https://donnees.en-marche.fr/">
+                        Mes données personnelles
+                    </a>
+                </li>
+                <li>
+                    <a href="https://en-marche.fr/mentions-legales">
+                        Mentions légales
+                    </a>
+                </li>
+                <li>
+                    <a href="https://en-marche.fr/politique-cookies">
+                        Politique de cookies
+                    </a>
+                </li>
+                <li>
+                    <a href="https://en-marche.fr/politique-protection-donnees">
+                        Politique de protection des données
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.bkms-system.com/bkwebanon/report/clientInfo?cin=Jp3wHD&c=-1&language=fre">
+                        Cellule d&apos;alerte
+                    </a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default MentionsLegales;
