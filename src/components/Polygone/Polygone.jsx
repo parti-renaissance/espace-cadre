9384d116b87291de82ad3efe31680e29774b5337
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-06-03 16:59:31 +0200

    Switch between two layers

--- src/components/Polygone/Polygone.jsx
+++ src/components/Polygone/Polygone.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {
-    MapContainer, Polygon, TileLayer,
+    MapContainer, Polygon, TileLayer, LayersControl, FeatureGroup,
 } from 'react-leaflet';
 import coordinates from './data.json';
 import Spinner from '../Spinner/Spinner';
@@ -14,20 +14,51 @@ const Polygone = () => {
             {geoData.length > 0 ? (
                 <MapContainer
                     center={center}
-                    zoom="13"
+                    zoom="11"
                     className="leaflet-container-polygone"
                 >
-                    <TileLayer
-                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-                    />
-                    {geoData.map((departement) => (
-                        <Polygon
-                            key={departement.properties.nom}
-                            pathOptions={{ color: '#0049C6', weight: '0.5' }}
-                            positions={departement.geometry.coordinates}
+                    <LayersControl position="topright">
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
                         />
-                    ))}
+                        <LayersControl.Overlay name="Présidentielles 2017">
+                            <FeatureGroup>
+                                {geoData.map((departement) => (
+                                    <Polygon
+                                        key={departement.properties.nom}
+                                        pathOptions={{ color: '#0049C6', weight: '0.5' }}
+                                        positions={departement.geometry.coordinates}
+                                    />
+                                ))}
+                            </FeatureGroup>
+                        </LayersControl.Overlay>
+                        <LayersControl.Overlay name="Présidentielles 2021">
+                            <Polygon
+                                pathOptions={{ color: 'red', weight: '0.5' }}
+                                positions={
+                                    [
+                                        [
+                                            48.884291351874,
+                                            2.3646776664012,
+                                        ],
+                                        [
+                                            48.883620167912,
+                                            2.3495508401232,
+                                        ],
+                                        [
+                                            48.882249883375,
+                                            2.3379555061118,
+                                        ],
+                                        [
+                                            48.883485416617,
+                                            2.3271206970663,
+                                        ],
+                                    ]
+                                }
+                            />
+                        </LayersControl.Overlay>
+                    </LayersControl>
                 </MapContainer>
             ) : <Spinner />}
         </>
