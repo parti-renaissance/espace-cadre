38a7b0da2153eb5b6e85adf1591aebf625f81571
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-06-11 18:44:43 +0200

    Working map from mapbox studio

--- src/components/Elections/Elections.jsx
+++ src/components/Elections/Elections.jsx
@@ -3,9 +3,6 @@ import React, { useEffect, useRef, useState } from 'react';
 import mapboxgl from '!mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import departements from './data/departements_v4.json';
-import arrondissementParis from './data/paris_arrondissements.json';
-
 mapboxgl.accessToken = 'pk.eyJ1IjoibGFyZW0iLCJhIjoiY2twcW9wYWp6MW54MDJwcXF4em1ieWh3eSJ9.LxKs_dipHMNZ-JdTkyKEMQ';
 
 export default () => {
@@ -13,84 +10,36 @@ export default () => {
     const map = useRef(null);
     const [lng] = useState(2.213749);
     const [lat] = useState(46.227638);
-    const [zoom] = useState(5);
-    const [departementsData] = useState(departements);
-    const [arrondissementsData] = useState(arrondissementParis);
+    const [zoom] = useState(6);
 
     useEffect(() => {
         if (map.current) return; // initialize map only once
 
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
+            style: 'mapbox://styles/larem/ckps60m1405ic17qb2olzlahx',
             center: [lng, lat],
             zoom,
         });
 
         map.current.on('load', () => {
-            map.current.addSource('france', {
-                type: 'geojson',
-                data: departementsData,
-            });
+            map.current.on('mousemove', (e) => {
+                const departementsFromMapbox = map.current.queryRenderedFeatures(e.point, {
+                    layers: ['departementsdata'],
+                });
 
-            // Départements
-            map.current.addLayer({
-                id: 'france',
-                type: 'fill',
-                source: 'france',
-                layout: {},
-                paint: {
-                    'fill-color': '#0080ff',
-                    'fill-opacity': 0.5,
-                },
-            });
-
-            map.current.addLayer({
-                id: 'outline',
-                type: 'line',
-                source: 'france',
-                layout: {},
-                paint: {
-                    'line-color': '#F0F1F3',
-                    'line-width': 0.5,
-                },
-            });
-        });
-
-        map.current.on('load', () => {
-            // Arrondissements
-            map.current.addSource('arrondissements', {
-                type: 'geojson',
-                data: arrondissementsData,
-            });
-
-            map.current.addLayer({
-                id: 'arrondissements',
-                type: 'fill',
-                source: 'arrondissements',
-                layout: {},
-                paint: {
-                    'fill-color': '#0080ff',
-                    'fill-opacity': 0.5,
-                },
-            });
-
-            map.current.addLayer({
-                id: 'outline2',
-                type: 'line',
-                source: 'arrondissements',
-                layout: {},
-                paint: {
-                    'line-color': '#F0F1F3',
-                    'line-width': 0.5,
-                },
+                if (departementsFromMapbox.length > 0) {
+                    document.getElementById('pd').innerHTML = `<div>${departementsFromMapbox[0].properties.elections} infos</div>`;
+                } else {
+                    document.getElementById('pd').innerHTML = '<div>Survolez la carte pour afficher des informations</div>';
+                }
             });
         });
     });
-
     return (
         <div>
             <div ref={mapContainer} className="map-container" />
+            <div className="map-overlay" id="features"><h2>Cartes des élections</h2><div id="pd"><p>Survolez la carte</p></div></div>
         </div>
     );
 };
