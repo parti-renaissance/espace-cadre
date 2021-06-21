72e4cba199e50824c7b6b9795e92225fddabaa7b
XavierLac <80252457+XavierLac@users.noreply.github.com>
2021-06-21 10:51:33 +0200

    Mapbox (#206)
    * Bump node-sass from 5.0.0 to 6.0.0 (#212)
    
    Bumps [node-sass](https://github.com/sass/node-sass) from 5.0.0 to 6.0.0.
    - [Release notes](https://github.com/sass/node-sass/releases)
    - [Changelog](https://github.com/sass/node-sass/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/sass/node-sass/compare/v5.0.0...v6.0.0)
    
    ---
    updated-dependencies:
    - dependency-name: node-sass
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * WIP
    
    * Add component to sidebar + map size
    
    * Working map with two geojson files
    
    * Working map from mapbox studio
    
    * WIP show map with papaparse
    
    * WIP
    
    * Style for popup
    
    * Departements csv
    
    * Regions csv
    
    * Add layer select
    
    * Prevent popup from multiple layers to pop in the same time
    
    * Add Cantons csv
    
    * Add new cantons csv
    
    * Handle cantons layer
    
    * Update style
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>

---
+++ src/components/Elections/Elections.jsx
@@ -0,0 +1,282 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useEffect, useRef, useState } from 'react';
+import Papa from 'papaparse';
+// eslint-disable-next-line import/no-unresolved,import/no-webpack-loader-syntax
+import mapboxgl from '!mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import regions from './data/regions.csv';
+import departements from './data/departements.csv';
+import cantons from './data/cantons.csv';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibGFyZW0iLCJhIjoiY2twcW9wYWp6MW54MDJwcXF4em1ieWh3eSJ9.LxKs_dipHMNZ-JdTkyKEMQ';
+
+const LAYER_REGION = 'regions';
+const LAYER_DEPARTMENT = 'departements';
+const LAYER_CANTONS = 'cantons';
+
+function Elections() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [regionsCsv, setRegionsCsv] = useState();
+    const [departementsCsv, setDepartementsCsv] = useState();
+    const [cantonsCsv, setCantonsCsv] = useState();
+    const [activeLayer, setActiveLayer] = useState(LAYER_REGION);
+    const [mapLoaded, setMapLoaded] = useState(false);
+    const layers = ['Régions', 'Départements', 'Cantons'];
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        // Display the map
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/larem/ckps60m1405ic17qb2olzlahx',
+            center: [2.213749, 46.227638],
+            zoom: 5,
+        });
+    }, []);
+
+    // Wait for the map to be loaded and display layer visibility to none
+    useEffect(() => {
+        map.current.on('load', () => {
+            setMapLoaded(true);
+            map.current.setLayoutProperty(LAYER_REGION, 'visibility', 'none');
+            map.current.setLayoutProperty(LAYER_DEPARTMENT, 'visibility', 'none');
+            map.current.setLayoutProperty(LAYER_CANTONS, 'visibility', 'none');
+        });
+    }, [map]);
+
+    // Once the map is loaded, set region as default layer and prepare csv data
+    useEffect(() => {
+        if (mapLoaded) {
+            map.current.setLayoutProperty(activeLayer, 'visibility', 'visible');
+
+            // Convert region csv to JSON
+            Papa.parse(regions, {
+                delimiter: ',',
+                download: true,
+                header: true,
+                skipEmptyLines: true,
+                complete(results) {
+                    setRegionsCsv(results);
+                },
+            });
+
+            // Convert departements csv to JSON
+            Papa.parse(departements, {
+                delimiter: ',',
+                download: true,
+                header: true,
+                skipEmptyLines: true,
+                complete(results) {
+                    setDepartementsCsv(results);
+                },
+            });
+
+            // Convert cantons csv to JSON
+            Papa.parse(cantons, {
+                delimiter: ',',
+                download: true,
+                header: true,
+                skipEmptyLines: true,
+                complete(results) {
+                    setCantonsCsv(results);
+                },
+            });
+        }
+    }, [activeLayer, mapLoaded]);
+
+    // Display region on the map
+    useEffect(() => {
+        if (regionsCsv !== undefined) {
+            const popup = new mapboxgl.Popup();
+
+            map.current.on('click', (e) => {
+                map.current.getCanvas().style.cursor = 'pointer';
+                const regionsFromMapbox = map.current.queryRenderedFeatures(e.point, {
+                    layers: ['regions'],
+                });
+
+                const props = regionsFromMapbox[0];
+                if (props !== undefined) {
+                    // eslint-disable-next-line react/prop-types
+                    const data = regionsCsv.data.filter((el) => (el.region === props.properties.code));
+                    console.log(data);
+                    popup
+                        .setLngLat(e.lngLat)
+                        .setHTML(data.map((el) => (
+                            `
+                                <table class="table table-stripe">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">
+                                                Année
+                                            </th>
+                                            <th scope="col">
+                                                Nom de la liste
+                                            </th>
+                                            <th scope="col">
+                                                Tour
+                                            </th>
+                                            <th scope="col">
+                                                votants
+                                            </th>
+                                            <th scope="col">
+                                            Voix
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>${el.annee}</td>
+                                            <td>${el.nom_liste}</td>
+                                            <td>${el.tour}</td>
+                                            <td>${el.votants}</td>
+                                            <td>${el.voix}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            `
+                        ))).addTo(map.current);
+                }
+            });
+        }
+    }, [regionsCsv]);
+
+    // Display departements on the map
+    useEffect(() => {
+        if (departementsCsv !== undefined) {
+            const popup = new mapboxgl.Popup();
+
+            map.current.on('click', (e) => {
+                map.current.getCanvas().style.cursor = 'pointer';
+                const departementsFromMapbox = map.current.queryRenderedFeatures(e.point, {
+                    layers: ['departements'],
+                });
+                const props = departementsFromMapbox[0];
+                if (props !== undefined) {
+                    // eslint-disable-next-line react/prop-types
+                    const data = departementsCsv.data.filter((el) => (el.departement === props.properties.code));
+                    popup
+                        .setLngLat(e.lngLat)
+                        .setHTML(data.map((el) => (
+                            `
+                                <table class="table table-stripe">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">
+                                                Année
+                                            </th>
+                                            <th scope="col">
+                                                Nom de la liste
+                                            </th>
+                                            <th scope="col">
+                                                Tour
+                                            </th>
+                                            <th scope="col">
+                                                votants
+                                            </th>
+                                            <th scope="col">
+                                            Voix
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>${el.annee}</td>
+                                            <td>${el.nom_liste}</td>
+                                            <td>${el.tour}</td>
+                                            <td>${el.votants}</td>
+                                            <td>${el.voix}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            `
+                        )))
+                        .addTo(map.current);
+                }
+            });
+        }
+    }, [departementsCsv]);
+
+    // Display cantons on the map
+    useEffect(() => {
+        if (cantonsCsv !== undefined) {
+            const popup = new mapboxgl.Popup();
+
+            map.current.on('click', (e) => {
+                map.current.getCanvas().style.cursor = 'pointer';
+                const cantonsFromMapbox = map.current.queryRenderedFeatures(e.point, {
+                    layers: ['cantons'],
+                });
+                const props = cantonsFromMapbox[0];
+                if (props !== undefined) {
+                    // eslint-disable-next-line react/prop-types
+                    const data = cantonsCsv.data.filter((el) => (el.code_canton === props.properties.code));
+                    popup
+                        .setLngLat(e.lngLat)
+                        .setHTML(data.map((el) => (
+                            `
+                                <table class="table table-stripe">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">
+                                                Année
+                                            </th>
+                                            <th scope="col">
+                                                Nom de la liste
+                                            </th>
+                                            <th scope="col">
+                                                Tour
+                                            </th>
+                                            <th scope="col">
+                                                votants
+                                            </th>
+                                            <th scope="col">
+                                            Voix
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>${el.annee}</td>
+                                            <td>${el.nom_liste}</td>
+                                            <td>${el.tour}</td>
+                                            <td>${el.votants}</td>
+                                            <td>${el.voix}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            `
+                        )))
+                        .addTo(map.current);
+                }
+            });
+        }
+    }, [cantonsCsv]);
+
+    const handleChange = (e) => {
+        if (e.target.value === 'Régions') {
+            setActiveLayer(LAYER_REGION);
+            map.current.setLayoutProperty(LAYER_DEPARTMENT, 'visibility', 'none');
+            map.current.setLayoutProperty(LAYER_CANTONS, 'visibility', 'none');
+        } else if (e.target.value === 'Départements') {
+            setActiveLayer(LAYER_DEPARTMENT);
+            map.current.setLayoutProperty(LAYER_REGION, 'visibility', 'none');
+            map.current.setLayoutProperty(LAYER_CANTONS, 'visibility', 'none');
+        } else if (e.target.value === 'Cantons') {
+            setActiveLayer(LAYER_CANTONS);
+            map.current.setLayoutProperty(LAYER_DEPARTMENT, 'visibility', 'none');
+            map.current.setLayoutProperty(LAYER_REGION, 'visibility', 'none');
+        }
+    };
+    return (
+        <div>
+            <select className="mb-3 mr-3" onChange={handleChange}>
+                {layers.map((layer, i) => <option key={i + 1} value={layer}>{layer}</option>)}
+            </select>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
+}
+
+export default Elections;
