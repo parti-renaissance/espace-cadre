57c41077e55c270c0ed749cf3343995ac9e8bf73
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-06-22 10:24:00 +0200

    Add proptypes

--- src/components/Elections/ElectionTable.jsx
+++ src/components/Elections/ElectionTable.jsx
@@ -1,6 +1,12 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ElectionTable = ({ row }) => (
+const ElectionTable = ({
+    row: {
+        election, annee, nom_liste, tour, voix,
+    },
+}) => (
     <table className="table elections-table">
         <thead>
             <tr>
@@ -13,14 +19,18 @@ const ElectionTable = ({ row }) => (
         </thead>
         <tbody>
             <tr>
-                <td>{row.election}</td>
-                <td>{row.annee}</td>
-                <td>{row.nom_liste}</td>
-                <td>{row.tour}</td>
-                <td>{row.voix}</td>
+                <td>{election}</td>
+                <td>{annee}</td>
+                <td>{nom_liste}</td>
+                <td>{tour}</td>
+                <td>{voix}</td>
             </tr>
         </tbody>
     </table>
 );
 
 export default ElectionTable;
+
+ElectionTable.propTypes = {
+    row: PropTypes.objectOf(Object).isRequired,
+};
