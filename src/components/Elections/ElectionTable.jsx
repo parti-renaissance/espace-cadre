041f1e1a09137fa7501c819ff04c5f8654d1ca2b
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-06-22 10:59:38 +0200

    Add nuance in popup

--- src/components/Elections/ElectionTable.jsx
+++ src/components/Elections/ElectionTable.jsx
@@ -2,25 +2,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const ELECTION = 'Élection';
+const YEAR = 'Année';
+const NUANCE = 'Nuance';
+const LIST = 'Liste';
+const ROUND = 'Tour';
+const VOTE = 'Voix';
+const TABLE_TITLE = [
+    {
+        code: ELECTION,
+        label: 'Élection',
+    },
+    {
+        code: YEAR,
+        label: 'Année',
+    },
+    {
+        code: NUANCE,
+        label: 'Nuance',
+    },
+    {
+        code: LIST,
+        label: 'Liste',
+    },
+    {
+        code: ROUND,
+        label: 'Tour',
+    },
+    {
+        code: VOTE,
+        label: 'Voix',
+    },
+];
+
 const ElectionTable = ({
     row: {
-        election, annee, nom_liste, tour, voix,
+        election, annee, nom_liste, tour, voix, nuance,
     },
 }) => (
     <table className="table elections-table">
         <thead>
-            <tr>
-                <th scope="col">Election</th>
-                <th scope="col">Année</th>
-                <th scope="col">Liste</th>
-                <th scope="col">Tour</th>
-                <th scope="col">Voix</th>
+            <tr>{TABLE_TITLE.map((title, i) => <th scope="col" key={i + 1}>{title.label}</th>)}
             </tr>
         </thead>
         <tbody>
             <tr>
                 <td>{election}</td>
                 <td>{annee}</td>
+                <td>{nuance}</td>
                 <td>{nom_liste}</td>
                 <td>{tour}</td>
                 <td>{voix}</td>
