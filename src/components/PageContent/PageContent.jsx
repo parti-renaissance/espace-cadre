1fab79fb6af9048340f254fd1d791c0f805bbf92
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-06-30 11:34:12 +0200

    Dynamic list of roles

--- src/components/PageContent/PageContent.jsx
+++ src/components/PageContent/PageContent.jsx
@@ -1,10 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { MENU } from '../../Routes';
-import { useDashboardAdherentCache } from '../../redux/dashboard/hooks';
+import { apiClient } from '../../services/networking/client';
 
 const PageContent = ({ children, currentUser }) => {
     const { pathname } = useLocation();
@@ -13,7 +13,31 @@ const PageContent = ({ children, currentUser }) => {
     if (pathIndex !== -1) {
         pageTitle = MENU[pathIndex].label || null;
     }
-    const [dashboardAdherents] = useDashboardAdherentCache();
+    const [roles, setRoles] = useState([]);
+
+    function parseName(name) {
+        if (name === 'candidate') {
+            return 'Candidat';
+        } if (name === 'deputy') {
+            return 'Député';
+        } if (name === 'senator') {
+            return 'Sénateur';
+        }
+        return 'Référent';
+    }
+
+    const getScopes = async () => {
+        try {
+            setRoles(await apiClient.get('/v3/profile/me/scopes'));
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    useEffect(() => {
+        getScopes();
+    }, []);
+
     return (
         <div className="page-content" id="content">
             <div className="container-fluid mt-3">
@@ -27,34 +51,26 @@ const PageContent = ({ children, currentUser }) => {
                     {currentUser && (
                         <div className="ml-sm-auto mr-3 mt-3">
                             <Dropdown>
-                                <Dropdown.Toggle variant="none">
+                                <Dropdown.Toggle variant="">
                                     <span className="profile-id">{currentUser.firstName} {currentUser.lastName}</span> <br />
-                                    <span className="profile-info">{dashboardAdherents !== null ? <span> Candidat &gt; {dashboardAdherents.zoneName} (11)</span> : ''}</span>
+                                    {roles.length > 0 && <span className="profile-info">{parseName(roles[0].code)} &gt; {roles[0].zones[0].name} {`(${roles[0].zones[0].code})`}</span>}
+                                    <img className="caret-dropdown" src="images/vector.svg" alt="caret" />
                                 </Dropdown.Toggle>
-
                                 <Dropdown.Menu>
-                                    <Dropdown.Item href="#/action-2">
-                                        <span className="profile-role">Référent</span> <br />
-                                        <span className="profile-place">Hauts-de-Seine (92)</span>
-                                    </Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">
-                                        <span className="profile-role">Référent délégué</span> <br />
-                                        <span className="profile-place">Victor Cohen - Hauts-de-Seine (92)</span>
-                                    </Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">
-                                        <span className="profile-role">Animateur</span> <br />
-                                        <span className="profile-place">Courbevoie En Marche (92400)</span>
-                                    </Dropdown.Item>
-                                    <Dropdown.Divider />
                                     <Dropdown.Item href="https://en-marche.fr/">
                                         <span className="profile-role">Retour sur en-marche.fr</span>
                                     </Dropdown.Item>
+                                    {roles.length > 0 && roles.slice(1).map((role, i) => (
+                                        <Dropdown.Item href="#/action-2" key={i + 1}>
+                                            <span className="profile-role">{parseName(role.code)}</span> <br />
+                                            <span className="profile-place">{role.zones[0].name} {`(${role.zones[0].code})`}</span>
+                                        </Dropdown.Item>
+                                    ))}
                                 </Dropdown.Menu>
                             </Dropdown>
                         </div>
                     )}
                 </div>
-
                 {children}
             </div>
         </div>
