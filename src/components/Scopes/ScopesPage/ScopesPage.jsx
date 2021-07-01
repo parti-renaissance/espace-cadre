767c76189ab28f1e53b02151e868bd0f161b160a
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-07-01 13:21:08 +0200

    Handle logic for choosen scope

--- src/components/Scopes/ScopesPage/ScopesPage.jsx
+++ src/components/Scopes/ScopesPage/ScopesPage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { getCurrentUser, getUserScopes } from '../../../redux/user/selectors';
@@ -5,6 +7,11 @@ import { getCurrentUser, getUserScopes } from '../../../redux/user/selectors';
 function ScopesPage() {
     const userScopes = useSelector(getUserScopes);
     const currentUser = useSelector(getCurrentUser);
+
+    function handleClick(scope) {
+        console.log(scope);
+    }
+
     return (
         <div className="scopes-page-container">
             <div className="row logo-title-container">
@@ -18,15 +25,12 @@ function ScopesPage() {
             </div>
             {userScopes.length > 0 && (
                 <div className="row secondary-scope-card-container">
-                    {userScopes.map((userScope, index) => {
-                        console.log(userScope);
-                        return (
-                            <div className="secondary-card" key={index + 1}>
-                                <div className="role">{userScope.code}</div>
-                                <div className="zone">{userScope.zones[0].name} {`(${userScope.zones[0].code})`}</div>
-                            </div>
-                        );
-                    })}
+                    {userScopes.map((userScope, index) => (
+                        <div className="secondary-card" key={index + 1} value={userScope} onClick={() => handleClick(userScope)}>
+                            <div className="role">{userScope.code}</div>
+                            <div className="zone">{userScope.zones[0].name} {`(${userScope.zones[0].code})`}</div>
+                        </div>
+                    ))}
                 </div>
             )}
         </div>
