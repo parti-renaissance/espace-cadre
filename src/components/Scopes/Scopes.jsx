17cb2ca2d6cad61183f673735d016fa7f40cad55
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-07-02 14:18:52 +0200

    Style for dropdown

--- src/components/Scopes/Scopes.jsx
+++ src/components/Scopes/Scopes.jsx
@@ -26,14 +26,21 @@ function Scopes() {
             {currentUser && userScopes.length > 0 && (
                 <Dropdown>
                     <Dropdown.Toggle variant="">
-                        <span className="profile-id">{currentUser.firstName} {currentUser.lastName}</span> <br />
-                        <span className="profile-info"> <ParseName name={currentScope.code} /> &gt; {currentScope.zones[0].name} {`(${currentScope.zones[0].code})`}</span>
-                        <img className="caret-dropdown" src="images/vector.svg" alt="caret" />
+                        <div className="row">
+                            <div className="col-10 info-col">
+                                <span className="profile-id">{currentUser.firstName} {currentUser.lastName}</span> <br />
+                                <span className="profile-info"> <ParseName name={currentScope.code} /> &gt; {currentScope.zones[0].name} {`(${currentScope.zones[0].code})`}</span>
+                            </div>
+                            <div className="col-2 caret-col">
+                                <img className="caret-dropdown" src="images/vector.svg" alt="caret" />
+                            </div>
+                        </div>
                     </Dropdown.Toggle>
                     <Dropdown.Menu>
                         <Dropdown.Item href="https://en-marche.fr/">
                             <span className="profile-role">Retour sur en-marche.fr</span>
                         </Dropdown.Item>
+                        {userScopes.length > 1 && <Dropdown.Divider />}
                         {userScopes.filter((el) => el !== currentScope).map((userScope, i) => (
                             <Dropdown.Item href="#/action-2" key={i + 1} onClick={() => handleClick(userScope)}>
                                 <span className="profile-role"><ParseName name={userScope.code} /></span> <br />
