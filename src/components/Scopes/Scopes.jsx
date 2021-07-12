8fbdf33b6d97d00c80099335946e54c73d8f27cc
XavierLac <80252457+XavierLac@users.noreply.github.com>
2021-07-12 16:42:20 +0200

    Add all zones for scopes (#264)
    * Add all zones for scopes
    
    * Add all areas for scope in dropdown

--- src/components/Scopes/Scopes.jsx
+++ src/components/Scopes/Scopes.jsx
@@ -14,7 +14,6 @@ function Scopes() {
     if (currentScope && currentScope.zones !== undefined && currentScope.zones.length) {
         zone = `${currentScope.zones[0].name} (${currentScope.zones[0].code})`;
     }
-
     return (
         <div>
             {currentUser && userScopes.length > 0 && (
@@ -40,7 +39,12 @@ function Scopes() {
                         {userScopes.filter((el) => el.code !== currentScope.code).map((userScope, i) => (
                             <Dropdown.Item key={i + 1} onClick={() => updateCurrentScope(userScope)}>
                                 <span className="profile-role">{userScope.name}</span> <br />
-                                <span className="profile-place">{userScope.zones[0].name} {`(${userScope.zones[0].code})`}</span>
+
+                                {
+                                    userScope.zones.length > 1
+                                        ? <span className="profile-place">{userScope.zones.map((el, index) => <div key={index + 1}>{el.name} ({el.code})</div>)}</span>
+                                        : <span className="profile-place">{userScope.zones[0].name} {`(${userScope.zones[0].code})`}</span>
+                                }
                             </Dropdown.Item>
                         ))}
                     </Dropdown.Menu>
