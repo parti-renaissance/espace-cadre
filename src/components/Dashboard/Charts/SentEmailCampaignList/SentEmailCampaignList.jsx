f738b7d17a5c13f834eabe9f1e72db5f87ed3a7e
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-07-15 11:56:39 +0200

    Display message when no email campaign

--- src/components/Dashboard/Charts/SentEmailCampaignList/SentEmailCampaignList.jsx
+++ src/components/Dashboard/Charts/SentEmailCampaignList/SentEmailCampaignList.jsx
@@ -20,51 +20,56 @@ function SentEmailCampaignList() {
         getEmailCampaignReports();
     }, [emailCampaignReports]);
 
+    const emailCampaignsContent = () => {
+        if (emailCampaignReports !== null && emailCampaignReports[0].campagnes.length > 0) {
+            return (emailCampaignReports[0].campagnes.map((el, index) => (
+                <div className="col-12 with-background dc-container big-card" key={index + 1}>
+                    <p className="headline">{el.titre}</p>
+                    <p className="subtitle-text-card">Le {el.date}, par {el.auteur}</p>
+                    <div className="row p-3 flash-card-row">
+                        <div className="col-5 col-sm flash-card mr-2">
+                            <div className="info-number">{el.nbEmails}</div>
+                            <div className="text-below-info-number">Email{el.nbEmails > 1 && 's'}</div>
+                        </div>
+                        <div className="col-5 col-sm flash-card mr-2">
+                            <div className="info-number">
+                                <ConvertToPercent
+                                    valueToConvert={el.txOuverture}
+                                />
+                                <span className="parenthese-info">({el.nbOuvertures})</span>
+                            </div>
+                            <div className="text-below-info-number">Ouvertures</div>
+                        </div>
+                        <div className="col-5 col-sm flash-card mr-2">
+                            <div className="info-number">
+                                <ConvertToPercent valueToConvert={el.txClique} />
+                                <span className="parenthese-info">({el.nbCliques})</span>
+                            </div>
+                            <div className="text-below-info-number">Clics</div>
+                        </div>
+                        <div className="col-5 col-sm flash-card">
+                            <div className="info-number">
+                                <ConvertToPercent valueToConvert={el.txDesabonnement} />
+                                <span className="parenthese-info">({el.nbDesabonnements})</span>
+                            </div>
+                            <div className="text-below-info-number">Désabonnements</div>
+                        </div>
+                    </div>
+                </div>
+            )));
+        } if (emailCampaignReports !== null && emailCampaignReports[0].campagnes.length === 0) {
+            return <div className="col with-background dc-container text-center mb-3">Aucune campagne à afficher</div>;
+        }
+        return (
+            <div className="col with-background dc-container text-center mb-3">
+                <Loader />
+            </div>
+        );
+    };
+
     return (
         <>
-            {emailCampaignReports !== null && emailCampaignReports[0].campagnes.length > 0
-                ? (
-                    emailCampaignReports[0].campagnes.map((el, index) => (
-                        <div className="col-12 with-background dc-container big-card" key={index + 1}>
-                            <p className="headline">{el.titre}</p>
-                            <p className="subtitle-text-card">Le {el.date}, par {el.auteur}</p>
-                            <div className="row p-3 flash-card-row">
-                                <div className="col-5 col-sm flash-card mr-2">
-                                    <div className="info-number">{el.nbEmails}</div>
-                                    <div className="text-below-info-number">Email{el.nbEmails > 1 && 's'}</div>
-                                </div>
-                                <div className="col-5 col-sm flash-card mr-2">
-                                    <div className="info-number">
-                                        <ConvertToPercent
-                                            valueToConvert={el.txOuverture}
-                                        />
-                                        <span className="parenthese-info">({el.nbOuvertures})</span>
-                                    </div>
-                                    <div className="text-below-info-number">Ouvertures</div>
-                                </div>
-                                <div className="col-5 col-sm flash-card mr-2">
-                                    <div className="info-number">
-                                        <ConvertToPercent valueToConvert={el.txClique} />
-                                        <span className="parenthese-info">({el.nbCliques})</span>
-                                    </div>
-                                    <div className="text-below-info-number">Clics</div>
-                                </div>
-                                <div className="col-5 col-sm flash-card">
-                                    <div className="info-number">
-                                        <ConvertToPercent valueToConvert={el.txDesabonnement} />
-                                        <span className="parenthese-info">({el.nbDesabonnements})</span>
-                                    </div>
-                                    <div className="text-below-info-number">Désabonnements</div>
-                                </div>
-                            </div>
-                        </div>
-                    ))
-                )
-                : (
-                    <div className="col with-background dc-container text-center mb-3">
-                        <Loader />
-                    </div>
-                )}
+            {emailCampaignsContent()}
         </>
     );
 }
