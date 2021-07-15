9be64921f2d9b844c723e5397b9157b6d1c0cf58
XavierLac <80252457+XavierLac@users.noreply.github.com>
2021-07-15 12:43:53 +0200

    Fix missing interests (#279)

--- src/components/ContactsPage/ColumnsContentRendering/InterestRendering.jsx
+++ src/components/ContactsPage/ColumnsContentRendering/InterestRendering.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const InterestRendering = ({ interest }) => interest.length > 0 && interest.value.map((element, index) => (
-    <span key={index} className="interests-badge">
-        <span>
-            {element.charAt(0).toUpperCase() + element.slice(1)}
-        </span>
-    </span>
-));
+const InterestRendering = ({ interest }) => {
+    if (interest.value[0] === '') {
+        return '';
+    }
+
+    return (
+        <>
+            {interest.value.length > 0 && interest.value.map((element, index) => (
+                <span key={index} className="interests-badge">
+                    <span>
+                        {element.charAt(0).toUpperCase() + element.slice(1)}
+                    </span>
+                </span>
+            ))}
+        </>
+    );
+};
 
 export default InterestRendering;
 
