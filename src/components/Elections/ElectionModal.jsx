523c28f32df61a797c3e514ec15e62079d3b4858
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-07-21 09:36:26 +0200

    Reintroduce modal

--- src/components/Elections/ElectionModal.jsx
+++ src/components/Elections/ElectionModal.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 
 const ElectionModal = ({
     row: {
-        voix, nomNuance, liste, voixPourcent, codeCouleur,
+        nuance, nom, prenom, voix,
     },
 }) => (
     <div className="election-modal-content">
-        <div className="candidat-name">{liste}</div>
-        <div className="candidat-nuance">{nomNuance}</div>
-        <div className="candidat-resultat">{voix.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} voix - <span className="resultat-span">{voixPourcent}%</span></div>
-        <div className="progress" style={{ height: '8px' }}>
+        <div className="candidat-name">{prenom} {nom}</div>
+        <div className="candidat-nuance">{nuance}</div>
+        <div className="candidat-resultat">{voix.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} voix - <span className="resultat-span">{voix}</span></div>
+        {/* <div className="progress" style={{ height: '8px' }}>
             <div
                 className="progress-bar"
                 role="progressbar"
@@ -20,7 +20,7 @@ const ElectionModal = ({
                 aria-valuemin="0"
                 aria-valuemax="100"
             />
-        </div>
+</div> */}
     </div>
 );
 
--- src/components/Elections/Elections.jsx
+++ src/components/Elections/Elections.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useRef, useState } from 'react';
-// import { renderToString } from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 import $ from 'jquery';
 // eslint-disable-next-line import/no-unresolved,import/no-webpack-loader-syntax
 import mapboxgl from '!mapbox-gl';
@@ -8,6 +8,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 // import ElectionModal from './ElectionModal';
 import LayerFilter from './Filter/LayerFilter';
 import { apiClientProxy } from '../../services/networking/client';
+import ElectionModal from './ElectionModal';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX;
 
@@ -43,18 +44,12 @@ const LAYERS_TYPES = [
         label: 'Bureaux de vote',
     },
 ];
-const ELECTION_TYPE_PRESIDENTIAL = 'presidential';
-const ELECTION_TYPE_DEPARTMENTAL = 'departmental';
-const ELECTION_TYPE_LEGISLATIVE = 'legislative';
-const ELECTION_TYPE_REGIONAL = 'regional';
-const ELECTION_TYPE_EUROPEANS = 'Européennes';
-
-const ELECTION_LABELS = {};
-ELECTION_LABELS[ELECTION_TYPE_PRESIDENTIAL] = 'Présidentielles';
-ELECTION_LABELS[ELECTION_TYPE_DEPARTMENTAL] = 'Départementales';
-ELECTION_LABELS[ELECTION_TYPE_LEGISLATIVE] = 'Législatives';
-ELECTION_LABELS[ELECTION_TYPE_REGIONAL] = 'Régionales';
-ELECTION_LABELS[ELECTION_TYPE_EUROPEANS] = 'Européennes';
+const ELECTION_TYPE_PRESIDENTIAL = 'Présidentielles';
+const ELECTION_TYPE_DEPARTMENTAL = 'Départementales';
+const ELECTION_TYPE_LEGISLATIVE = 'Législatives';
+const ELECTION_TYPE_REGIONAL = 'Régionales';
+const ELECTION_TYPE_EUROPEAN = 'Européennes';
+const ELECTION_TYPE_MUNICIPAL = 'Municipales';
 
 const ELECTION_ROUND_FIRST = '1';
 const ELECTION_ROUND_SECOND = '2';
@@ -64,20 +59,20 @@ ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST] = '1er tour';
 ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND] = '2e tour';
 
 const ELECTIONS_LIST = [
-    'Européennes 2014',
-    `Départementales 2015 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
-    `Départementales 2015 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
-    `Régionales 2015 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
-    `Régionales 2015 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
-    `Présidentielles 2017 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
-    `Présidentielles 2017 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
-    `Législatives 2017 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
-    `Législatives 2017 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
-    'Européennes 2019',
-    `Municipales 2020 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
-    `Municipales 2020 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
-    `Régionales 2021 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
-    `Régionales 2021 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
+    `${ELECTION_TYPE_EUROPEAN} 2014`,
+    `${ELECTION_TYPE_DEPARTMENTAL} 2015 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
+    `${ELECTION_TYPE_DEPARTMENTAL} 2015 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
+    `${ELECTION_TYPE_REGIONAL} 2015 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
+    `${ELECTION_TYPE_REGIONAL} 2015 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
+    `${ELECTION_TYPE_PRESIDENTIAL} 2017 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
+    `${ELECTION_TYPE_PRESIDENTIAL} 2017 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
+    `${ELECTION_TYPE_LEGISLATIVE} 2017 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
+    `${ELECTION_TYPE_LEGISLATIVE} 2017 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
+    `${ELECTION_TYPE_EUROPEAN} 2019`,
+    `${ELECTION_TYPE_MUNICIPAL} 2020 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
+    `${ELECTION_TYPE_MUNICIPAL} 2020 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
+    `${ELECTION_TYPE_REGIONAL} 2021 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_FIRST]}`,
+    `${ELECTION_TYPE_REGIONAL} 2021 - ${ELECTION_ROUND_LABELS[ELECTION_ROUND_SECOND]}`,
 ];
 
 function Elections() {
@@ -88,6 +83,8 @@ function Elections() {
     const [currentPoint, setCurrentPoint] = useState();
     const [selectedElection, setSelectedElection] = useState('');
     const [participation, setParticipation] = useState([]);
+    const [results, setResults] = useState([]);
+    const [zone, setZone] = useState();
 
     // Display only the choosen layer
     const switchLayer = () => {
@@ -100,7 +97,7 @@ function Elections() {
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/larem/ckq7vwlpl8ott17n6jol1mvhd',
-            center: [2.3487871369502145, 48.85335278688857],
+            center: [2.213749, 46.227638],
             zoom: 5,
         });
     }, []);
@@ -133,20 +130,50 @@ function Elections() {
         }
 
         const propsFromMapbox = map.current.queryRenderedFeatures(currentPoint.point, { layers: [activeLayer] });
+        setZone(propsFromMapbox[0].properties.nom);
 
         const getParticipation = async () => {
             if (selectedElection !== '') {
                 try {
-                    const electionAndYear = selectedElection.substr(0, selectedElection.indexOf('-'));
-                    const getTour = (selectedElection.split('-')[1]).slice(1, 2);
+                    const electionAndYear = (selectedElection.substr(0, selectedElection.indexOf('-'))).trim();
+                    const getTour = ((selectedElection.split('-')[1]).slice(1, 2)).trim();
                     setParticipation(await apiClientProxy.get(`/election/participation?maillage=${activeLayer}&code_zone=${propsFromMapbox[0].properties.code}&election=${electionAndYear}&tour=${getTour}`));
+                    setResults(await apiClientProxy.get(`/election/results?maillage=${activeLayer}&code_zone=${propsFromMapbox[0].properties.code}&election=${electionAndYear}&tour=${getTour}`));
                 } catch (error) {
                     console.log(error);
                 }
             }
         };
         getParticipation();
-    }, [currentPoint, participation]);
+    }, [currentPoint]);
+
+    useEffect(() => {
+        const modalContent = document.getElementById('map-overlay');
+        if (participation.length > 0 && results.length > 0) {
+            modalContent.innerHTML = `
+                            <div class="elections-area">${zone}</div>
+                            <div class="election-name">${participation[0].election}</div>
+                            <div id="close-modal">x</div>
+                            <div class="flash-info">
+                                <div class="flash-div"><span class="flash-span">${participation[0].inscrits.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')} inscrits</span></div>
+                                <div class="flash-div">Votants: <span class="flash-span">${participation[0].votants.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}</span></div>
+                                <div class="flash-div">Votes exprimés: <span class="flash-span">${participation[0].exprimes.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}</span></div>
+                            </div>
+                            <div>
+                            <div>
+                                ${renderToString(results.sort((a, b) => b.voix - a.voix).map((element, i) => <ElectionModal key={i + 1} row={element} />))}
+                            </div>
+                            </div>
+                        `;
+        } else {
+            modalContent.innerHTML = `
+                            <div id="close-modal">x</div>
+                            <div class="no-data">
+                                <div class="flash-div"><span class="flash-span">Sélectionnez une élection</span></div>
+                            </div>
+                        `;
+        }
+    }, [participation, results]);
 
     return (
         <div>
