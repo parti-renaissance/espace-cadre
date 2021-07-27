c62ffd16186ea49f7000eddc1d9e1bd5d64a8ae6
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-07-27 16:15:43 +0200

    Handle error + progress bar

--- src/components/TextGenerator/TextGenerator.jsx
+++ src/components/TextGenerator/TextGenerator.jsx
@@ -1,45 +1,62 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, {
-    useState, useEffect, useRef,
+    useState, useEffect,
 } from 'react';
+import $ from 'jquery';
 import { useDebounce } from 'use-debounce';
 import { apiClientProxy } from '../../services/networking/client';
+import ErrorComponent from '../ErrorComponent/ErrorComponent';
 
 function TextGenerator() {
     const [text, setText] = useState('');
     const [textGenerated, setTextGenerated] = useState('Votre texte généré apparaîtra ici');
-    const myTextArea = useRef(null);
     const [debouncedSearchTerm] = useDebounce(text, 1000);
+    const [hasError, setHasError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const sendText = async () => {
-        if (debouncedSearchTerm !== '') {
-            setTextGenerated(JSON.stringify(await apiClientProxy.get(`/textGenerator?text=${debouncedSearchTerm}`)));
+        try {
+            if (debouncedSearchTerm !== '') {
+                setTextGenerated(JSON.stringify(await apiClientProxy.get(`/textGenerator?text=${debouncedSearchTerm}`)));
+                $('.bar').css('display', 'none');
+            }
+        } catch (error) {
+            console.log(error);
+            setHasError(true);
+            setErrorMessage(error);
         }
     };
+
     useEffect(() => {
         sendText();
     }, [debouncedSearchTerm]);
 
     return (
         <div className="container-fluid">
-            <div className="row">
-                <div className="col-12 col-lg-6 mb-3">
-                    <label htmlFor="myText">Votre texte:</label>
-                    <textarea
-                        className="form-control"
-                        id="myText"
-                        ref={myTextArea}
-                        rows="20"
-                        value={text}
-                        onChange={(e) => setText(e.target.value)}
-                        placeholder="Écrivez ici"
-                    />
+            {!hasError ? (
+                <div className="row">
+                    <div className="col-12 col-lg-6 mb-3">
+                        <label htmlFor="myText">Votre texte:</label>
+                        <textarea
+                            className="form-control"
+                            id="myText"
+                            rows="20"
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                            placeholder="Écrivez ici"
+                        />
+                    </div>
+                    <div className="col-12 col-lg-6">
+                        {debouncedSearchTerm && (
+                            <div className="bar mb-3">
+                                <div className="in" />
+                            </div>
+                        )}
+                        <label htmlFor="suggestedText">Texte suggéré:</label>
+                        <textarea className="form-control" id="suggestedText" rows="20" value={textGenerated} readOnly />
+                    </div>
                 </div>
-                <div className="col-12 col-lg-6">
-                    <label htmlFor="suggestedText">Texte suggéré:</label>
-                    <textarea className="form-control" id="suggestedText" rows="20" value={textGenerated} readOnly />
-                </div>
-            </div>
+            ) : <ErrorComponent errorMessage={errorMessage} />}
         </div>
     );
 }
