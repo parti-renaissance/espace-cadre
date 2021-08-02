70d4e65f23f9c928276b1f7f8dd68b32991d7578
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-08-02 19:01:48 +0200

    Update no match component style

--- src/components/NoMatch/NoMatch.jsx
+++ src/components/NoMatch/NoMatch.jsx
@@ -7,12 +7,17 @@ function NoMatch() {
     const location = useLocation();
 
     return (
-        <div className="with-background dc-container text-center p-3">
-            <span>
-                L&apos;URL recherchée {location.pathname} n&apos;existe pas ou vous n&apos;avez pas les droits pour y accéder
-            </span>
-            <Link to="/" className="btn btn-primary mt-3">Retournez à l&apos;accueil</Link>
+        <div className="container">
+            <div className="row with-background dc-container text-center p-3 ">
+                <div className="col-12">
+                    L&apos;URL recherchée <strong>{location.pathname}</strong> n&apos;existe pas ou vous n&apos;avez pas les droits pour y accéder
+                </div>
+                <div className="col-12">
+                    <Link to="/" className="btn btn-primary mt-3">Retournez à l&apos;accueil</Link>
+                </div>
+            </div>
         </div>
+
     );
 }
 
