5edb143c932c95bbcc9b50b0a8dc1201536f8c57
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-08-03 11:26:06 +0200

    Error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,32 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import { ErrorBoundary } from 'react-error-boundary';
 import { persistor, store } from './redux/store';
 import Root from './components/Root/Root';
 import Routes from './Routes';
+import Fallback from './components/ErrorComponent/Fallback';
 
 import './style/index.scss';
 
-const App = () => (
-    <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <BrowserRouter>
-                <Root>
-                    <Routes />
-                </Root>
-            </BrowserRouter>
-        </PersistGate>
-    </Provider>
-);
+const App = () => {
+    const errorHandler = (error, errorInfo) => {
+        console.log('Logging!!!', error, errorInfo);
+    };
+
+    return (
+        <ErrorBoundary FallbackComponent={Fallback} onError={errorHandler}>
+            <Provider store={store}>
+                <PersistGate loading={null} persistor={persistor}>
+                    <BrowserRouter>
+                        <Root>
+                            <Routes />
+                        </Root>
+                    </BrowserRouter>
+                </PersistGate>
+            </Provider>
+        </ErrorBoundary>
+    );
+};
 
 export default App;
