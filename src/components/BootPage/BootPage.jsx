166a0a3788533b74634dd037ffe7f3238fc21981
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-08-05 16:51:26 +0200

    Bootpage migration

--- src/components/BootPage/BootPage.jsx
+++ src/components/BootPage/BootPage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Container, Grid } from '@material-ui/core';
 
 function BootPage() {
     return (
-        <div className="container text-center">
-            <div className="col-12" style={{ margin: '48px 0' }}>
-                <h2>L&apos;application sera bientôt prête</h2>
-            </div>
-            <div className="col-12">
-                <img src="images/bootPage.svg" alt="" />
-            </div>
-        </div>
+        <Container className="container text-center">
+            <Grid container>
+                <Grid item xs={12} style={{ margin: '48px 0' }}>
+                    <h2>L&apos;application sera bientôt prête</h2>
+                </Grid>
+                <Grid item xs={12}>
+                    <img src="images/bootPage.svg" alt="loading" />
+                </Grid>
+            </Grid>
+        </Container>
     );
 }
 
