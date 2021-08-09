b7a4e06c423a841dbd74a0f030695113a1a8b6de
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-08-09 17:31:54 +0200

    Spinner migration

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 
 function Spinner() {
     return (
-        <div className="d-flex flex-column align-items-center justify-content-center" style={{ marginTop: 'calc(100vh - 70vh)' }}>
-            <div className="row mb-2">
-                <div className="spinner-border" role="status">
-                    <span className="sr-only">Loading...</span>
-                </div>
-            </div>
-            <div className="row">
+        <Grid container style={{ marginTop: 'calc(100vh - 70vh)', textAlign: 'center' }}>
+            <Grid item xs={12}>
+                <CircularProgress />
+            </Grid>
+            <Grid item xs={12}>
                 <strong>Page en cours de chargement</strong>
-            </div>
-        </div>
+            </Grid>
+        </Grid>
     );
 }
 
