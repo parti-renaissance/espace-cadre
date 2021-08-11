5e2ddd3e51dc161580de6efb1a5676633ebd8e0d
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-08-11 16:31:51 +0200

    Gender select + interests select

--- src/components/ContactsPageV2/FiltersBlock/FiltersBlock.jsx
+++ src/components/ContactsPageV2/FiltersBlock/FiltersBlock.jsx
@@ -1,45 +1,80 @@
 /* eslint-disable no-unused-expressions */
-import React from 'react';
+import React, { useState } from 'react';
 import {
-    Grid, FormControl, InputLabel, MenuItem, Select,
+    Grid, FormControl, InputLabel, MenuItem, Select, Input, Checkbox, ListItemText,
 } from '@material-ui/core';
 import { useColumnsTitleCache } from '../../../redux/contacts/hooks';
 
 function FiltersBlock() {
     const [columnsTitle] = useColumnsTitleCache();
     const filteredColumnsTitle = columnsTitle && columnsTitle.filter((columnTitle) => 'filter' in columnTitle);
+    const [gender, setGender] = useState([]);
+    const [interests, setInterests] = useState([]);
 
-    const filtersContent = () => {
-        filteredColumnsTitle && filteredColumnsTitle.map((column) => {
-            if (column.filter.type === 'select') {
-                console.log(column);
-                return (
-                    <FormControl variant="outlined">
-                        <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-outlined-label"
-                            id="demo-simple-select-outlined"
-                            // value={age}
-                            // onChange={handleChange}
-                            label="Age"
-                        >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                            <MenuItem value={10}>Ten</MenuItem>
-                            <MenuItem value={20}>Twenty</MenuItem>
-                            <MenuItem value={30}>Thirty</MenuItem>
-                        </Select>
-                    </FormControl>
-                );
-            }
-        });
+    const handleInterestsChange = (event) => {
+        setInterests(event.target.value);
     };
+
+    const handleGenderChange = (event) => {
+        setGender(event.target.value);
+    };
+
+    const filtersContent = filteredColumnsTitle && filteredColumnsTitle.map((column) => {
+        if (column.key === 'interests' && column.filter.type === 'select') {
+            const selectOptions = Object.keys(column.filter.options.choices);
+
+            return (
+                <FormControl className="contacts-select">
+                    <InputLabel id="contacts-mutiple-select">{column.label}</InputLabel>
+                    <Select
+                        labelId="contacts-mutiple-select"
+                        id="contacts-mutiple-checkbox"
+                        multiple
+                        value={interests}
+                        onChange={handleInterestsChange}
+                        input={<Input />}
+                        renderValue={(selected) => selected.join(', ')}
+                    >
+                        {selectOptions.map((option) => (
+                            <MenuItem key={option} value={option}>
+                                <Checkbox checked={interests.indexOf(option) > -1} />
+                                <ListItemText primary={option} />
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
+            );
+        }
+        if (column.filter.type === 'select') {
+            const selectOptions = Object.values(column.filter.options.choices);
+            return (
+                <FormControl className="contacts-select">
+                    <InputLabel id="contacts-single-select">{column.label}</InputLabel>
+                    <Select
+                        labelId="contacts-single-select"
+                        id="contacts-single-select-outlined"
+                        value={gender}
+                        onChange={handleGenderChange}
+                        label={column.label}
+                    >
+                        {
+                            selectOptions.map((option, index) => (
+                                <MenuItem value={option} key={index}>
+                                    <em>{option.charAt(0).toUpperCase() + option.slice(1)}</em>
+                                </MenuItem>
+                            ))
+                        }
+
+                    </Select>
+                </FormControl>
+            );
+        }
+    });
 
     return (
         <Grid item xs={12} className="with-background dc-container filters-block-container">
             <div>
-                {filtersContent()}
+                {filtersContent}
             </div>
         </Grid>
     );
