c96f51643d02e6310b5c82547d49e320525d8624
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-08-11 18:17:45 +0200

    Error handling

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,19 +1,24 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Grid } from '@material-ui/core';
 import { apiClient } from '../../services/networking/client';
 import { useColumnsTitleCache } from '../../redux/contacts/hooks';
 import FiltersBlock from './FiltersBlock';
 import TableHeadComponent from './TableHeadComponent/TableHeadComponent';
+import ErrorComponent from '../ErrorComponent';
+import Loader from '../Loader';
 
 function Contacts() {
     const [columnsTitle, setColumnsTitle] = useColumnsTitleCache();
+    const [hasError, setHasError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const getColumnsTitle = async () => {
         try {
             setColumnsTitle(await apiClient.get('v3/adherents/columns'));
         } catch (error) {
-            console.log(error);
+            setHasError(true);
+            setErrorMessage(error);
         }
     };
 
@@ -21,12 +26,30 @@ function Contacts() {
         getColumnsTitle();
     }, []);
 
+    const ContactsContent = () => {
+        if (columnsTitle.length > 0) {
+            return (
+                <>
+                    <FiltersBlock columnsTitle={columnsTitle} />
+                    <Grid container>
+                        <TableHeadComponent columnsTitle={columnsTitle} />
+                    </Grid>
+                </>
+            );
+        }
+        if (hasError) {
+            return <ErrorComponent errorMessage={errorMessage} />;
+        }
+        return (
+            <div style={{ textAlign: 'center' }} className="with-background dc-container">
+                <Loader />
+            </div>
+        );
+    };
+
     return (
         <Container maxWidth="xl" className="contacts-container">
-            <FiltersBlock columnsTitle={columnsTitle} />
-            <Grid container>
-                <TableHeadComponent columnsTitle={columnsTitle} />
-            </Grid>
+            {ContactsContent()}
         </Container>
     );
 }
