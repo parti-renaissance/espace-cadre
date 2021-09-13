bf872a9a8ebe5b049d38f44bb3a64dea4205acb9
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-09-13 14:07:45 +0200

    Introduce MUI theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
+import { ThemeProvider } from '@material-ui/core/styles';
 import { persistor, store } from './redux/store';
 import Layout from './components/Layout';
 import Routes from './Routes';
 
 import './style/index.scss';
 
+import { theme } from './stylesheet';
+
 const App = () => (
     <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
             <BrowserRouter>
-                <Layout>
-                    <Routes />
-                </Layout>
+                <ThemeProvider theme={theme}>
+                    <Layout>
+                        <Routes />
+                    </Layout>
+                </ThemeProvider>
             </BrowserRouter>
         </PersistGate>
     </Provider>
---
+++ src/stylesheet.jsx
@@ -0,0 +1,37 @@
+import { createTheme } from '@material-ui/core';
+
+export const colorPalette = {
+// White
+    whiteCorner: '#fff',
+
+    // Black
+    blackCorner: '#1A334D',
+
+    // Blue
+    blueCorner: '#0049C6',
+    blue2Corner: '#135CEB',
+    blueCornerHover: '#1766ff',
+    messagerieButtonHover: '#1f5bc2',
+    blueBubble: '#D9EAFF',
+
+    // Grey
+    greyCornerBg: '#F0F1F3',
+    greyCorner3: '#717BA0',
+    mentionsLegales: '#B0B6C9',
+    interestsBubble: '#EFF1F3',
+    gray100: '#F3F4F6',
+    gray200: '#E5E7EB',
+    gray300: '#D1D5DB',
+    gray400: '#9CA3AF',
+    gray500: '#6B7280',
+    gray600: '#4B5563',
+    gray700: '#374151',
+    gray800: '#1F2937',
+
+    // Green
+    greenCorner: '#008000',
+};
+
+export const theme = createTheme({
+    colorPalette,
+});
