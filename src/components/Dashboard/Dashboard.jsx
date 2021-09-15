52e41b86b3704b96810d8b33a2fb4ca7f2a1c06b
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-09-15 17:11:56 +0200

    MUI update in several files

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,35 +1,62 @@
 import React from 'react';
-import { Container, Grid } from '@material-ui/core';
+import {
+    Container, Grid, makeStyles, createStyles,
+} from '@material-ui/core';
 import ActiveUsers from './Charts/ActiveUsers/ActiveUsers';
 import DownloadsCount from './Charts/DownloadsCount/DownloadsCount';
 import KpiEmailCampaign from './Charts/KpiEmailCampaign/KpiEmailCampaign';
 import MapComponent from './Map/MapComponent';
 import TextChart from './Charts/TextChart/TextChart';
 
-const Dashboard = () => (
-    <Container maxWidth="lg">
-        <Grid container>
-            <TextChart />
-            <KpiEmailCampaign />
-            <Grid item xs={12}>
-                <p className="title"><img src="images/smartphone.svg" alt="smartphone-logo" />Application mobile</p>
-            </Grid>
-            <Grid container spacing={2} style={{ marginBottom: '8px' }}>
-                <Grid item xs={12} lg={6}>
-                    <Grid item className="with-background dc-container downloadsCount-grid">
-                        <DownloadsCount />
+const useStyles = makeStyles((theme) => createStyles({
+    mainContainer: {
+        marginBottom: '16px',
+    },
+    title: {
+        fontSize: '20px',
+        fontWeight: '700',
+        color: theme.colorPalette.grayCorner3,
+    },
+    phoneImg: {
+        verticalAlign: 'middle',
+        marginRight: '8px',
+    },
+    kpiContainer: {
+        marginBottom: '8px',
+    },
+}));
+
+const Dashboard = () => {
+    const classes = useStyles();
+
+    return (
+        <Container maxWidth="lg" className={classes.mainContainer}>
+            <Grid container>
+                <TextChart />
+                <KpiEmailCampaign />
+                <Grid item xs={12}>
+                    <p className={classes.title}>
+                        <img src="images/smartphone.svg" alt="smartphone-logo" className={classes.phoneImg} />
+                        <span>Application mobile</span>
+                    </p>
+                </Grid>
+                <Grid container spacing={2} className={classes.kpiContainer}>
+                    <Grid item xs={12} lg={6}>
+                        <Grid item className="with-background dc-container">
+                            <DownloadsCount />
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} lg={6}>
+                        <Grid item className="with-background dc-container">
+                            <ActiveUsers />
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Grid item xs={12} lg={6}>
-                    <Grid item className="with-background dc-container">
-                        <ActiveUsers />
-                    </Grid>
+                <Grid item xs={12} className="with-background dc-container">
+                    <MapComponent />
                 </Grid>
             </Grid>
-            <Grid item xs={12} className="with-background dc-container" style={{ marginBottom: '16px' }}>
-                <MapComponent />
-            </Grid>
-        </Grid>
-    </Container>
-);
+        </Container>
+    );
+};
 export default Dashboard;
