c0af78b9d6e88924cbf577610256c42a26607112
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-09-21 15:59:04 +0200

    Update textField factory

--- src/components/Filters/FiltersFactory/Factory/TextFactory.jsx
+++ src/components/Filters/FiltersFactory/Factory/TextFactory.jsx
@@ -3,13 +3,10 @@ import React from 'react';
 import { TextField, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        border: 'none',
-    },
     filterBasicStyle: {
-        height: '39px',
         background: theme.palette.whiteCorner,
         borderRadius: '8px',
+        width: '100%',
     },
 }));
 
@@ -23,10 +20,10 @@ class TextFactory {
 
         return (
             <TextField
-                placeholder={filter.label}
                 variant="outlined"
-                value={value}
                 size="medium"
+                label={filter.label}
+                value={value}
                 onChange={(e) => onChange(e.target.value)}
                 className={classes.filterBasicStyle}
                 classes={{ root: classes.root }}
