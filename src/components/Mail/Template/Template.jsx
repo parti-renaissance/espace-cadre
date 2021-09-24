2bd613133913c3510c2bc0d1ddade3e9d5d30135
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-09-24 11:20:08 +0200

    Add filters block

--- src/components/Mail/Template/Template.jsx
+++ src/components/Mail/Template/Template.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from '@material-ui/core';
-import Filters from './Filters';
 import SendForm from './SendForm';
 import TemplateSelect from './TemplateSelect';
 import Editor from './Editor';
+import DynamicFilters from '../../Filters/DynamicFilters';
+import { FEATURE_MESSAGES } from '../../Feature/FeatureCode';
 
-const Template = () => (
-    <Container maxWidth="xl">
-        <Filters />
-        <SendForm />
-        <TemplateSelect />
-        <Editor />
-    </Container>
-);
+const Template = () => {
+    const [filters, setFilters] = useState({});
+
+    return (
+        <Container maxWidth="xl">
+            <DynamicFilters
+                feature={FEATURE_MESSAGES}
+                values={filters}
+                onSubmit={(newFilters) => setFilters({ ...newFilters, ...{ page: 1 } })}
+                onReset={() => { setFilters({ page: 1 }); }}
+            />
+            <SendForm />
+            <TemplateSelect />
+            <Editor />
+        </Container>
+    );
+};
 
 export default Template;
