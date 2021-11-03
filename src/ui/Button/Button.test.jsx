fb56e173ac436e8ed4565d9b01a73f031f777f7c
jb cazaux <jbcazaux@gmail.com>
2021-11-03 15:00:36 +0100

    eject

--- src/ui/Button/Button.test.jsx
+++ src/ui/Button/Button.test.jsx
@@ -1,20 +1,21 @@
+import React from 'react'
 import { render } from '@testing-library/react'
 import Button from './Button'
 
 jest.mock('@material-ui/core', () => ({
-    Button: ({ children }) => <div className="mui-button-mock">{ children }</div>,
-    makeStyles: () => () => ({}),
+  Button: ({ children, ...props }) => React.createElement('div', props, children),
+  makeStyles: () => () => ({}),
 }))
 
 describe('Button', () => {
-    it('displays button', () => {
-        const mock = jest.fn()
-        const { container } = render(
-            <Button buttonClasses="fooClass" handleClick={mock}>
-                Foo
-            </Button>,
-        )
+  it('displays button', () => {
+    const mock = jest.fn()
+    const { container } = render(
+      <Button buttonClasses="fooClass" handleClick={mock}>
+        Foo
+      </Button>
+    )
 
-        expect(container).toMatchSnapshot()
-    })
+    expect(container).toMatchSnapshot()
+  })
 })
