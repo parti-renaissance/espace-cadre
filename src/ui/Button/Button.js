649760fe31641797e67532999fd38b1f5ceec8ba
jean-baptiste cazaux <jbcazaux@gmail.com>
2021-11-03 16:36:58 +0100

    Chore/remove cra 2 (#522)
    * eject CRA !
    
    * fix .env

---
+++ src/ui/Button/Button.js
@@ -0,0 +1,39 @@
+import { Button as MuiButton, makeStyles, createStyles } from '@material-ui/core'
+import PropTypes from 'prop-types'
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      borderRadius: '8.35px',
+      padding: theme.spacing(1, 2),
+    },
+  })
+)
+
+const Button = ({ children, buttonClasses, handleClick, disabled }) => {
+  const classes = useStyles()
+
+  return (
+    <MuiButton
+      variant="contained"
+      className={`${classes.root} ${buttonClasses}`}
+      onClick={handleClick}
+      disabled={disabled}
+    >
+      {children}
+    </MuiButton>
+  )
+}
+
+export default Button
+
+Button.defaultProp = {
+  disabled: false,
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  buttonClasses: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+}
