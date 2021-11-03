778f8decc80283d8e90d3f78a195b7f6888270f3
jb cazaux <jbcazaux@gmail.com>
2021-11-03 14:23:50 +0100

    open navigator on yarn start

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = (env, argv = {}) => {
     devtool: argv.mode === 'development' ? 'source-map' : false,
     devServer: {
       static: path.resolve(__dirname, 'public'),
-      open: false,
+      open: true,
       port: 3000,
       historyApiFallback: true,
     },
