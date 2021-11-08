52019f4120ad8dfc6b6a865b984a0508efc1c7d6
jb cazaux <jbcazaux@gmail.com>
2021-11-08 17:16:20 +0100

    typo

--- src/api/filters.js
+++ src/api/filters.js
@@ -1,7 +1,7 @@
 import { apiClient } from 'services/networking/client'
 
-export const getFilters = async (id, cb) => {
-  const filters = await apiClient.get(`/v3/adherent_messages/${id}`)
+export const getFilters = async (feature, cb) => {
+  const filters = await apiClient.get(`v3/adherents/filters?feature=${feature}`)
   cb && cb(filters)
   return filters
 }
