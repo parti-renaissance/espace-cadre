60a343b3f7dd0c5dadf6d6e39b58efe3da386a28
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-11-22 11:03:48 +0100

    Update UICard snapshot

--- src/ui/UICard/UICard.test.js
+++ src/ui/UICard/UICard.test.js
@@ -1,9 +1,12 @@
 import { render } from '@testing-library/react'
 import UICard from './UICard'
 
-jest.mock('@mui/styles', () => ({
+jest.mock('@mui/material', () => ({
   Grid: ({ children }) => <div className="mui-grid-mock">{children}</div>,
   Paper: ({ children }) => <div className="mui-paper-mock">{children}</div>,
+}))
+
+jest.mock('@mui/styles', () => ({
   makeStyles: () => () => ({ root: 'root', container: 'container', title: 'title', creator: 'creator' }),
 }))
 
