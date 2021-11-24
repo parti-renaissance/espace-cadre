a8e868be5acfdf9e85d6e02cfed126a905cab258
jb cazaux <jbcazaux@gmail.com>
2021-11-24 11:01:27 +0100

    fix test

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux'
 import { reducer as auth } from '../auth'
 import { reducer as dashboard } from '../dashboard'
-import { reducer as adherents } from '../adherents'
 import { reducer as errors } from '../errors'
 
 const rootReducer = combineReducers({
   auth,
   dashboard,
-  adherents,
   errors,
 })
 
