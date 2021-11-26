5823eadba1a30b57bf7f1e64ffc267a883df8ea8
jb cazaux <jbcazaux@gmail.com>
2021-11-26 17:34:45 +0100

    clean code

--- src/components/Dashboard/Charts/SentEmailCampaignList/SentEmailCampaignList.js
+++ src/components/Dashboard/Charts/SentEmailCampaignList/SentEmailCampaignList.js
@@ -11,58 +11,6 @@ import { generatePath, useNavigate } from 'react-router-dom'
 import paths from 'components/Messagerie/shared/paths'
 
 const useStyles = makeStyles(theme => ({
-  bigCard: {
-    marginBottom: theme.spacing(2),
-    background: theme.palette.whiteCorner,
-    borderRadius: '6px',
-    boxShadow: '0 1px 1px 0 rgba(0, 0, 0, .04)',
-  },
-  titleRow: {
-    padding: theme.spacing(0, 2),
-  },
-  headline: {
-    color: theme.palette.blackCorner,
-    fontSize: '16px',
-    fontWeight: '600',
-    marginBottom: 0,
-  },
-  subtitle: {
-    color: theme.palette.grayCorner3,
-    fontSize: '12px',
-    fontWeight: '400',
-    margin: 0,
-  },
-  cardRow: {
-    padding: theme.spacing(2),
-  },
-  card: {
-    '&:not(:last-child)': {
-      paddingRight: theme.spacing(1),
-    },
-  },
-  cardItem: {
-    padding: theme.spacing(2, 2, 2, 0),
-    borderRadius: '6px',
-    [theme.breakpoints.up('sm')]: {
-      border: `solid 1px ${theme.palette.grayCornerBg}`,
-      paddingLeft: theme.spacing(2),
-    },
-  },
-  infoNumber: {
-    color: theme.palette.blueCorner,
-    fontSize: '16px',
-    fontWeight: 600,
-  },
-  parentheseInfo: {
-    fontWeight: 400,
-    color: theme.palette.grayCorner3,
-    marginLeft: theme.spacing(0.25),
-  },
-  text: {
-    fontSize: '12px',
-    fontWeight: '400',
-    color: theme.palette.grayCorner3,
-  },
   noData: {
     textAlign: 'center',
     padding: theme.spacing(0.75),
@@ -70,11 +18,7 @@ const useStyles = makeStyles(theme => ({
 }))
 
 const messages = {
-  email: 'Email',
-  opening: 'Ouvertures',
-  clicks: 'Clics',
-  unsubscribing: 'Désabonnements',
-  noCampaign: 'Aucune campagne à afficher',
+  nocampaign: 'Aucune campagne à afficher',
 }
 
 const SentEmailCampaignList = () => {
@@ -105,7 +49,7 @@ const SentEmailCampaignList = () => {
     return (
       <>
         <SentEmailCampaignListTitle />
-        <UIContainer rootClasses={classes.noData}>Aucune campagne à afficher</UIContainer>
+        <UIContainer rootClasses={classes.noData}>{messages.nocampaign}</UIContainer>
       </>
     )
   }
@@ -125,59 +69,6 @@ const SentEmailCampaignList = () => {
           </UICard>
         ))}
       </Grid>
-
-      {/*emailCampaignReports.data.map(message => {
-        const { statistics: stats } = message
-        return (
-          <Grid container className={classes.bigCard} key={message.id}>
-            <Grid container className={classes.titleRow}>
-              <Grid item xs={12}>
-                <p className={classes.headline}>{message.subject}</p>
-              </Grid>
-              <Grid item xs={12}>
-                <p className={classes.subtitle}>
-                  Le {format(message.createdAt, 'dd/MM/yyyy')}, par {message.author}
-                </p>
-              </Grid>
-            </Grid>
-            <Grid container className={classes.cardRow}>
-              <Grid item xs={5} sm={3} className={classes.card}>
-                <Grid item className={classes.cardItem}>
-                  <div className={classes.infoNumber}>{stats?.sent}</div>
-                  <div className={classes.text}>Email{stats?.sent > 1 && 's'}</div>
-                </Grid>
-              </Grid>
-              <Grid item xs={5} sm={3} className={classes.card}>
-                <Grid item className={classes.cardItem}>
-                  <div className={classes.infoNumber}>
-                    <Percentage>{stats.openRate}</Percentage>
-                    <span className={classes.parentheseInfo}>({stats.opens})</span>
-                  </div>
-                  <div className={classes.text}>Ouvertures</div>
-                </Grid>
-              </Grid>
-              <Grid item xs={5} sm={3} className={classes.card}>
-                <Grid item className={classes.cardItem}>
-                  <div className={classes.infoNumber}>
-                    <Percentage>{stats.clickRate}</Percentage>
-                    <span className={classes.parentheseInfo}>({stats.clicks})</span>
-                  </div>
-                  <div className={classes.text}>Clics</div>
-                </Grid>
-              </Grid>
-              <Grid item xs={5} sm={3} className={classes.card}>
-                <Grid item className={classes.cardItem}>
-                  <div className={classes.infoNumber}>
-                    <Percentage>{stats.unsubscribeRate}</Percentage>
-                    <span className={classes.parentheseInfo}>({stats.unsubscribes})</span>
-                  </div>
-                  <div className={classes.text}>Désabonnements</div>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        )
-      })*/}
     </>
   )
 }
