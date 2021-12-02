c104a4e252ff275f3e7b46f3c78f84b307324774
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-12-02 15:55:38 +0100

    Refacto MapComponent

--- src/components/Dashboard/Map/MapComponent.js
+++ src/components/Dashboard/Map/MapComponent.js
@@ -1,48 +1,60 @@
 import { useState, useEffect } from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet'
-import { Grid, Box } from '@mui/material'
-import { makeStyles } from '@mui/styles'
+import { MapContainer as LeafletContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Grid, Typography } from '@mui/material'
+import { styled } from '@mui/system'
 import { useDashboardSurveyCache } from '../../../redux/dashboard/hooks'
+import { useUserScope } from '../../../redux/user/hooks'
 import { apiClientProxy } from 'services/networking/client'
 import Loader from 'ui/Loader'
-import { useUserScope } from '../../../redux/user/hooks'
 import ErrorComponent from '../../ErrorComponent/ErrorComponent'
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    padding: theme.spacing(2),
-  },
-  leafletContainer: {
-    width: '100%',
-    height: '500px',
-    borderRadius: '0 0 6px 6px',
-  },
-  countBubble: {
-    color: theme.palette.blueCorner,
-    fontWeight: '600',
-    fontSize: '18px',
-    backgroundColor: theme.palette.blueBubble,
-    padding: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    borderRadius: '6px',
-  },
-  chartTitle: {
-    color: theme.palette.blackCorner,
-    fontWeight: '600',
-  },
-  chartSubtitle: {
-    color: theme.palette.grayCorner3,
-    fontSize: '12px',
-    fontWeight: '400',
-  },
-}))
+const MapComponentHeader = styled(Grid)(
+  ({ theme }) => `
+  padding: ${theme.spacing(2)}
+`
+)
+
+const CountBubble = styled('span')(
+  ({ theme }) => `
+  font-size: 18px;
+  font-weight: 600;
+  color: ${theme.palette.blueCorner};
+  background-color: ${theme.palette.blueBubble};
+  padding: ${theme.spacing(1)};
+  margin-right: ${theme.spacing(1)};
+  border-radius: 6px;
+`
+)
+
+const SurveyCount = styled(Typography)(
+  ({ theme }) => `
+  font-weight: 600;
+  color: ${theme.palette.blackCorner};
+`
+)
+
+const Subtitle = styled(Typography)(
+  ({ theme }) => `
+  font-size: 12px;
+  font-weight: 400;
+  color: ${theme.palette.grayCorner3};
+`
+)
+
+const MapContainer = styled(LeafletContainer)(
+  () => `
+  width: 100%;
+  height: 500px;
+  border-radius: 0 0 6px 6px;
+`
+)
 
 function MapComponent() {
-  const classes = useStyles()
   const [dashboardSurvey, setDashboardSurvey] = useDashboardSurveyCache()
   const [currentScope] = useUserScope()
   const [errorMessage, setErrorMessage] = useState()
+  const { survey_datas, latitude, longitude } = dashboardSurvey || {}
 
   useEffect(() => {
     const getSurvey = async () => {
@@ -59,31 +71,25 @@ function MapComponent() {
 
   L.Icon.Default.imagePath = '/'
 
-  const dashboardSurveyContent = () => {
-    if (dashboardSurvey) {
-      return (
+  return (
+    <>
+      {dashboardSurvey ? (
         <>
-          <Grid container className={classes.container}>
-            <span className={classes.countBubble}>{dashboardSurvey.survey_datas.length}</span>
-            <Grid item>
-              <Box className={classes.chartTitle}>
-                Questionnaire{dashboardSurvey.survey_datas.length > 1 && 's'} rempli
-                {dashboardSurvey.survey_datas.length > 1 && 's'}
-              </Box>
-              <Box className={classes.chartSubtitle}>Répartition géographique dans votre région</Box>
-            </Grid>
-          </Grid>
-          <MapContainer
-            center={[dashboardSurvey.latitude, dashboardSurvey.longitude]}
-            zoom={8}
-            className={classes.leafletContainer}
-          >
+          <MapComponentHeader container>
+            <CountBubble>{survey_datas.length}</CountBubble>
+            <SurveyCount>
+              Questionnaire{survey_datas.length > 1 && 's'} rempli
+              {survey_datas.length > 1 && 's'}
+            </SurveyCount>
+            <Subtitle>Répartition géographique dans votre région</Subtitle>
+          </MapComponentHeader>
+          <MapContainer center={[latitude, longitude]} zoom={8}>
             <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
             />
 
-            {dashboardSurvey.survey_datas.map(data => (
+            {survey_datas.map(data => (
               <Marker key={data.id} position={[data.latitude, data.longitude]}>
                 <Popup>
                   <strong>Nom du sondage:</strong> {data.data_survey.survey.name} <br />
@@ -93,19 +99,14 @@ function MapComponent() {
             ))}
           </MapContainer>
         </>
-      )
-    }
-    if (errorMessage) {
-      return <ErrorComponent errorMessage={errorMessage} />
-    }
-    return (
-      <Box style={{ textAlign: 'center' }}>
-        <Loader />
-      </Box>
-    )
-  }
-
-  return <>{dashboardSurveyContent()}</>
+      ) : (
+        <div xs={{ textAlign: 'center' }}>
+          <Loader />
+        </div>
+      )}
+      {errorMessage && <ErrorComponent errorMessage={errorMessage} />}
+    </>
+  )
 }
 
 export default MapComponent
