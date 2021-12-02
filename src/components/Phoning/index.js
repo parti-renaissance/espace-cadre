422b16ba6d4c70905020ed336189381f862dc9a6
CoZil <nicolas7even@gmail.com>
2021-12-02 14:14:29 -0500

    [EM-1719] Phoning campaign route

--- src/components/Phoning/index.js
+++ src/components/Phoning/index.js
@@ -1,1 +1,12 @@
-export { default } from './Phoning'
+import { Route, Routes } from 'react-router-dom'
+import Phoning from './Phoning'
+import PhoningCampaign from './PhoningCampaign'
+
+const PhoningRoute = () => (
+  <Routes>
+    <Route path="" element={<Phoning />} />
+    <Route path=":campaignId" element={<PhoningCampaign />} />
+  </Routes>
+)
+
+export default PhoningRoute
