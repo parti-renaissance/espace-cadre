47c73edc93c3a94ee6d11d964e371b8ac336fbcc
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2021-12-06 18:27:21 +0100

    Display Kpi

--- src/components/Phoning/Phoning.js
+++ src/components/Phoning/Phoning.js
@@ -1,9 +1,10 @@
-import { Container, Grid, Paper, Card, CardHeader, CardContent, Typography } from '@mui/material'
+import { Container, Grid, Paper, Card, CardContent, Typography } from '@mui/material'
 import { styled } from '@mui/system'
 import PageHeader from 'ui/PageHeader'
 import { useErrorHandler } from 'components/shared/error/hooks'
 import { useQuery } from 'react-query'
 import { getGlobalKpiQuery } from 'api/phoning'
+import pluralize from 'components/shared/pluralize/pluralize'
 
 const CardWrapper = styled(props => <Grid item {...props} />)`
   flex-grow: 1;
@@ -17,9 +18,40 @@ const Title = styled(Typography)(
 `
 )
 
+const Kpi = styled(Typography)(
+  ({ theme }) => `
+	color: ${theme.palette.indigo700};
+	font-size: 28px;
+	font-weight: 600px;
+`
+)
+
+const KpiTitle = styled(Typography)(
+  ({ theme }) => `
+	color: ${theme.palette.gray900};
+	font-size: 16px;
+	font-weight: 600px;
+`
+)
+
+const SecondaryKpi = styled(Typography)(
+  ({ theme }) => `
+	color: ${theme.palette.gray600};
+	font-size: 12px;
+	font-weight: 400px;
+`
+)
+
 const messages = {
   title: 'Phoning',
   actionButtonText: 'Créer une campagne',
+  kpiContainerTitle: 'Indicateurs',
+  campaign: 'Campagne',
+  survey: 'Questionnaire',
+  call: 'Appel',
+  lastMonth: 'sur un mois',
+  ongoingCampaignsPrefix: 'Dont',
+  ongoingCampaignsSuffix: 'en cours',
 }
 
 const Phoning = () => {
@@ -40,17 +72,48 @@ const Phoning = () => {
       </Grid>
       <Paper sx={{ p: 2, background: '#E5E7EB', borderRadius: '12px' }}>
         <Grid container>
-          <Title>Indicateurs</Title>
+          <Title>{messages.kpiContainerTitle}</Title>
         </Grid>
         <Grid container spacing={2}>
-          {/* {[...Array(5).keys()].map(index => (
-            <CardWrapper key={index + 1} lg={2.4} xl={2.4}>
-              <Card>
-                <CardHeader title="Title" />
-                <CardContent>Lorem ipsum fluctuat nec mergitur</CardContent>
-              </Card>
-            </CardWrapper>
-          ))} */}
+          <CardWrapper lg={2.4} xl={2.4}>
+            <Card>
+              <CardContent>
+                <Kpi>{globalKpi.campaignCount}</Kpi>
+                <br />
+                <KpiTitle>{pluralize(globalKpi.campaignCount, messages.campaign)}</KpiTitle>
+                <br />
+                <SecondaryKpi>
+                  {messages.ongoingCampaignsPrefix} {globalKpi.ongoingCampaigns} {messages.ongoingCampaignsSuffix}
+                </SecondaryKpi>
+              </CardContent>
+            </Card>
+          </CardWrapper>
+          <CardWrapper lg={2.4} xl={2.4}>
+            <Card>
+              <CardContent>
+                <Kpi>{globalKpi.surveysCount}</Kpi>
+                <br />
+                <KpiTitle>{pluralize(globalKpi.surveysCount, messages.survey)}</KpiTitle>
+                <br />
+                <SecondaryKpi>
+                  {globalKpi.lastMonthSurveysCount} {messages.lastMonth}
+                </SecondaryKpi>
+              </CardContent>
+            </Card>
+          </CardWrapper>
+          <CardWrapper lg={2.4} xl={2.4}>
+            <Card>
+              <CardContent>
+                <Kpi>{globalKpi.callsCount}</Kpi>
+                <br />
+                <KpiTitle>{pluralize(globalKpi.callsCount, messages.call)}</KpiTitle>
+                <br />
+                <SecondaryKpi>
+                  {globalKpi.lastMonthCallsCount} {messages.lastMonth}
+                </SecondaryKpi>
+              </CardContent>
+            </Card>
+          </CardWrapper>
         </Grid>
       </Paper>
     </Container>
