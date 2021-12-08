8af1fce8d4f455b495a7d50ecf95deac030e81ca
jb cazaux <jbcazaux@gmail.com>
2021-12-08 01:50:27 +0100

    add tests

---
+++ src/ui/Card/Title/Title.test.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react'
+import { Title } from 'ui/Card/Title/Title'
+
+jest.mock('@mui/system', () => ({
+  styled: c => () => c,
+}))
+jest.mock('@mui/material', () => ({
+  Typography: ({ children }) => <div className="mui-typography-mock">{children}</div>,
+}))
+describe('Title', () => {
+  it('displays a Title', () => {
+    const { container } = render(<Title subject="subject" author="author" />)
+    expect(container).toMatchSnapshot()
+  })
+})
