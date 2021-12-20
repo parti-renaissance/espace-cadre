4d80b46450282d13eae76aa4bef99fe685cf01a1
jb cazaux <jbcazaux@gmail.com>
2021-12-20 12:06:05 +0100

    do not use template with error

--- src/components/Messagerie/Component/Editor.js
+++ src/components/Messagerie/Component/Editor.js
@@ -6,6 +6,11 @@ import { useParams } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { useUserScope } from '../../../redux/user/hooks'
 import { getMessageContent } from 'api/messagerie'
+import { useQuery } from 'react-query'
+import { useErrorHandler } from 'components/shared/error/hooks'
+import { useCustomSnackbar } from 'components/shared/notification/hooks'
+import { notifyVariants } from 'components/shared/notification/constants'
+import UIFormMessage from 'ui/FormMessage'
 
 const downloadHtml = html => {
   const file = new Blob([html], { type: 'text/html' })
@@ -15,6 +20,10 @@ const downloadHtml = html => {
   a.click()
 }
 
+const messages = {
+  errorTemplate: 'Erreur au chargement du template',
+  errorTemplateRecreate: 'Template non conforme, veuillez en créer un nouveau.',
+}
 const useStyles = makeStyles(theme => ({
   emailEditor: {
     marginBottom: theme.spacing(2),
@@ -56,10 +65,13 @@ const editorConfiguration = {
 }
 const Editor = ({ onMessageSubject, onMessageUpdate }) => {
   const [editorLoaded, setEditorLoaded] = useState(false)
+  const [messageContentError, setMessageContentError] = useState(false)
   const emailEditorRef = useRef(null)
   const classes = useStyles()
   const { messageUuid } = useParams()
   const [currentScope] = useUserScope()
+  const { handleError } = useErrorHandler()
+  const { enqueueSnackbar } = useCustomSnackbar()
 
   const [templateId] = useState(() => {
     if (currentScope?.code === 'referent') {
@@ -83,19 +95,33 @@ const Editor = ({ onMessageSubject, onMessageUpdate }) => {
     })
   }, [onMessageUpdate])
 
+  const { data: messageContent = null } = useQuery(
+    ['messageContent', messageUuid],
+    () => getMessageContent(messageUuid),
+    { onError: handleError, enabled: editorLoaded }
+  )
+
   useEffect(() => {
     const { editor } = emailEditorRef.current
-    const onEditorLoaded = async () => {
-      if (messageUuid) {
-        const messageContent = await getMessageContent(messageUuid)
+    if (messageContent) {
+      onMessageSubject(messageContent.subject)
+      if (messageContent.json_content) {
         const design = JSON.parse(messageContent.json_content)
         editor.loadDesign(design)
-        onMessageSubject(messageContent.subject)
         onMessageUpdate({
           design: design,
           chunks: { body: messageContent.content },
         })
+      } else {
+        setMessageContentError(true)
+        enqueueSnackbar(messages.errorTemplate, notifyVariants.error)
       }
+    }
+  }, [enqueueSnackbar, messageContent, onMessageSubject, onMessageUpdate])
+
+  useEffect(() => {
+    const { editor } = emailEditorRef.current
+    const onEditorLoaded = () => {
       editor.addEventListener('design:updated', updateMessageTemplateCallback)
     }
 
@@ -106,7 +132,7 @@ const Editor = ({ onMessageSubject, onMessageUpdate }) => {
     return () => {
       editor?.removeEventListener('design:updated', updateMessageTemplateCallback)
     }
-  }, [editorLoaded, messageUuid, onMessageSubject, onMessageUpdate, updateMessageTemplateCallback])
+  }, [editorLoaded, updateMessageTemplateCallback])
 
   const exportHtml = () => {
     emailEditorRef.current.editor.exportHtml(data => {
@@ -116,22 +142,28 @@ const Editor = ({ onMessageSubject, onMessageUpdate }) => {
 
   return (
     <div className={classes.emailEditor}>
-      <EmailEditor
-        minHeight="85vh"
-        ref={emailEditorRef}
-        projectId={process.env.REACT_APP_UNLAYER_PROJECT_ID}
-        onLoad={() => setEditorLoaded(true)}
-        options={{
-          locale: 'fr-FR',
-          safeHtml: true,
-          templateId: messageUuid ? null : templateId,
-          tools: editorConfiguration.tools,
-          features: editorConfiguration.features,
-        }}
-      />
-      <Button variant="contained" size="medium" className={classes.exportButton} onClick={exportHtml}>
-        Export HTML
-      </Button>
+      {messageContentError ? (
+        <UIFormMessage severity="error">{messages.errorTemplateRecreate}</UIFormMessage>
+      ) : (
+        <>
+          <EmailEditor
+            minHeight="85vh"
+            ref={emailEditorRef}
+            projectId={process.env.REACT_APP_UNLAYER_PROJECT_ID}
+            onLoad={() => setEditorLoaded(true)}
+            options={{
+              locale: 'fr-FR',
+              safeHtml: true,
+              templateId: messageUuid ? null : templateId,
+              tools: editorConfiguration.tools,
+              features: editorConfiguration.features,
+            }}
+          />
+          <Button variant="contained" size="medium" className={classes.exportButton} onClick={exportHtml}>
+            Export HTML
+          </Button>
+        </>
+      )}
     </div>
   )
 }
