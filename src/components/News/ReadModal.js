5b227c93a119f0af97eca031c9de7c84a7b1a3a8
XavierLac <80252457+XavierLac@users.noreply.github.com>
2021-12-20 13:20:42 +0100

    Style for readonly news modal (#762)

--- src/components/News/ReadModal.js
+++ src/components/News/ReadModal.js
@@ -1,97 +1,95 @@
-import { Dialog, Paper, Grid, Icon, Typography, Button as MuiButton } from '@mui/material'
+import PropTypes from 'prop-types'
+import { Dialog, Paper, Grid, Icon as MuiIcon, Typography, Button } from '@mui/material'
 import { styled } from '@mui/system'
 import NotificationsActiveRoundedIcon from '@mui/icons-material/NotificationsActiveRounded'
 import NotificationsOffRoundedIcon from '@mui/icons-material/NotificationsOffRounded'
 import MuiCloseIcon from '@mui/icons-material/Close'
-import PropTypes from 'prop-types'
 import DomainNews from 'domain/news'
 import { shouldForwardProps } from 'components/shared/shouldForwardProps'
 import EditIcon from '@mui/icons-material/EditRounded'
 import { format } from 'date-fns'
+import { TruncatedText } from 'components/shared/styled'
 
-const StyledPaper = styled(Paper)(
-  ({ theme }) => `
-  padding: ${theme.spacing(4)};
-  width: 664px;
-  border-radius: 12px;
+const StyledPaper = styled(Paper)`
+  padding: ${({ theme }) => theme.spacing(4)};
+  width: 586px;
+  border-radius: 8px;
 `
-)
 
-const Title = styled(Typography)(
-  ({ theme }) => `
-  font-size: 16px;
-  font-weight: 400;
-  color: ${theme.palette.gray800};
-  margin-top: ${theme.spacing(1)};
-  width: 400px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
+const HeaderContainer = styled(Grid)`
+  display: flex;
+  align-items: center;
+  margin-bottom: ${({ theme }) => theme.spacing(2)};
 `
-)
-
-const Button = styled(MuiButton)(
-  ({ theme }) => `
-  color: ${theme.palette.orange500};
-  background: ${theme.palette.newsBackground};
-  border-radius: 8.35px;
-  margin-bottom: ${theme.spacing(4)};
-  padding: ${theme.spacing(0.75, 1)};
-`
-)
-
-const CloseIcon = styled(MuiCloseIcon)(
-  ({ theme }) => `
-  color: ${theme.palette.gray700};
-  cursor: pointer;
-`
-)
 
 const StatusIcon = styled(
   'span',
   shouldForwardProps
 )(
   ({ theme, active }) => `
-  font-size: 12px;
+  font-size: 10px;
   font-weight: 500;
+  line-height: 15px;
   border-radius: 19px;
+  margin-right: ${theme.spacing(1)};
   padding: ${theme.spacing(0.25, 1)};
-  color: ${active ? theme.palette.teal700 : theme.palette.red600};
+  color: ${active ? theme.palette.green700 : theme.palette.red600};
   background: ${active ? theme.palette.activeLabel : theme.palette.inactiveLabel};
 `
 )
 
-const DateItem = styled(Grid)(
+const NotificationIcon = styled(MuiIcon)(
   ({ theme }) => `
-  font-size: 14px;
-  margin-top: ${theme.spacing(0.25)};
-`
-)
-
-const AuthorWrapper = styled(Grid)(
-  ({ theme }) => `
-  margin-bottom: ${theme.spacing(4)};
-`
-)
-
-const Author = styled(Grid)(
-  ({ theme }) => `
-  font-size: 10px;
-  color: ${theme.palette.gray600};
-  padding: 0;
-`
-)
-
-const NotificationIcon = styled(Icon)(
-  ({ theme }) => `
+  font-size: 15px;
   padding: ${theme.spacing(0.25, 1)};
-  margin: ${theme.spacing(0.25, 0, 0, 0)};
-  border: 1px solid ${theme.palette.gray200};
+  margin-right: ${theme.spacing(1)};
+  border: 1px solid ${theme.palette.gray300};
   border-radius: 19px;
-  font-size: 14px;
-  font-weight: 500;
 `
 )
+
+const DateItem = styled(Typography)`
+  font-size: 10px;
+  font-weight: 400;
+  line-height: 15px;
+  color: ${({ theme }) => theme.palette.gray600};
+`
+
+const EditButton = styled(Button)(
+  ({ theme }) => `
+  color: ${theme.palette.orange500};
+  background: ${theme.palette.newsBackground};
+  border-radius: 8.35px;
+  margin: ${theme.spacing('auto', 1, 'auto', 'auto')};
+  padding: ${theme.spacing(0.75, 1)};
+`
+)
+
+const CloseIcon = styled(MuiCloseIcon)`
+  color: ${({ theme }) => theme.palette.gray700};
+  cursor: pointer;
+`
+
+const Title = styled(TruncatedText)`
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 28px;
+  color: ${({ theme }) => theme.palette.gray800};
+  width: 400px;
+`
+
+const Author = styled(props => <Typography variant="subtitle2" {...props} />)(
+  ({ theme }) => `
+  color: ${theme.palette.gray600};
+  margin-bottom: ${theme.spacing(2)};
+`
+)
+
+const Text = styled(Typography)`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 28px;
+`
 
 const messages = {
   published: 'Publiée',
@@ -101,6 +99,8 @@ const messages = {
 }
 
 const ReadModal = ({ open, news, handleEdit, onCloseResolve }) => {
+  const Icon = news?.withNotification ? NotificationsActiveRoundedIcon : NotificationsOffRoundedIcon
+
   if (!news) return null
 
   const handleClose = () => {
@@ -109,32 +109,21 @@ const ReadModal = ({ open, news, handleEdit, onCloseResolve }) => {
 
   return (
     <Dialog open={open} onClose={handleClose} PaperComponent={StyledPaper} data-testid="news-read-only-modal">
-      <Grid container>
-        <Title title={news?.title}>{news?.title}</Title>
-        <Button onClick={handleEdit} sx={{ ml: 'auto', mr: 1 }}>
+      <HeaderContainer container>
+        <StatusIcon active={news?.status}>{news?.status ? messages.published : messages.unpublished}</StatusIcon>
+        <NotificationIcon component={Icon} />
+        <DateItem>{format(news?.createdAt || new Date(), 'dd/MM/yyyy')}</DateItem>
+        <EditButton onClick={handleEdit}>
           <EditIcon sx={{ mr: 1 }} />
           {messages.edit}
-        </Button>
-        <CloseIcon onClick={handleClose} sx={{ mt: 0.75 }} data-testid="close-icon" />
-      </Grid>
-      <Grid container spacing={1}>
-        <Grid item>
-          <StatusIcon active={news?.status}>{news?.status ? messages.published : messages.unpublished}</StatusIcon>
-        </Grid>
-        <Grid item>
-          {news?.withNotification && <NotificationIcon component={NotificationsActiveRoundedIcon} />}
-          {!news?.withNotification && <NotificationIcon component={NotificationsOffRoundedIcon} />}
-        </Grid>
-        <DateItem item>{format(news?.createdAt || new Date(), 'dd/MM/yyyy')}</DateItem>
-      </Grid>
-      <AuthorWrapper container>
-        <Author item>{`${messages.author} ${news?.creator}`}</Author>
-      </AuthorWrapper>
-      <Grid container>
-        <Grid item xs={12}>
-          <Typography sx={{ fontSize: '12px' }}>{news?.body}</Typography>
-        </Grid>
-      </Grid>
+        </EditButton>
+        <CloseIcon onClick={handleClose} data-testid="close-icon" />
+      </HeaderContainer>
+
+      <Title title={news?.title}>{news?.title}</Title>
+      <Author>{`${messages.author} ${news?.creator}`}</Author>
+
+      <Text sx={{ fontSize: '12px' }}>{news?.body}</Text>
     </Dialog>
   )
 }
