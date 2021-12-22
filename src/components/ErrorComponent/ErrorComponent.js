f152cbab73492e08e9adae1104ca82f1958f98d1
jean-baptiste cazaux <jbcazaux@gmail.com>
2021-12-22 15:25:03 +0100

    use react query on messages (#782)
    * use react query
    
    * fix error component
    
    * review

--- src/components/ErrorComponent/ErrorComponent.js
+++ src/components/ErrorComponent/ErrorComponent.js
@@ -1,19 +1,22 @@
 import PropTypes from 'prop-types'
 import UIContainer from 'ui/Container'
-import { styled } from '@mui/system'
 
-const Container = styled(UIContainer)(
-  ({ theme }) => `
-  color: ${theme.palette.statusError};
-  text-align: center;
-  width: 100%;
-  margin-bottom: ${theme.spacing(2)};
-  padding: ${theme.spacing(2)};
-  border-radius: 6px,
-  `
+const ErrorComponent = ({ errorMessage }) => (
+  <UIContainer
+    rootProps={{
+      sx: {
+        color: 'statusError',
+        textAlign: 'center',
+        width: '100%',
+        mb: 2,
+        p: 2,
+        borderRadius: '8px',
+      },
+    }}
+  >
+    {errorMessage?.message}
+  </UIContainer>
 )
-
-const ErrorComponent = ({ errorMessage }) => <Container>{errorMessage?.message}</Container>
 
 ErrorComponent.propTypes = {
   errorMessage: PropTypes.shape({
