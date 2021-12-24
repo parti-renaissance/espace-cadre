6d94588ddb35a26562b1d524afe83b43cf6dacf7
jb cazaux <jbcazaux@gmail.com>
2021-12-24 12:36:11 +0100

    use query for downloads

--- src/components/Dashboard/Charts/DownloadsCount/DownloadsCount.js
+++ src/components/Dashboard/Charts/DownloadsCount/DownloadsCount.js
@@ -1,46 +1,22 @@
-import { useState, useEffect } from 'react'
-import { ResponsiveContainer, AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
-import { Grid } from '@mui/material'
-import { makeStyles } from '@mui/styles'
-import { useDashboardDownloadsCache } from '../../../../redux/dashboard/hooks'
-import { apiClientProxy } from 'services/networking/client'
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import { Grid, Typography } from '@mui/material'
 import Loader from 'ui/Loader'
-import { useUserScope } from '../../../../redux/user/hooks'
 import ErrorComponent from 'components/ErrorComponent'
 import UIContainer from 'ui/Container'
 import pluralize from 'components/shared/pluralize/pluralize'
+import { useQuery } from 'react-query'
+import { styled } from '@mui/system'
+import { downloadsCount } from 'api/downloads'
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    padding: theme.spacing(2),
-  },
-  countBubble: {
-    color: theme.palette.blueCorner,
-    fontWeight: '600',
-    fontSize: '18px',
-    backgroundColor: theme.palette.blueBubble,
-    padding: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    borderRadius: '6px',
-  },
-  chartTitle: {
-    color: theme.palette.blackCorner,
-    fontWeight: '600',
-  },
-  chartSubtitle: {
-    color: theme.palette.grayCorner3,
-    fontSize: '12px',
-    fontWeight: '400',
-  },
-  legendChart: {
-    fontSize: '12px',
-    fontWeight: '400',
-    margin: theme.spacing(2, 2, 2, 4),
-  },
-  noData: {
-    padding: theme.spacing(2),
-  },
-}))
+const CountBubble = styled('span')`
+  color: ${({ theme }) => theme.palette.blueCorner};
+  font-weight: 600;
+  font-size: 18px;
+  background-color: ${({ theme }) => theme.palette.blueBubble};
+  padding: ${({ theme }) => theme.spacing(1)};
+  margin-right: ${({ theme }) => theme.spacing(1)};
+  border-radius: 8px;
+`
 
 const messages = {
   downloads: 'Téléchargement',
@@ -50,136 +26,131 @@ const messages = {
   errorMessage: "Les données de téléchargement de l'app sont indisponibles",
 }
 
-function DownloadsCount() {
-  const classes = useStyles()
-  const [dashboardDownloads, setDashboardDownloads] = useDashboardDownloadsCache()
-  const { totalDownloads, downloads } = dashboardDownloads || {}
-  const [currentScope] = useUserScope()
-  const [errorMessage, setErrorMessage] = useState()
+const ChartLegend = styled('li')`
+  margin: ${({ theme }) => theme.spacing(2, 2, 2, 4)};
+  font-size: 12px;
+`
 
-  useEffect(() => {
-    const getDownloads = async () => {
-      try {
-        if (dashboardDownloads === null && currentScope) {
-          setDashboardDownloads(await apiClientProxy.get('/jemengage/downloads'))
-        }
-      } catch (error) {
-        setErrorMessage(error)
-      }
-    }
-    getDownloads()
-  }, [currentScope, dashboardDownloads, setDashboardDownloads])
+const DownloadsCount = () => {
+  const {
+    data: downloads = null,
+    isLoading,
+    isError,
+  } = useQuery('downloads', downloadsCount, {
+    cacheTime: 60 * 60 * 1000,
+    staleTime: 60 * 60 * 1000,
+  })
 
-  const dashboardDownloadsContent = () => {
-    if (dashboardDownloads !== null && downloads.length > 0) {
-      return (
-        <>
-          <Grid container className={classes.container}>
-            <span className={classes.countBubble}>{totalDownloads}</span>
-            <Grid item>
-              <div className={classes.chartTitle}>
-                {pluralize(totalDownloads, messages.downloads)}
-                {messages.period}
-              </div>
-              <div className={classes.chartSubtitle}>{messages.subtitle}</div>
-            </Grid>
-          </Grid>
-          <ResponsiveContainer width="100%" height={250}>
-            <AreaChart
-              data={downloads}
-              margin={{
-                top: 5,
-                right: 20,
-                bottom: 5,
-                left: 0,
-              }}
-            >
-              <defs>
-                <linearGradient id="colorQuotidien" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#0049C6" stopOpacity={0.8} />
-                  <stop offset="95%" stopColor="#0049C6" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <XAxis
-                dataKey="date"
-                angle={-14}
-                tickMargin={8}
-                interval={4}
-                style={{
-                  color: '#717BA0',
-                  fontFamily: 'roboto',
-                  fontSize: '12px',
-                }}
-              />
-              <YAxis
-                axisLine={false}
-                tickLine={false}
-                style={{
-                  color: '#717BA0',
-                  fontFamily: 'roboto',
-                  fontSize: '12px',
-                }}
-              />
-              <CartesianGrid strokeDasharray=".08" />
-              <Tooltip
-                contentStyle={{
-                  background: '#fff',
-                  borderColor: '#F0F1F3',
-                  borderRadius: '6px',
-                }}
-                labelStyle={{
-                  fontSize: '14px',
-                  color: '#1A334D',
-                  fontFamily: 'Poppins',
-                  fontWeight: 'bold',
-                  padding: '2px 5px',
-                  textAlign: 'left',
-                }}
-                itemStyle={{
-                  fontSize: '14px',
-                  fontFamily: 'Poppins',
-                  padding: '2px 5px',
-                  textAlign: 'left',
-                }}
-                cursor={{
-                  stroke: '#0049C6',
-                  strokeWidth: 0.5,
-                }}
-              />
-              <Area
-                type="monotone"
-                name={messages.dailyDownloads}
-                dataKey="unique_user"
-                stroke="#0049C6"
-                fillOpacity={1}
-                fill="url(#colorQuotidien)"
-              />
-            </AreaChart>
-          </ResponsiveContainer>
-          <Grid container>
-            <Grid item>
-              <li className={classes.legendChart} style={{ color: '#0049C6' }}>
-                {messages.dailyDownloads}
-              </li>
-            </Grid>
-          </Grid>
-        </>
-      )
-    }
-    if (dashboardDownloads !== null && downloads.length === 0) {
-      return <UIContainer className={classes.noData}>{messages.errorMessage}</UIContainer>
-    }
-    if (errorMessage) {
-      return <ErrorComponent errorMessage={errorMessage} />
-    }
+  if (isLoading) {
     return (
       <UIContainer textAlign="center">
         <Loader />
       </UIContainer>
     )
   }
+  if (isError) {
+    return (
+      <UIContainer textAlign="center">
+        <ErrorComponent errorMessage={{ message: messages.errorMessage }} />
+      </UIContainer>
+    )
+  }
 
-  return <div>{dashboardDownloadsContent()}</div>
+  return (
+    <>
+      <Grid container sx={{ p: 2 }}>
+        <CountBubble>{downloads.totalDownloads}</CountBubble>
+        <Grid item>
+          <Typography variant="subtitle1" component="div" sx={{ color: 'blackCorner' }}>
+            {pluralize(downloads.totalDownloads, messages.downloads)}
+            {messages.period}
+          </Typography>
+          <Typography variant="subtitle2" component="div" sx={{ color: 'grayCorner3' }}>
+            {messages.subtitle}
+          </Typography>
+        </Grid>
+      </Grid>
+      <ResponsiveContainer width="100%" height={250}>
+        <AreaChart
+          data={downloads.downloads}
+          margin={{
+            top: 5,
+            right: 20,
+            bottom: 5,
+            left: 0,
+          }}
+        >
+          <defs>
+            <linearGradient id="colorQuotidien" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#0049C6" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#0049C6" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis
+            dataKey="date"
+            angle={-14}
+            tickMargin={8}
+            interval={4}
+            style={{
+              color: '#717BA0',
+              fontFamily: 'roboto',
+              fontSize: '12px',
+            }}
+          />
+          <YAxis
+            axisLine={false}
+            tickLine={false}
+            style={{
+              color: '#717BA0',
+              fontFamily: 'roboto',
+              fontSize: '12px',
+            }}
+          />
+          <CartesianGrid strokeDasharray=".08" />
+          <Tooltip
+            contentStyle={{
+              background: '#fff',
+              borderColor: '#F0F1F3',
+              borderRadius: '6px',
+            }}
+            labelStyle={{
+              fontSize: '14px',
+              color: '#1A334D',
+              fontFamily: 'Poppins',
+              fontWeight: 'bold',
+              padding: '2px 5px',
+              textAlign: 'left',
+            }}
+            itemStyle={{
+              fontSize: '14px',
+              fontFamily: 'Poppins',
+              padding: '2px 5px',
+              textAlign: 'left',
+            }}
+            cursor={{
+              stroke: '#0049C6',
+              strokeWidth: 0.5,
+            }}
+          />
+          <Area
+            type="monotone"
+            name={messages.dailyDownloads}
+            dataKey="unique_user"
+            stroke="#0049C6"
+            fillOpacity={1}
+            fill="url(#colorQuotidien)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+      <Grid container>
+        <Grid item>
+          <ChartLegend sx={{ color: '#0049C6' }}>
+            <Typography variant="subtitle2">{messages.dailyDownloads}</Typography>
+          </ChartLegend>
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
 export default DownloadsCount
