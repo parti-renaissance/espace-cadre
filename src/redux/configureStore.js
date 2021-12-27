0c378c59c3e81874ed468595e5bcfe573175d1ee
jb cazaux <jbcazaux@gmail.com>
2021-12-27 15:05:16 +0100

    review

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,32 +1,33 @@
-import { configureStore as reduxConfigureStore } from '@reduxjs/toolkit';
-import { persistReducer, persistStore, PERSIST } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { createReduxEnhancer } from '@sentry/react';
-import rootReducer from './reducers';
+import { configureStore as reduxConfigureStore } from '@reduxjs/toolkit'
+import { persistReducer, persistStore, PERSIST } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { createReduxEnhancer } from '@sentry/react'
+import rootReducer from './reducers'
 
 const configureStore = () => {
-    const persistConfig = {
-        key: 'root',
-        whitelist: ['auth', 'adherents'],
-        storage,
-    };
+  const persistConfig = {
+    key: 'root',
+    whitelist: ['auth'],
+    storage,
+  }
 
-    const persistedReducer = persistReducer(persistConfig, rootReducer);
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-    const store = reduxConfigureStore({
-        devTools: process.env.NODE_ENV !== 'production',
-        middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [PERSIST], // see https://github.com/rt2zz/redux-persist/issues/988#issuecomment-529575333
-            },
-        }),
-        reducer: persistedReducer,
-        enhancers: [createReduxEnhancer()],
-    });
+  const store = reduxConfigureStore({
+    devTools: process.env.NODE_ENV !== 'production',
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [PERSIST], // see https://github.com/rt2zz/redux-persist/issues/988#issuecomment-529575333
+        },
+      }),
+    reducer: persistedReducer,
+    enhancers: [createReduxEnhancer()],
+  })
 
-    const persistor = persistStore(store);
+  const persistor = persistStore(store)
 
-    return { store, persistor };
-};
+  return { store, persistor }
+}
 
-export default configureStore;
+export default configureStore
