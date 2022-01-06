045bb7506c6fc9bf2c3664975dde33dd5636246d
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-01-06 10:28:05 +0100

    Messagerie modal confirmation

--- src/components/Messagerie/Component/ModalComponent.js
+++ src/components/Messagerie/Component/ModalComponent.js
@@ -1,97 +1,87 @@
-import { Dialog, Box, Button } from '@mui/material'
-import { makeStyles } from '@mui/styles'
+import { Dialog, Paper, Grid, Button, Typography } from '@mui/material'
+import { styled } from '@mui/system'
 import PropTypes from 'prop-types'
 import pluralize from 'components/shared/pluralize/pluralize'
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    background: theme.palette.whiteCorner,
-    borderRadius: '12px',
-    padding: '32px',
-  },
-  title: {
-    fontSize: '18px',
-    color: theme.palette.gray700,
-    fontWeight: '600',
-    marginBottom: '10px',
-  },
-  description: {
-    fontSize: '14px',
-    color: theme.palette.gray700,
-    fontWeight: '400',
-    margin: '0 0 32px',
-  },
-  buttonContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  cancelButton: {
-    color: theme.palette.blue600,
-    background: theme.palette.whiteCorner,
-    border: `1px solid ${theme.palette.blue600}`,
-    borderRadius: '8px',
-    padding: '8px 16px',
-    marginRight: '16px',
-    '&:hover': {
-      background: theme.palette.gray100,
-    },
-  },
-  sendButton: {
-    color: theme.palette.whiteCorner,
-    background: theme.palette.blue600,
-    borderRadius: '8px',
-    padding: '8px 16px',
-    '&:hover': {
-      background: theme.palette.blue800,
-    },
-  },
-}))
+const StyledPaper = styled(Paper)`
+  background: ${({ theme }) => theme.palette.whiteCorner};
+  border-radius: 12px;
+  padding: 32px;
+`
+
+const Title = styled(Typography)`
+  font-size: 18px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.palette.gray700};
+  margin-bottom: 10px;
+`
+
+const Confirmation = styled(Typography)(
+  ({ theme }) => `
+  font-size: 14px;
+  color: ${theme.palette.gray700};
+  font-weight: 400;
+  margin: ${theme.spacing(0, 0, 4)}
+`
+)
+
+const Cancel = styled(Button)(
+  ({ theme }) => `
+  color: ${theme.palette.blue600};
+  background: ${theme.palette.whiteCorner};
+  border: 1px solid ${theme.palette.blue600};
+  border-radius: 8px;
+  padding: ${theme.spacing(1, 2)};
+  margin-right: ${theme.spacing(2)};
+  &:hover {
+    background: ${theme.palette.gray100};
+  }
+`
+)
+
+const Send = styled(Button)(
+  ({ theme }) => `
+  color: ${theme.palette.whiteCorner};
+  border-radius: 8px;
+  padding: ${theme.spacing(1, 2)};
+  &:hover {
+    background: ${theme.palette.blue800}
+  }
+`
+)
 
 const messages = {
-  confirmationSentence: 'Êtes-vous sûr de vouloir envoyer le message à ',
+  title: 'Confirmation',
+  confirmation: 'Êtes-vous sûr de vouloir envoyer le message à ',
   contact: 'contact',
   questionMark: '?',
-  confirmation: 'Confirmation',
   cancel: 'Annuler',
   send: 'Envoyer',
 }
 
-const ModalComponent = ({ open, handleClose, handleSendEmail, recipientCount }) => {
-  const classes = useStyles()
-
-  return (
-    <Dialog
-      open={open}
-      onClose={handleClose}
-      classes={{ paper: classes.paper }}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <Box className={classes.title}>{messages.confirmation}</Box>
-      <Box id="alert-dialog-description" className={classes.description}>
-        <span>
-          {messages.confirmationSentence}
-          {recipientCount} {pluralize(recipientCount, messages.contact)} {messages.questionMark}
-        </span>
-      </Box>
-      <Box className={classes.buttonContainer}>
-        <Button variant="contained" onClick={handleClose} className={classes.cancelButton}>
-          {messages.cancel}
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => {
-            handleSendEmail()
-            handleClose()
-          }}
-          className={classes.sendButton}
-        >
-          {messages.send}
-        </Button>
-      </Box>
-    </Dialog>
-  )
-}
+const ModalComponent = ({ open, handleClose, handleSendEmail, recipientCount }) => (
+  <Dialog open={open} onClose={handleClose} PaperComponent={StyledPaper}>
+    <Title>{messages.title}</Title>
+    <Confirmation>
+      {messages.confirmation}
+      {recipientCount} {pluralize(recipientCount, messages.contact)} {messages.questionMark}
+    </Confirmation>
+    <Grid container sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <Cancel variant="contained" onClick={handleClose}>
+        {messages.cancel}
+      </Cancel>
+      <Send
+        variant="contained"
+        onClick={() => {
+          handleSendEmail()
+          handleClose()
+        }}
+      >
+        {messages.send}
+      </Send>
+    </Grid>
+  </Dialog>
+)
 
 export default ModalComponent
 
