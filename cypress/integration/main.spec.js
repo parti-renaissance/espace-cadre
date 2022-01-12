1bb24648abe004e4eebe00ed0ec5b5852b004c60
CoZil <nicolas7even@gmail.com>
2022-01-12 13:49:08 +0100

    [EM-1966] Phoning campaign list (cypress mock)

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -29,7 +29,11 @@ export const initialization = () => {
   mock('GET', '/api/v3/ripostes?order[created_at]=desc&page=1&page_size=20&scope=national', 'ripostes/ripostes')
   mock('GET', '/api/v3/profile/me/scope/phoning_national_manager', 'phoning/phoningScope')
   mock('GET', '/api/v3/phoning_campaigns/kpi?scope=phoning_national_manager', 'phoning/kpi')
-  mock('GET', '/api/v3/phoning_campaigns?scope=phoning_national_manager', 'phoning/campaigns')
+  mock(
+    'GET',
+    '/api/v3/phoning_campaigns?order[created_at]=desc&page=1&page_size=20&scope=phoning_national_manager',
+    'phoning/campaigns'
+  )
   mock(
     'GET',
     '/api/v3/phoning_campaigns/11111111-1111-1111-1111-111111111111?scope=phoning_national_manager',
