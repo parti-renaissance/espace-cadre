f9d1402ec5b3df10f5b500f59b9ed7eac9ff6f7c
CoZil <nicolas7even@gmail.com>
2022-01-18 12:25:44 +0100

    [EM-2001] Pickers

--- src/components/Phoning/CreateEdit/shared/components/styled.js
+++ src/components/Phoning/CreateEdit/shared/components/styled.js
@@ -1,5 +1,6 @@
 import { styled } from '@mui/system'
 import { Checkbox as MuiCheckbox, InputLabel, TextField, Typography } from '@mui/material'
+import MuiPickersDay from '@mui/lab/PickersDay'
 
 export const StepTitle = styled(Typography)(
   ({ theme }) => `
@@ -70,3 +71,21 @@ export const Input = styled(props => <TextField size="small" variant="outlined" 
     },
   })
 )
+
+export const PickersDay = styled(MuiPickersDay)(({ theme }) => ({
+  '&:not(.Mui-selected)': {
+    borderColor: theme.palette.campaign.color,
+    '&:hover, &:focus': {
+      backgroundColor: theme.palette.campaign.color,
+      color: theme.palette.whiteCorner,
+    },
+  },
+  '&.Mui-selected': {
+    borderColor: 'transparent',
+    backgroundColor: theme.palette.campaign.color,
+    '&:hover, &:focus': {
+      backgroundColor: theme.palette.campaign.color,
+      color: theme.palette.whiteCorner,
+    },
+  },
+}))
