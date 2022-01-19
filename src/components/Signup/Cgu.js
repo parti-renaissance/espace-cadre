b280f2c641ff7d4660e989fbb95de8a7d5a3f9a4
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-01-19 10:15:35 +0100

    Mentions légales

--- src/components/Signup/Cgu.js
+++ src/components/Signup/Cgu.js
@@ -1,14 +1,24 @@
 import ReactMarkdown from 'react-markdown'
 import { cgu as cguQuery } from 'api/legal'
 import { useQuery } from 'react-query'
+import { styled } from '@mui/system'
+
+const Container = styled('div')(
+  ({ theme }) => `
+  max-width: 700px;
+  padding: ${theme.spacing(2)};
+  margin: ${theme.spacing(0, 'auto')};
+`
+)
 
 const CGU = () => {
   const { data: cgu } = useQuery('rgpd', cguQuery)
+
   return (
-    <>
+    <Container>
       <h1>{cgu?.title}</h1>
       <ReactMarkdown>{cgu?.content}</ReactMarkdown>
-    </>
+    </Container>
   )
 }
 
