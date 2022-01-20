afd62f07a502e45c7838d75c17f4909c6cf93e12
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-01-20 18:37:43 +0100

    Success page for signup

--- src/components/Signup/SignupConfirm.js
+++ src/components/Signup/SignupConfirm.js
@@ -1,23 +1,44 @@
 import { styled } from '@mui/system'
-import { Box as MuiBox, Container, Typography as MuiTypography } from '@mui/material'
+import { Typography } from '@mui/material'
 
-const Box = styled(MuiBox)(
+const Container = styled('div')`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  padding: ${({ theme }) => theme.spacing(0, 2)};
+`
+
+const Title = styled(Typography)(
   ({ theme }) => `
-    margin: ${theme.spacing(4, 'auto', 0)}
+  font-family: MaaxItalic;
+  color: ${theme.palette.blackCorner};
+  font-size: 40px;
+  font-weight: 600;
+  line-height: 56px;
+  margin-bottom: ${theme.spacing(3)};
 `
 )
 
-const Typography = styled(MuiTypography)`
-  font-size: 28px;
-  font-weight: 500;
-  line-height: 38px;
+const Main = styled(Typography)`
+  font-size: 16px;
+  ${({ theme }) => theme.palette.gray600};
+  font-weight: 400;
+  line-height: 24px;
 `
 
+const messages = {
+  title: "Je m'engage",
+  subtitle: 'Vous êtes bien inscrit',
+  body: 'Vous avez reçu un email pour valider votre compte et créer votre mot de passe',
+}
+
 const SignupConfirm = () => (
-  <Container maxWidth="md">
-    <Box>
-      <Typography>Merci de vous être inscrit</Typography>
-    </Box>
+  <Container>
+    <Title>{messages.title}</Title>
+    <Main sx={{ mb: 4 }}>{messages.subtitle}</Main>
+    <Main>{messages.body}</Main>
   </Container>
 )
 
