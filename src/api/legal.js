38c0bd1093844c28093436365f83f70fca63d889
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-01-21 12:16:34 +0100

    Update legal endpoints

--- src/api/legal.js
+++ src/api/legal.js
@@ -1,5 +1,5 @@
 import { apiClientPublic } from 'services/networking/client'
 
 export const RGPDQuery = () => apiClientPublic('get', '/api/je-mengage/rgpd')
-export const CGUQuery = () => apiClientPublic('get', '/api/pages/je-mengage-mentions-legales')
-export const PPDQuery = () => apiClientPublic('get', '/api/pages/politique-protection-donnees')
+export const CGUQuery = () => apiClientPublic('get', '/api/je-mengage/je-mengage-web-mentions-legales')
+export const PPDQuery = () => apiClientPublic('get', '/api/je-mengage/politique-protection-donnees')
