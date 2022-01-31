8cd6726a818833c0b049e2e632dbed82b0a0092d
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-01-31 15:57:27 +0100

    Fix returns

--- src/components/DTD/CampaignDetail/shared/helpers.js
+++ src/components/DTD/CampaignDetail/shared/helpers.js
@@ -1,18 +1,22 @@
-import { isBefore, isAfter } from 'date-fns'
+import { isBefore } from 'date-fns'
 import { chipLabelByStatus, chipColorsByStatus, multipleChoice, simpleField, uniqueChoice } from './constants'
+
+const today = new Date()
 
 export const secondsToMinutes = seconds => (Number.isInteger(seconds) ? `${Math.floor(seconds / 60)} min` : null)
 
 export const chipColorsByDate = (startDate, endDate) => {
-  if (isBefore(new Date(), startDate)) return chipColorsByStatus.tocome
-  if (isBefore(new Date(), endDate)) return chipColorsByStatus.ongoing
-  if (isAfter(new Date(), endDate)) return chipColorsByStatus.finished
+  if (isBefore(today, startDate)) return chipColorsByStatus.tocome
+  if (isBefore(today, endDate)) return chipColorsByStatus.ongoing
+  return chipColorsByStatus.finished
 }
 
 export const chipLabelByDate = (startDate, endDate) => {
-  if (isBefore(new Date(), startDate)) return chipLabelByStatus.tocome
-  if (isBefore(new Date(), endDate)) return chipLabelByStatus.ongoing
-  if (isAfter(new Date(), endDate)) return chipLabelByStatus.finished
+  const today = new Date()
+
+  if (isBefore(today, startDate)) return chipLabelByStatus.tocome
+  if (isBefore(today, endDate)) return chipLabelByStatus.ongoing
+  return chipLabelByStatus.finished
 }
 
 export const surveysColumnsStyles = {
