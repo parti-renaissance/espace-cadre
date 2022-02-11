f82cbcc4e3e68ce80598e1fc4b4f38908fe73b51
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-02-11 18:26:34 +0100

    Event chip label

--- src/components/events/card/Header.js
+++ src/components/events/card/Header.js
@@ -1,5 +1,5 @@
 import { Title, UIChip } from 'ui/Card'
-import { format } from 'date-fns'
+import { format, isAfter } from 'date-fns'
 import { styled } from '@mui/system'
 import GroupIcon from '@mui/icons-material/Group'
 import { Box, Typography } from '@mui/material'
@@ -71,43 +71,61 @@ export const formatAddress = ({ address, postalCode, city }) =>
 const messages = {
   scheduled: 'À venir',
   canceled: 'Annulé',
+  past: 'Passé',
 }
+const today = new Date()
 
-const Header = ({ event }) => (
-  <>
-    {event.img ? (
-      <Image src={event.img} />
-    ) : (
-      <NoImageContainer>
-        <img src={noImage} height="100px" width="100px" alt="no image" />
-      </NoImageContainer>
-    )}
-    <HorizontalContainer>
-      <UIChip
-        color={event.scheduled ? 'teal700' : 'red600'}
-        bgcolor={event.scheduled ? 'activeLabel' : 'inactiveLabel'}
-        label={event.scheduled ? messages.scheduled : messages.canceled}
-      />
-      <AttendeesIcon>
-        <GroupIcon sx={{ mr: 1, fontSize: '16px', fontWeight: '500', color: 'gray500' }} />
-        {event.attendees}
-      </AttendeesIcon>
-    </HorizontalContainer>
-    <Title subject={event.name} author={`Par ${event.organizer}`} lines={2} sx={{ pt: 1 }} />
-    <Box component="div" sx={{ display: 'flex', mt: 1 }}>
-      <CalendarTodayIcon />
-      <BeginAtTypography variant="subtitle2">
-        {format(event.beginAt, 'd MMM yyyy HH:mm', { locale: fr })}
-      </BeginAtTypography>
-    </Box>
-    <Box component="div" sx={{ display: 'flex', mt: 1 }}>
-      <RoomIcon sx={{ mr: 1, fontSize: '16px', fontWeight: '500', color: 'gray500' }} />
-      <TruncatedText lines={1} variant="subtitle2" sx={{ height: '55px', color: 'gray600' }}>
-        {formatAddress(event.address)}
-      </TruncatedText>
-    </Box>
-  </>
-)
+const Header = ({ event }) => {
+  const eventStatus = () => {
+    if (event.scheduled && !isAfter(today, event?.beginAt)) return messages.scheduled
+    if (!event.scheduled) return messages.canceled
+    if (isAfter(today, event?.beginAt)) return messages.past
+  }
+
+  const eventColor = () => {
+    if (event.scheduled && !isAfter(today, event?.beginAt)) return 'teal700'
+    if (!event.scheduled) return 'red600'
+    if (isAfter(today, event?.beginAt)) return 'gray700'
+  }
+
+  const eventBackgroundColor = () => {
+    if (event.scheduled && !isAfter(today, event?.beginAt)) return 'activeLabel'
+    if (!event.scheduled) return 'inactiveLabel'
+    if (isAfter(today, event?.beginAt)) return 'pastLabel'
+  }
+
+  return (
+    <>
+      {event.img ? (
+        <Image src={event.img} />
+      ) : (
+        <NoImageContainer>
+          <img src={noImage} height="100px" width="100px" alt="no image" />
+        </NoImageContainer>
+      )}
+      <HorizontalContainer>
+        <UIChip color={eventColor()} bgcolor={eventBackgroundColor()} label={eventStatus()} />
+        <AttendeesIcon>
+          <GroupIcon sx={{ mr: 1, fontSize: '16px', fontWeight: '500', color: 'gray500' }} />
+          {event.attendees}
+        </AttendeesIcon>
+      </HorizontalContainer>
+      <Title subject={event.name} author={`Par ${event.organizer}`} lines={2} sx={{ pt: 1 }} />
+      <Box component="div" sx={{ display: 'flex', mt: 1 }}>
+        <CalendarTodayIcon />
+        <BeginAtTypography variant="subtitle2">
+          {format(event.beginAt, 'd MMM yyyy HH:mm', { locale: fr })}
+        </BeginAtTypography>
+      </Box>
+      <Box component="div" sx={{ display: 'flex', mt: 1 }}>
+        <RoomIcon sx={{ mr: 1, fontSize: '16px', fontWeight: '500', color: 'gray500' }} />
+        <TruncatedText lines={1} variant="subtitle2" sx={{ height: '55px', color: 'gray600' }}>
+          {formatAddress(event.address)}
+        </TruncatedText>
+      </Box>
+    </>
+  )
+}
 
 Header.propTypes = {
   event: Event.propTypes.isRequired,
