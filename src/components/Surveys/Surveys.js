acd742121c309964866a9b9f941ac4ffd57685c2
CoZil <nicolas7even@gmail.com>
2022-02-24 22:26:47 +0100

    [LPN-366] Surveys Local / National tabs & KPI

--- src/components/Surveys/Surveys.js
+++ src/components/Surveys/Surveys.js
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { generatePath, useNavigate } from 'react-router'
 import { useMutation } from 'react-query'
-import { Container, Grid, Typography } from '@mui/material'
+import { Container, Grid, Tab as MuiTab, Tabs, Typography } from '@mui/material'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import { styled } from '@mui/system'
 
 import { useInfiniteQueryWithScope, useQueryWithScope } from 'api/useQueryWithScope'
 import { getNextPageParam, usePaginatedData } from 'api/pagination'
@@ -12,11 +13,24 @@ import { useCustomSnackbar } from 'components/shared/notification/hooks'
 import { notifyVariants } from 'components/shared/notification/constants'
 import { useUserScope } from '../../redux/user/hooks'
 import { scopesVisibility, visibility } from './shared/constants'
-import SurveyItem from './SurveyItem'
+import SurveyItem from './SurveysItem'
+import SurveysKPI from './SurveysKPI'
 import CreateEdit from './CreateEdit/CreateEdit'
 import { PageHeaderButton } from 'ui/PageHeader/PageHeader'
 import Loader from 'ui/Loader'
 import PageHeader from 'ui/PageHeader'
+
+const Tab = styled(MuiTab)(({ theme }) => ({
+  textTransform: 'none',
+  color: theme.palette.gray400,
+  '&.Mui-selected': {
+    color: theme.palette.gray800,
+  },
+}))
+const TabLabel = styled(Typography)`
+  font-size: 18px;
+  font-weight: 400;
+`
 
 const infiniteScrollStylesOverrides = {
   '& .infinite-scroll-component__outerdiv': {
@@ -29,11 +43,16 @@ const messages = {
   create: 'Créer un questionnaire',
   publishSuccess: 'Questionnaire publié avec succès',
   unpublishSuccess: 'Questionnaire dépublié avec succès',
+  localSurvey: 'Questionnaire local',
+  localSurveys: 'Questionnaires locaux',
+  nationalSurvey: 'Questionnaire national',
+  nationalSurveys: 'Questionnaires nationaux',
 }
 
 const Surveys = () => {
   const [currentScope] = useUserScope()
   const { code: scope } = currentScope
+  const [selectedTab, setSelectedTab] = useState(visibility.local)
   const [isCreateEditModalOpen, setIsCreateEditModalOpen] = useState(false)
   const [surveyIdToUpdate, setSurveyIdToUpdate] = useState()
   const navigate = useNavigate()
@@ -42,6 +61,7 @@ const Surveys = () => {
 
   const {
     data: paginatedSurveys = null,
+    isLoading: isSurveysLoading,
     fetchNextPage: fetchNextPageSurveys,
     hasNextPage: hasNextPageSurveys,
     refetch: refetchSurveys,
@@ -54,6 +74,8 @@ const Surveys = () => {
     }
   )
   const surveys = usePaginatedData(paginatedSurveys)
+  const localSurveys = useMemo(() => surveys.filter(({ type }) => type === visibility.local), [surveys])
+  const nationalSurveys = useMemo(() => surveys.filter(({ type }) => type === visibility.national), [surveys])
 
   const { mutate: createOrUpdateSurvey } = useMutation(createOrUpdateSurveyQuery, {
     onSuccess: ({ published: isPublished }) => {
@@ -93,6 +115,10 @@ const Surveys = () => {
     setIsCreateEditModalOpen(false)
   }
 
+  const handleTabChange = (_, tabId) => {
+    setSelectedTab(tabId)
+  }
+
   return (
     <Container maxWidth="lg" sx={{ mb: 3 }}>
       <Grid container justifyContent="space-between">
@@ -120,30 +146,96 @@ const Surveys = () => {
         data-cy="surveys-container"
         sx={{ pt: 4, ...infiniteScrollStylesOverrides }}
       >
-        {surveys.length > 0 && (
-          <InfiniteScroll
-            dataLength={surveys.length}
-            next={() => fetchNextPageSurveys()}
-            hasMore={hasNextPageSurveys}
-            loader={<Loader />}
-          >
-            <Grid container spacing={2} data-cy="surveys-list">
-              {surveys.map(survey => (
-                <SurveyItem
-                  key={survey.id}
-                  isPublished={survey.isPublished}
-                  readOnly={survey.type !== scopesVisibility[scope]}
-                  title={survey.title}
-                  author={survey.author}
-                  questionsCount={survey.questionsCount}
-                  answersCount={survey.answersCount}
-                  handleView={handleView(survey.id)}
-                  handlePublish={togglePublish(survey.id)}
-                  handleUpdate={handleUpdate(survey.id)}
-                />
-              ))}
-            </Grid>
-          </InfiniteScroll>
+        {!isSurveysLoading && (
+          <>
+            <SurveysKPI
+              local={{
+                count: localSurveys.length,
+                title: localSurveys.length > 1 ? messages.localSurveys : messages.localSurvey,
+                publishedCount: localSurveys.filter(({ isPublished }) => !!isPublished).length,
+              }}
+              national={{
+                count: nationalSurveys.length,
+                title: nationalSurveys.length > 1 ? messages.nationalSurveys : messages.nationalSurvey,
+              }}
+              collectedAnswers={{
+                count: 19,
+                byTheMonth: 3,
+              }}
+            />
+
+            <Tabs
+              variant="scrollable"
+              value={selectedTab}
+              onChange={handleTabChange}
+              TabIndicatorProps={{ sx: { bgcolor: 'indigo700' } }}
+              sx={{ my: 2 }}
+            >
+              <Tab
+                value={visibility.local}
+                label={<TabLabel>{messages.localSurveys}</TabLabel>}
+                disableRipple
+                disableFocusRipple
+              />
+              <Tab
+                value={visibility.national}
+                label={<TabLabel>{messages.nationalSurveys}</TabLabel>}
+                disableRipple
+                disableFocusRipple
+              />
+            </Tabs>
+
+            {selectedTab === visibility.local && surveys.length > 0 && (
+              <InfiniteScroll
+                dataLength={localSurveys.length}
+                next={() => fetchNextPageSurveys()}
+                hasMore={hasNextPageSurveys}
+                loader={<Loader />}
+              >
+                <Grid container spacing={2} data-cy="surveys-list">
+                  {localSurveys.map(survey => (
+                    <SurveyItem
+                      key={survey.id}
+                      isPublished={survey.isPublished}
+                      readOnly={survey.type !== scopesVisibility[scope]}
+                      title={survey.title}
+                      author={survey.author}
+                      questionsCount={survey.questionsCount}
+                      answersCount={survey.answersCount}
+                      handleView={handleView(survey.id)}
+                      handlePublish={togglePublish(survey.id)}
+                      handleUpdate={handleUpdate(survey.id)}
+                    />
+                  ))}
+                </Grid>
+              </InfiniteScroll>
+            )}
+            {selectedTab === visibility.national && surveys.length > 0 && (
+              <InfiniteScroll
+                dataLength={nationalSurveys.length}
+                next={() => fetchNextPageSurveys()}
+                hasMore={hasNextPageSurveys}
+                loader={<Loader />}
+              >
+                <Grid container spacing={2} data-cy="surveys-list">
+                  {nationalSurveys.map(survey => (
+                    <SurveyItem
+                      key={survey.id}
+                      isPublished={survey.isPublished}
+                      readOnly={survey.type !== scopesVisibility[scope]}
+                      title={survey.title}
+                      author={survey.author}
+                      questionsCount={survey.questionsCount}
+                      answersCount={survey.answersCount}
+                      handleView={handleView(survey.id)}
+                      handlePublish={togglePublish(survey.id)}
+                      handleUpdate={handleUpdate(survey.id)}
+                    />
+                  ))}
+                </Grid>
+              </InfiniteScroll>
+            )}
+          </>
         )}
       </Grid>
 
