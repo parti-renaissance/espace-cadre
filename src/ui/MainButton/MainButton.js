9f6c86e7686d437405264f0b60e6f6839d55ecb1
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-03-11 10:42:54 +0100

    Refacto MainButton

--- src/ui/MainButton/MainButton.js
+++ src/ui/MainButton/MainButton.js
@@ -2,15 +2,17 @@ import { Button as MuiButton } from '@mui/material'
 import PropTypes from 'prop-types'
 import { styled } from '@mui/system'
 
-const Button = styled(MuiButton)`
-  color: ${({ theme }) => theme.palette.button.color};
-  background: ${({ theme }) => theme.palette.button.background.main};
+const Button = styled(MuiButton)(
+  ({ theme }) => `
+  color: ${theme.palette.button.color};
+  background: ${theme.palette.button.background.main};
   &:hover {
-    background: ${({ theme }) => theme.palette.button.background.hover};
+    background: ${theme.palette.button.background.hover};
   }
   height: 35px;
-  padding: ${({ theme }) => theme.spacing(0.75, 2)};
+  padding: ${theme.spacing(0.75, 2)};
 `
+)
 
 const MainButton = ({ children, onClick, rootProps }) => (
   <Button variant="contained" onClick={onClick} {...rootProps}>
