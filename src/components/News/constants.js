accc84c7172b2d363faa9a6a34a10913adb22471
Marius <riamarius@gmail.com>
2022-03-14 11:30:09 +0100

    Fix typo

--- src/components/News/constants.js
+++ src/components/News/constants.js
@@ -4,7 +4,7 @@ export const NEWS_OPTION = '(Optionnel)'
 export const NEWS_NOTIFICATION_TEXT_1 =
   'Vous pouvez, à la publication de cette actualité, l’envoyer par notification push à tous vos militants connectés et toujours abonnés aux notifications de l’application mobile.'
 export const NEWS_NOTIFICATION_TEXT_2 =
-  'Attention, si vous abusez des push, vos militants se désabonneront des notifocations de l’application mobile.'
+  'Attention, si vous abusez des push, vos militants se désabonneront des notifications de l’application mobile.'
 export const NEWS_SEND_NOTIFICATION = 'Envoyer une notification push'
 export const NEWS_CTA_TITLE = 'Bouton d’action'
 export const NEWS_CTA_PLACEHOLDER = 'Je m’engage'
