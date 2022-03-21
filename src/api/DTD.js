3789fe7fbb1efecc583abe1914d7d621ba30871e
Rémi Gardien <gardien.remi@gmail.com>
2022-03-21 09:31:18 +0100

    Change .xlsx to .xls and add mimetype (#1224)
    * Change .xlsx to .xls and add mimetype
    
    * Remove custom type

--- src/api/DTD.js
+++ src/api/DTD.js
@@ -121,5 +121,8 @@ export const getDTDCampaignSurveysReplies = async campaignId => {
 
 export const getPhoningCampaignSurveysRepliesExport = async campaignId => {
   const data = await apiClient.get(`api/v3/pap_campaigns/${campaignId}/replies.xls`)
-  saveAs(new Blob([data]), `Questionnaires PAP - ${format(new Date(), 'dd.MM.yyyy')}.xlsx`)
+  saveAs(
+    new Blob([data]),
+    `Questionnaires PAP - ${format(new Date(), 'dd.MM.yyyy')}.xls`
+  )
 }
--- src/api/surveys.js
+++ src/api/surveys.js
@@ -58,7 +58,10 @@ export const getSurveyRepliesQuery = async surveyId => {
 
 export const getSurveysRepliesExport = async (surveyId, surveyTitle) => {
   const data = await apiClient.get(`api/v3/surveys/${surveyId}/replies.xls`)
-  saveAs(new Blob([data]), `${surveyTitle} - ${format(new Date(), 'dd.MM.yyyy')}.xlsx`)
+  saveAs(
+    new Blob([data]),
+    `${surveyTitle} - ${format(new Date(), 'dd.MM.yyyy')}.xls`
+  )
 }
 
 const formatChoicePayload =
