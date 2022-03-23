f903a2ead85cf00f0a92533e300b93f771dc3d8f
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-03-23 10:46:34 +0100

    Test Europe/Paris TZ

--- cypress/integration/messagerie.spec.js
+++ cypress/integration/messagerie.spec.js
@@ -2,6 +2,7 @@ import { initialization } from './main.spec'
 
 const DeleteButton = '[data-cy="dot-action-menu"]'
 const MailCard = '[data-cy="email-campaign-card"]'
+const MailObjectInput = '[data-cy="mail-editor-object"]'
 
 
   const navigate = () => {
@@ -16,34 +17,33 @@ const MailCard = '[data-cy="email-campaign-card"]'
   })
 
   describe('Messagerie homepage ', () => {
-    it('should have a page title', () => {
+    xit('should have a page title', () => {
       cy.contains('Indicateurs')
     })
 
-    it('should have a card with action buttons', () => {
+    xit('should have a card with action buttons', () => {
       cy.get(MailCard).eq(1).contains('sujet 1')
       cy.get(MailCard).eq(1).contains('Brouillon')
 
       cy.get(DeleteButton).click()
       // cy.contains('Supprimer').click()
-      // cy.wait(2000)
+
       // cy.get(MailCard).eq(1).should('not.exist')
     })
 
     it('should have a card withoud action buttons', () => {
       cy.get(MailCard).first().contains('sujet 2')
       cy.get(MailCard).first().contains('Envoyé')
-      cy.get(MailCard).first().contains(`Le ${new Date(2021, 10, 2).toLocaleDateString()}`)
-
-    })
-
-    it('should have a button that redirects to email editor page', () => {
-      cy.contains('Envoyer un email').click()
+      cy.get(MailCard).first().contains(`Le ${new Date(2021, 10, 2, 12, 0).toLocaleDateString()}`)
     })
   })
 
-  // describe('Email editor page', () => {
-
-  // })
+  describe('Email editor page', () => {
+    it('should have a title', () => {
+      cy.contains('Envoyer un email').click()
+      cy.contains('Créer un message')
+      cy.get(MailObjectInput)
+    })
+  })
 
   
--- src/components/Messagerie/Template.js
+++ src/components/Messagerie/Template.js
@@ -79,6 +79,7 @@ const Template = ({ modeUpdate = false }) => {
             value={messageSubject}
             onChange={event => setMessageSubject(event.target.value)}
             sx={{ width: '100%' }}
+            data-cy="mail-editor-object"
           />
         </Grid>
         <Grid item xs={5} />
