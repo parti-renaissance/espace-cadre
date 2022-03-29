aeacf80496212c4b960e5671ed6c0e9c456a83e0
Marius <riamarius@gmail.com>
2022-03-29 12:13:19 +0200

    Add register page && validation

--- src/components/DTDLegislative/PollingStationSelect.js
+++ src/components/DTDLegislative/PollingStationSelect.js
@@ -1,3 +1,39 @@
-const PollingStationSelect = () => <div>PollingStationSelect</div>
+import TextField from 'ui/TextField'
+
+const PollingStationSelect = ({ formik, values, handleChange, handleSubmit, back, errors, touched, handleBlur }) => {
+  const firstNameHasError = errors.firstName && touched.firstName
+  const lastNameHasError = errors.lastName && touched.lastName
+
+  return (
+    <>
+      <TextField
+        formik={formik}
+        label="firstName"
+        placeholder="firstname"
+        onBlur={handleBlur}
+        defaultValue={values.firstName}
+        onChange={handleChange}
+        error={firstNameHasError}
+      />
+      <br />
+      <TextField
+        formik={formik}
+        label="lastName"
+        placeholder="lastName"
+        onBlur={handleBlur}
+        defaultValue={values.lastName}
+        onChange={handleChange}
+        error={lastNameHasError}
+      />
+      <br />
+      <button
+        disabled={!touched.firstName || !touched.lastName || lastNameHasError || firstNameHasError}
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+    </>
+  )
+}
 
 export default PollingStationSelect
--- src/components/DTDLegislative/index.js
+++ src/components/DTDLegislative/index.js
@@ -2,6 +2,9 @@ import { Grid, Container, Dialog, Button as MUIButton, Slide } from '@mui/materi
 import IconButton from '@mui/material/IconButton'
 import { ArrowBack as ArrowBackIcon, Close as CloseIcon } from '@mui/icons-material/'
 
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
 import Button from 'ui/Button'
 import Register from './Register'
 import Map from './Map'
@@ -10,12 +13,52 @@ import { useState, forwardRef } from 'react'
 import PollingStationSelect from './PollingStationSelect'
 import { Title } from './styles'
 
-const VIEW_MODE_REGISTER = 'register'
-const VIEW_MODE_SELECT = 'select'
-
 const Transition = forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />
 })
+
+const SignupSchema = Yup.object().shape({
+  title: Yup.string().required('title is required'),
+  objective: Yup.string().required('objective is required'),
+  startDate: Yup.string().required('startDate is required'),
+  endDate: Yup.string().required('endDate is required'),
+  brief: Yup.string().required('brief is required'),
+  survey: Yup.string().required('survey is required'),
+  firstName: Yup.string().required('firstName is required'),
+  lastName: Yup.string().required('lastName is required'),
+})
+
+const RenderStep = ({ formik, step, values, errors, touched, handleBlur, handleChange, handleSubmit, next, back }) => {
+  switch (step) {
+    case 1:
+      return (
+        <Register
+          formik={formik}
+          values={values}
+          errors={errors}
+          touched={touched}
+          handleChange={handleChange}
+          handleBlur={handleBlur}
+          next={next}
+        />
+      )
+    case 2:
+      return (
+        <PollingStationSelect
+          formik={formik}
+          values={values}
+          errors={errors}
+          touched={touched}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          handleBlur={handleBlur}
+          back={back}
+        />
+      )
+    default:
+      return <Register errors={errors} touched={touched} />
+  }
+}
 
 const messages = {
   title: 'Nouvelle campagne de porte à porte',
@@ -26,25 +69,82 @@ const messages = {
 
 function App() {
   const [open, setOpen] = useState(true)
-  const [viewMode, setViewMode] = useState(VIEW_MODE_REGISTER)
-  const shouldDisplayRegister = viewMode === VIEW_MODE_REGISTER
+  const [step, setStep] = useState(1)
+
+  const shouldDisplayRegister = step === 1
+
+  const formData = {
+    title: '',
+    objective: '',
+    startDate: null,
+    endDate: null,
+    brief: '',
+    survey: '',
+    firstName: '',
+    lastName: '',
+  }
+
+  const next = () => {
+    setStep(s => s + 1)
+  }
+
+  const back = () => {
+    setStep(s => s - 1)
+  }
+
+  const formik = useFormik({
+    initialValues: formData,
+    validationSchema: SignupSchema,
+    onSubmit: async values => {},
+  })
+
+  const isStepOneValid =
+    !formik.errors.title &&
+    !formik.errors.objective &&
+    !formik.errors.startDate &&
+    !formik.errors.endDate &&
+    !formik.errors.brief &&
+    !formik.errors.survey &&
+    formik.touched.title &&
+    formik.touched.objective
+
+  const isStepTwoValid =
+    !formik.errors.firstName && !formik.errors.lastName && formik.touched.firstName && formik.touched.lastName
+
+  const handleSubmit = () => {
+    console.log('===============Submit form=====================')
+    console.log(formik.values)
+    console.log('====================================')
+  }
 
   const handleClose = () => {
     setOpen(false)
     window.open('/porte-a-porte', '_self')
   }
 
-  const onRegisterSubmit = () => {
+  const ActionButton = () => {
     if (shouldDisplayRegister) {
-      setViewMode(VIEW_MODE_SELECT)
-    } else {
-      // ================Campagne crée!!====================
-      handleClose()
+      return (
+        <Button
+          type="submit"
+          onClick={next}
+          rootProps={{ sx: { color: 'whiteCorner', mr: 4 } }}
+          disabled={!isStepOneValid}
+        >
+          {messages.nextButton}
+        </Button>
+      )
     }
-  }
-
-  const onBackClickHandler = () => {
-    setViewMode(VIEW_MODE_REGISTER)
+    return (
+      <Button
+        type="submit"
+        onClick={handleSubmit}
+        rootProps={{ sx: { color: 'whiteCorner', mr: 4 } }}
+        disabled={!isStepTwoValid}
+      >
+        {messages.submitButton}
+      </Button>
+    )
   }
 
   return (
@@ -53,35 +153,33 @@ function App() {
         <Grid container sx={{ py: 4, pl: 2 }}>
           <Grid item xs={9}>
             {!shouldDisplayRegister && (
-              <MUIButton
-                startIcon={<ArrowBackIcon />}
-                onClick={onBackClickHandler}
-                size="large"
-                sx={{ color: 'main', mr: 4 }}
-              >
+              <MUIButton startIcon={<ArrowBackIcon />} onClick={back} size="large" sx={{ color: 'main', mr: 4 }}>
                 {messages.backButton}
               </MUIButton>
             )}
             <Title>{messages.title}</Title>
           </Grid>
           <Grid item xs={3} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Button
-              type="submit"
-              onClick={onRegisterSubmit}
-              rootProps={{ sx: { color: 'whiteCorner', mr: 4 } }}
-              disabled={false}
-            >
-              {shouldDisplayRegister ? messages.nextButton : messages.submitButton}
-            </Button>
-            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+            <ActionButton />
+            <IconButton edge="start" color="inherit" onClick={() => {}} aria-label="close">
               <CloseIcon />
             </IconButton>
           </Grid>
         </Grid>
         <Grid container sx={{ borderRadius: '12px', background: 'whiteCorner' }} className="main">
           <Grid item xs={6} sx={{ px: 4 }}>
-            {shouldDisplayRegister && <Register />}
-            {!shouldDisplayRegister && <PollingStationSelect />}
+            <RenderStep
+              formik={formik}
+              step={step}
+              values={formik.values}
+              errors={formik.errors}
+              touched={formik.touched}
+              handleBlur={formik.handleBlur}
+              handleChange={formik.handleChange}
+              handleSubmit={formik.handleSubmit}
+              next={next}
+              back={back}
+            />
           </Grid>
           <Grid item xs={6}>
             <Map />
