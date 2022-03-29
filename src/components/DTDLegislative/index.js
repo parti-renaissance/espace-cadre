2f0c33f8f87591791bca7d5daae538e8f7f59032
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-03-29 11:59:31 +0200

    Add responsive behaviour

--- src/components/DTDLegislative/index.js
+++ src/components/DTDLegislative/index.js
@@ -79,11 +79,11 @@ function App() {
           </Grid>
         </Grid>
         <Grid container sx={{ borderRadius: '12px', background: 'whiteCorner' }} className="main">
-          <Grid item xs={6} sx={{ px: 4 }}>
+          <Grid item xs={12} md={6} sx={{ px: 4 }}>
             {shouldDisplayRegister && <Register />}
             {!shouldDisplayRegister && <PollingStationSelect />}
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} md={6}>
             <Map />
           </Grid>
         </Grid>
