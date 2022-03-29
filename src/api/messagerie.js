367338ac7083790c6c813260672ebef255c1393e
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-03-29 15:30:29 +0200

    Revert "[Messages] replace segment endpoint by filter (#1244)"
    This reverts commit 4bc870cafef247a5334d19a4c9b5e022c2b5f175.

--- src/api/messagerie.js
+++ src/api/messagerie.js
@@ -34,12 +34,16 @@ export const messageSynchronizationStatus = async id => {
   return { synchronized: data.synchronized }
 }
 export const getMessageContent = id => apiClient.get(`/v3/adherent_messages/${id}/content`)
-export const getMessage = id => apiClient.get(`/v3/adherent_messages/${id}`)
-export const updateMessageFilter = ({ id, data }) => apiClient.put(`/v3/adherent_messages/${id}/filter`, data)
 export const createMessageContent = data => apiClient.post('/v3/adherent_messages', data)
 export const updateMessageContent = (id, data) => apiClient.put(`/v3/adherent_messages/${id}`, data)
 export const sendMessage = id => apiClient.post(`/v3/adherent_messages/${id}/send`)
 export const sendTestMessage = id => apiClient.post(`/v3/adherent_messages/${id}/send-test`)
+export const setMessageSegment = (messageId, segmentId) =>
+  apiClient.put(`/v3/adherent_messages/${messageId}/filter`, { segment: segmentId })
+
+export const getSegmentAudience = id => apiClient.get(`/v3/audience-segments/${id}`)
+export const createSegmentAudience = data => apiClient.post('/v3/audience-segments', data)
+export const updateSegmentAudience = audience => apiClient.put(`/v3/audience-segments/${audience.id}`, audience)
 
 export const reportsRatio = async () => {
   const data = await apiClientProxy.get('/mailCampaign/reportsRatios')
