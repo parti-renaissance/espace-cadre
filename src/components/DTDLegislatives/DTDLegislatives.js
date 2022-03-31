f643c28da9d986d90b9236f9bb5c144d6cc096f8
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-03-31 16:54:54 +0200

    Homepage for DTD legislatives

---
+++ src/components/DTDLegislatives/DTDLegislatives.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { Container, Grid } from '@mui/material'
+import PageHeader from 'ui/PageHeader'
+import { PageHeaderButton } from 'ui/PageHeader/PageHeader'
+import Modal from './Modal'
+
+const messages = {
+  title: 'Porte à porte législatives',
+  create: 'Créer une campagne',
+}
+
+const DTDLegislatives = () => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Container maxWidth="lg">
+      <Grid container justifyContent="space-between">
+        <PageHeader
+          title={messages.title}
+          button={<PageHeaderButton label={messages.create} onClick={() => setOpen(true)} isMainButton />}
+        />
+      </Grid>
+      {open && <Modal open={open} setOpen={setOpen} />}
+    </Container>
+  )
+}
+
+export default DTDLegislatives
