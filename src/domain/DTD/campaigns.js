f54187a5ad1c5fe8829a064b5645cc66c5225a77
Xavier Lacombe <xavier.lacombe@en-marche.fr>
2022-04-06 10:49:55 +0200

    Update DTD domain

--- src/domain/DTD/campaigns.js
+++ src/domain/DTD/campaigns.js
@@ -1,22 +1,33 @@
 import PropTypes from 'prop-types'
 
 export class DTDCampaignItemScore {
-  constructor(count, goal) {
-    this.count = count
+  constructor(surveysCount, goal, visitedDoors, addressesCount, votersCount, pollingStationsCount, collectedContacts) {
+    this.surveysCount = surveysCount
     this.goal = goal
+    this.visitedDoors = visitedDoors
+    this.addressesCount = addressesCount
+    this.votersCount = votersCount
+    this.pollingStationsCount = pollingStationsCount
+    this.collectedContacts = collectedContacts
   }
   static propTypes = {
     count: PropTypes.number.isRequired,
     goal: PropTypes.number.isRequired,
+    visitedDoors: PropTypes.number.isRequired,
+    addressesCount: PropTypes.number.isRequired,
+    votersCount: PropTypes.number.isRequired,
+    pollingStationsCount: PropTypes.number.isRequired,
+    collectedContacts: PropTypes.number.isRequired,
   }
 }
 
 export class DTDCampaignItem {
-  constructor(id, startDate, endDate, title, score) {
+  constructor(id, startDate, endDate, title, author, score) {
     this.id = id
     this.startDate = startDate
     this.endDate = endDate
     this.title = title
+    this.author = author
     this.score = score
   }
   static propTypes = {
@@ -24,6 +35,7 @@ export class DTDCampaignItem {
     startDate: PropTypes.object.isRequired,
     endDate: PropTypes.object.isRequired,
     title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
     score: PropTypes.shape(DTDCampaignItemScore.propTypes),
   }
 }
