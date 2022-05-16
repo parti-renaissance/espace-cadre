678a76e98f580fb6014aaad327c486afb015aa33
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2022-05-16 13:30:30 +0200

    Fix tests

--- cypress/integration/phoning-campaign.spec.js
+++ cypress/integration/phoning-campaign.spec.js
@@ -52,6 +52,11 @@ describe('PHONING > Campaign Detail', () => {
       '/api/v3/phoning_campaign_histories?campaign.uuid=11111111-1111-1111-1111-111111111111&order[created_at]=desc&page=1&page_size=20&scope=phoning_national_manager',
       'phoning/campaignDetail/histories'
     )
+    mock(
+      'GET',
+      '/api/v3/surveys?*',
+      'surveys/surveys'
+    )
     navigate()
   })
 
