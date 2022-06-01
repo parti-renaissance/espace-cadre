0871936f64d8ab5b1f6485c7cff616bae2db1a8a
Marius <riamarius@gmail.com>
2022-06-01 14:34:42 +0200

    Fix tests

--- cypress/integration/main.spec.js
+++ cypress/integration/main.spec.js
@@ -1,6 +1,6 @@
-export const mock = (method, url, fixture) => cy.intercept(method, url, { fixture }).as(fixture)
+const mock = (method, url, fixture) => cy.intercept(method, url, { fixture }).as(fixture)
 
-export const initialize = () => {
+const initialize = () => {
   cy.intercept('/api/**/*', (req) => {
     throw new Error('request not stubbed : '+req.url);
   })
