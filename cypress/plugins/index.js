551f77d0d0ab8ecf4eee75110f7b404de8000f50
Marius <riamarius@gmail.com>
2022-06-01 21:38:33 +0200

    Fix tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,64 +1,9 @@
 const path = require('path')
-const vite = require('vite')
-
-const cache = {}
+const { startDevServer } = require('@cypress/vite-dev-server')
 
 module.exports = (on, config) => {
-  on('file:preprocessor', async file => {
-    const { filePath, outputPath, shouldWatch } = file
-    if (cache[filePath]) {
-      return cache[filePath]
-    }
+  const viteConfig = { configFile: path.resolve(__dirname, '..', '..', 'vite.config.js') }
 
-    const filename = path.basename(outputPath)
-    const filenameWithoutExtension = path.basename(outputPath, path.extname(outputPath))
-
-    const viteConfig = {
-      build: {
-        emptyOutDir: false,
-        minify: false,
-        outDir: path.dirname(outputPath),
-        sourcemap: true,
-        write: true,
-      },
-    }
-
-    if (filename.endsWith('.html')) {
-      viteConfig.build.rollupOptions = {
-        input: {
-          [filenameWithoutExtension]: filePath,
-        },
-      }
-    } else {
-      viteConfig.build.lib = {
-        entry: filePath,
-        fileName: () => filename,
-        formats: ['es'],
-        name: filenameWithoutExtension,
-      }
-    }
-
-    if (shouldWatch) {
-      viteConfig.build.watch = true
-    }
-
-    const watcher = await vite.build(viteConfig)
-
-    if (shouldWatch) {
-      watcher.on('event', event => {
-        if (event.code === 'END') {
-          file.emit('rerun')
-        }
-      })
-      file.on('close', () => {
-        delete cache[filePath]
-        watcher.close()
-      })
-    }
-
-    cache[filePath] = outputPath
-    return outputPath
-  })
-
+  on('dev-server:start', options => startDevServer({ options, viteConfig }))
   return config
 }
--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,37 @@ import react from '@vitejs/plugin-react'
 
 const path = require('path')
 
-export default defineConfig({
-  plugins: [react(), VitePluginHtmlEnv(), EnvironmentPlugin('all', { prefix: 'REACT_APP_' })],
-  resolve: {
-    alias: {
-      api: path.resolve(__dirname, 'src/api'),
-      components: path.resolve(__dirname, 'src/components'),
-      domain: path.resolve(__dirname, 'src/domain'),
-      providers: path.resolve(__dirname, 'src/providers'),
-      assets: path.resolve(__dirname, 'src/assets'),
-      services: path.resolve(__dirname, 'src/services'),
-      style: path.resolve(__dirname, 'src/style'),
-      shared: path.resolve(__dirname, 'src/shared'),
-      ui: path.resolve(__dirname, 'src/ui'),
+export default defineConfig(generateConfig)
+
+export function generateConfig() {
+  return {
+    resolve: {
+      alias: {
+        api: path.resolve(__dirname, 'src/api'),
+        components: path.resolve(__dirname, 'src/components'),
+        domain: path.resolve(__dirname, 'src/domain'),
+        providers: path.resolve(__dirname, 'src/providers'),
+        assets: path.resolve(__dirname, 'src/assets'),
+        services: path.resolve(__dirname, 'src/services'),
+        style: path.resolve(__dirname, 'src/style'),
+        shared: path.resolve(__dirname, 'src/shared'),
+        ui: path.resolve(__dirname, 'src/ui'),
+      },
     },
-  },
-  server: {
-    open: '/',
-  },
-  build: {
-    outDir: 'build',
-  },
-})
+    plugins: [
+      process.env.MODE !== 'production'
+        ? react({
+            jsxRuntime: 'classic',
+          })
+        : react(),
+      VitePluginHtmlEnv(),
+      EnvironmentPlugin('all', { prefix: 'REACT_APP_' }),
+    ],
+    server: {
+      open: '/',
+    },
+    build: {
+      outDir: 'build',
+    },
+  }
+}
