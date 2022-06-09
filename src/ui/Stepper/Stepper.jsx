585ddc898c603363b763e22e1421ca9fffcc6f90
XavierLac <80252457+XavierLac@users.noreply.github.com>
2022-06-09 11:18:37 +0200

    [LPN-891] Improve event modal + clean stepper (#1637)

--- src/ui/Stepper/Stepper.jsx
+++ src/ui/Stepper/Stepper.jsx
@@ -1,8 +1,7 @@
 import PropTypes from 'prop-types'
-import React, { useCallback, useEffect, useState } from 'react'
+import React from 'react'
 import { styled } from '@mui/system'
 import {
-  Button,
   Stepper as MuiStepper,
   Step,
   StepButton as MuiStepButton,
@@ -22,8 +21,8 @@ const StepConnector = styled(MuiStepConnector)({
 const StepTitleButton = styled(
   MuiStepButton,
   shouldForwardProps
-)(({ theme, isCurrentStep }) => ({
-  cursor: !isCurrentStep ? 'pointer' : 'inherit',
+)(({ theme }) => ({
+  cursor: 'inherit',
   '& .MuiStepLabel-label.Mui-active, & .MuiStepLabel-label.Mui-completed, & .MuiStepLabel-label.Mui-disabled': {
     fontSize: '18px',
     fontWeight: 600,
@@ -35,58 +34,26 @@ const StepTitleButton = styled(
   },
 }))
 
-const messages = {
-  prev: 'Précédent',
-  next: 'Suivant',
-  finish: 'Terminer',
-}
-
-const Stepper = ({ validSteps = [], children, resetActiveStep, errors, ...props }) => {
-  const [activeStep, setActiveStep] = useState(0)
-  const handlePrevStep = useCallback(() => setActiveStep(prevStep => prevStep - 1), [])
-  const handleNextStep = useCallback(() => setActiveStep(prevStep => prevStep + 1), [])
-  const handleRestartFromStep = useCallback(
-    step => () => {
-      activeStep > step && setActiveStep(step)
-    },
-    [activeStep]
-  )
-
-  useEffect(() => {
-    resetActiveStep &&
-      resetActiveStep(() => {
-        setActiveStep(0)
-      })
-  }, [resetActiveStep])
-
-  if (!activeStep && activeStep !== 0) return null
-
-  return (
-    <MuiStepper connector={<StepConnector />} activeStep={activeStep} {...props}>
-      {React.Children.map(children, (step, index) => (
-        <Step key={index} expanded>
-          <Grid container direction="column">
-            <Grid item>
-              <StepTitleButton disabled={activeStep <= index} onClick={handleRestartFromStep(index)}>
-                {step.props.children.props.title}
-              </StepTitleButton>
-            </Grid>
-
-            <StepContent TransitionProps={{ unmountOnExit: false }}>
-              <Grid item>{React.cloneElement(step, step.props)}</Grid>
-            </StepContent>
+const Stepper = ({ children, ...props }) => (
+  <MuiStepper connector={<StepConnector />} {...props}>
+    {React.Children.map(children, (step, index) => (
+      <Step key={index} expanded>
+        <Grid container direction="column">
+          <Grid item>
+            <StepTitleButton>{step.props.children.props.title}</StepTitleButton>
           </Grid>
-        </Step>
-      ))}
-    </MuiStepper>
-  )
-}
+
+          <StepContent TransitionProps={{ unmountOnExit: false }}>
+            <Grid item>{React.cloneElement(step, step.props)}</Grid>
+          </StepContent>
+        </Grid>
+      </Step>
+    ))}
+  </MuiStepper>
+)
 
 Stepper.propTypes = {
-  validSteps: PropTypes.arrayOf(PropTypes.number).isRequired,
-  resetActiveStep: PropTypes.func,
   children: PropTypes.node.isRequired,
-  errors: PropTypes.bool,
 }
 
 export default Stepper
