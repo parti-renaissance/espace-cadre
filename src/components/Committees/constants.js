df8f744074d95f63d6cbec834affc146197e146e
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-03-06 11:53:45 +0100

    [Committee] add city_community zone type

--- src/components/Committees/constants.js
+++ src/components/Committees/constants.js
@@ -1,3 +1,9 @@
 import { zoneTypes } from 'domain/zone'
 
-export const committeeZones = [zoneTypes.CITY, zoneTypes.CANTON, zoneTypes.BOROUGH, zoneTypes.VOTE_PLACE]
+export const committeeZones = [
+  zoneTypes.CITY,
+  zoneTypes.CANTON,
+  zoneTypes.BOROUGH,
+  zoneTypes.VOTE_PLACE,
+  zoneTypes.CITY_COMMUNITY,
+]
