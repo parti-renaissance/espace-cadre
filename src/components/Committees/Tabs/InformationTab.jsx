71f16b56b8eae6ced79c7197e0acdf488cec91f4
Arthur Monney <monneylobe@gmail.com>
2023-03-16 14:40:22 +0100

    add Map on detail committee (#1991)
    * add Map on detail committee
    
    * remove unused state

--- src/components/Committees/Tabs/InformationTab.jsx
+++ src/components/Committees/Tabs/InformationTab.jsx
@@ -1,45 +1,53 @@
-import { Box, Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography, Alert } from '@mui/material'
 import PropTypes from 'prop-types'
 import { format } from 'date-fns'
 import UICard from 'ui/Card'
 import { LineContent } from '../styles'
+import ZoneContext from 'providers/context'
 import ZonesAccordion from '../Zone/Accordions'
+import Map from '../Map'
 
 const InformationTab = ({ committee }) => (
-  <Grid container spacing={2}>
-    <Grid item xs={12} md={6}>
-      <UICard
-        rootProps={{ sx: { overflow: 'hidden', pt: 1, pr: 0 } }}
-        content={
-          <>
-            <LineContent label="Nom du comité" value={committee.name} />
-            <LineContent
-              label="Date de création"
-              value={format(new Date(committee.created_at), 'dd/MM/yyyy à HH:mm:ss')}
-            />
-            <LineContent label="Description" value={committee.description} />
-            <Box sx={{ px: 3, py: 2 }}>
-              <Typography sx={{ fontSize: '14px', fontWeight: '500', color: theme => theme.palette.colors.gray[500] }}>
-                Zones
-              </Typography>
-              <Box
-                sx={{
-                  mt: 1.5,
-                  border: '1px solid',
-                  color: theme => theme.palette.colors.gray[200],
-                  borderRadius: '8px',
-                  overflow: 'hidden',
-                }}
-              >
-                <ZonesAccordion selectedZones={committee.zones} />
+  <ZoneContext.Provider value={{ zones: committee.zones }}>
+    <Grid container spacing={2}>
+      <Grid item xs={12} md={6}>
+        <UICard
+          rootProps={{ sx: { overflow: 'hidden', pt: 1, pr: 0 } }}
+          content={
+            <>
+              <LineContent label="Nom du comité" value={committee.name} />
+              <LineContent
+                label="Date de création"
+                value={format(new Date(committee.created_at), 'dd/MM/yyyy à HH:mm:ss')}
+              />
+              <LineContent label="Description" value={committee.description} />
+              <Box sx={{ px: 3, py: 2 }} className="space-y-3">
+                <Typography
+                  sx={{ fontSize: '14px', fontWeight: '500', color: theme => theme.palette.colors.gray[500] }}
+                >
+                  Zones
+                </Typography>
+                <Alert severity="info">Les zones en vert foncé sont les zones appartenant à ce comité</Alert>
+                <Box
+                  sx={{
+                    border: '1px solid',
+                    color: theme => theme.palette.colors.gray[200],
+                    borderRadius: '8px',
+                    overflow: 'hidden',
+                  }}
+                >
+                  <ZonesAccordion selectedZones={committee.zones} />
+                </Box>
               </Box>
-            </Box>
-          </>
-        }
-      />
+            </>
+          }
+        />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Map />
+      </Grid>
     </Grid>
-    <Grid item xs={12} md={6}></Grid>
-  </Grid>
+  </ZoneContext.Provider>
 )
 
 export default InformationTab
