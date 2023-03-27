64a7eeed9863df84b3700e546126b1bee4e737da
Arthur Monney <monneylobe@gmail.com>
2023-03-27 16:13:31 +0200

    fix bug on select elections date

--- src/components/Committees/Tabs/ElectionsTab.jsx
+++ src/components/Committees/Tabs/ElectionsTab.jsx
@@ -117,7 +117,7 @@ const ElectionsTab = ({ committee, committeeElectionId }) => {
         <CreateEditModal
           committeeUuid={committee.uuid}
           designation={designation}
-          status={committeeElection?.status}
+          status={committeeElection.status}
           handleClose={() => toggleCreateEditModal(designation, false)}
         />
       )}
