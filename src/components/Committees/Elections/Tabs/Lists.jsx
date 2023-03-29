d6c40b737455627f86596c8f5faa4c0859e7763a
Arthur Monney <monneylobe@gmail.com>
2023-03-29 16:43:20 +0200

    Update elections tabs screen

--- src/components/Committees/Elections/Tabs/Lists.jsx
+++ src/components/Committees/Elections/Tabs/Lists.jsx
@@ -1,9 +1,10 @@
+import PropTypes from 'prop-types'
 import { useMemo, useState } from 'react'
-import { Accordion, AccordionDetails, AccordionSummary, Box, IconButton, Grid, Typography } from '@mui/material'
+import { Box, IconButton, Grid, Typography, LinearProgress, linearProgressClasses } from '@mui/material'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { styled } from '@mui/system'
 import DeleteIcon from '@mui/icons-material/Delete'
 import AddIcon from '@mui/icons-material/Add'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import { createGroup, removeCandidate, deleteGroup } from 'api/committee_election'
 import { useCustomSnackbar } from 'components/shared/notification/hooks'
 import { useErrorHandler } from 'components/shared/error/hooks'
@@ -11,17 +12,30 @@ import { notifyVariants } from 'components/shared/notification/constants'
 import { CommitteeElection } from 'domain/committee_election'
 import Button from 'ui/Button'
 import Loader from 'ui/Loader'
+import { UIChip } from 'ui/Card'
 import AddCandidateModal from '../AddCandidateModal'
+import { GroupList } from '../../styles'
 
 const messages = {
   add: 'Ajouter',
-  addCandidate: 'Ajouter un candidat',
   noCandidate: 'Cette liste ne possède aucun candidat',
   list: 'Listes',
   description: 'Listes de tous les candidats de votre élection',
 }
 
-const Lists = ({ election }) => {
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: theme.palette.colors.gray[200],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.colors.blue[500],
+  },
+}))
+
+const Lists = ({ election, isResultsLoading, results }) => {
   const { handleError, errorMessages } = useErrorHandler()
   const { enqueueSnackbar } = useCustomSnackbar()
   const enabledAction = election.isEditable()
@@ -59,7 +73,9 @@ const Lists = ({ election }) => {
     >
       <Box maxWidth="40%">
         <Box display="flex" alignItems="center" className="space-x-4">
-          <Typography sx={{ color: 'gray900', fontSize: '24px', fontWeight: '500' }}>Candidatures</Typography>
+          <Typography sx={{ color: 'gray900', fontSize: '24px', fontWeight: '500' }}>
+            Candidatures {isResultsLoading && <Loader size={20} />}
+          </Typography>
           {enabledAction && (
             <Button onClick={addList} isMainButton>
               <AddIcon sx={{ color: 'main', fontSize: '20px' }} />
@@ -73,100 +89,95 @@ const Lists = ({ election }) => {
         </Typography>
       </Box>
 
-      <Grid container spacing={3} sx={{ mt: 1 }}>
-        {election.groups.map((list, index) => (
-          <Grid item xs={12} sm={6} lg={4} key={list.uuid} data-cy="election-list-card">
-            <Box
-              display="flex"
-              alignItems="center"
-              justifyContent="space-between"
-              sx={{
-                bgcolor: 'whiteCorner',
-                px: 1.5,
-                py: 1,
-                borderTopRightRadius: '6px',
-                borderTopLeftRadius: '6px',
-              }}
-            >
-              <Typography sx={{ fontSize: '16px', color: theme => theme.palette.colors.gray[500] }}>
-                Liste {index + 1}
-              </Typography>
-              {list.candidacies.length === 0 && enabledAction && (
-                <IconButton
-                  edge="start"
-                  color="inherit"
-                  onClick={() => deleteList(list.uuid)}
-                  aria-label="delete"
-                  sx={{ ml: 0.25 }}
-                >
-                  <DeleteIcon sx={{ color: theme => theme.palette.form.error.color, fontSize: '20px' }} />
-                </IconButton>
-              )}
-            </Box>
-            <Accordion>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls={`panel-content-${index + 1}`}
-                id={`panel-header-${index + 1}`}
-              >
-                <Box
-                  sx={{ display: 'flex', flex: '1 1 0%', justifyContent: 'space-between', alignItems: 'center' }}
-                  className="space-x-4"
-                >
-                  <Typography>Les candidats</Typography>
-                </Box>
-              </AccordionSummary>
-              <AccordionDetails>
-                <Box>
-                  {list.candidacies.length === 0 && <Typography>{messages.noCandidate}</Typography>}
-                  {list.candidacies.length > 0 &&
-                    list.candidacies.map(candidate => (
-                      <Box display="flex" alignItems="center" my={1.5} key={candidate.uuid}>
-                        <Typography sx={{ color: theme => theme.palette.colors.gray[900] }}>
-                          {candidate.committee_membership.adherent.first_name}{' '}
-                          {candidate.committee_membership.adherent.last_name}
-                        </Typography>
-                        {enabledAction && (
-                          <IconButton
-                            edge="start"
-                            color="inherit"
-                            onClick={() => deleteCandidate(candidate.uuid)}
-                            aria-label="delete"
-                            sx={{ ml: 1 }}
-                          >
-                            <DeleteIcon sx={{ color: theme => theme.palette.form.error.color, fontSize: '20px' }} />
-                          </IconButton>
-                        )}
-                      </Box>
-                    ))}
-                </Box>
-                {enabledAction && (
-                  <Box
-                    display="flex"
-                    alignItems="center"
-                    sx={{
-                      borderTop: '1px solid',
-                      borderTopColor: theme => theme.palette.colors.gray[200],
-                      pt: 1.5,
-                      mt: 2,
-                    }}
-                  >
-                    <Button
-                      onClick={() => toggleCreateEditModal(list.uuid, true)}
-                      aria-label="add"
-                      isMainButton
-                      rootProps={{ sx: { fontSize: '12px', px: 1.5 } }}
-                    >
-                      <AddIcon sx={{ color: theme => theme.palette.colors.blue[500], fontSize: '20px' }} />
-                      {messages.addCandidate}
-                    </Button>
+      {election.status === 'closed' && results ? (
+        <Grid container spacing={3} sx={{ mt: 1 }}>
+          {results.candidate_group_results.map((item, index) => (
+            <GroupList
+              xs={12}
+              sm={6}
+              lg={4}
+              key={index}
+              index={index}
+              header={
+                <Box flex="1 1 0%" py={0.5}>
+                  <Box display="flex" alignItems="center" justifyContent="space-between">
+                    <Typography sx={{ fontSize: '16px', color: 'colors.gray.600' }}>
+                      {item.candidate_group.title}
+                    </Typography>
+                    {item.candidate_group.elected && (
+                      <UIChip
+                        label="Liste élue"
+                        labelStyle={{ fontSize: '14px' }}
+                        color="teal700"
+                        bgcolor="activeLabel"
+                      />
+                    )}
                   </Box>
-                )}
-              </AccordionDetails>
-            </Accordion>
-          </Grid>
-        ))}
-      </Grid>
+                  <Box mt={1.5} display="flex" alignItems="center" justifyContent="space-between">
+                    <Box flex="1 1 0%">
+                      <BorderLinearProgress variant="determinate" value={item.rate} />
+                    </Box>
+                    <Typography sx={{ fontSize: '14px', color: 'colors.gray.600', ml: 2 }}>
+                      {item.total} votes ({item.rate.toFixed(2)}%)
+                    </Typography>
+                  </Box>
+                </Box>
+              }
+            >
+              <Box className="space-y-2">
+                {item.candidate_group.candidates.map((candidate, index) => (
+                  <Typography key={index} component="h3" sx={{ color: 'colors.gray.900' }}>
+                    {candidate.first_name} {candidate.last_name}
+                  </Typography>
+                ))}
+              </Box>
+            </GroupList>
+          ))}
+        </Grid>
+      ) : (
+        <Grid container spacing={3} sx={{ mt: 1 }}>
+          {election.groups.map((list, index) => (
+            <GroupList
+              xs={12}
+              sm={6}
+              lg={4}
+              key={list.uuid}
+              index={list.uuid}
+              header={
+                <Typography sx={{ fontSize: '16px', color: theme => theme.palette.colors.gray[500] }}>
+                  Liste {index + 1}
+                </Typography>
+              }
+              canDropList={list.candidacies.length === 0 && enabledAction}
+              dropList={list => deleteList(list.uuid)}
+              canAddCandidate={enabledAction}
+              addCandidate={() => toggleCreateEditModal(list.uuid, true)}
+            >
+              {list.candidacies.length === 0 && <Typography>{messages.noCandidate}</Typography>}
+              {list.candidacies.length > 0 &&
+                list.candidacies.map(candidate => (
+                  <Box display="flex" alignItems="center" my={1.5} key={candidate.uuid}>
+                    <Typography sx={{ color: 'colors.gray.900' }}>
+                      {candidate.committee_membership.adherent.first_name}{' '}
+                      {candidate.committee_membership.adherent.last_name}
+                    </Typography>
+                    {enabledAction && (
+                      <IconButton
+                        edge="start"
+                        color="inherit"
+                        onClick={() => deleteCandidate(candidate.uuid)}
+                        aria-label="delete"
+                        sx={{ ml: 1 }}
+                      >
+                        <DeleteIcon sx={{ color: 'form.error.color', fontSize: '20px' }} />
+                      </IconButton>
+                    )}
+                  </Box>
+                ))}
+            </GroupList>
+          ))}
+        </Grid>
+      )}
 
       {isCreateEditModalOpen && (
         <AddCandidateModal
@@ -184,4 +195,6 @@ export default Lists
 
 Lists.propTypes = {
   election: CommitteeElection.propTypes.isRequired,
+  isResultsLoading: PropTypes.bool,
+  results: PropTypes.object,
 }
--- src/components/Committees/styles.jsx
+++ src/components/Committees/styles.jsx
@@ -1,6 +1,19 @@
-import { Grid, Tab as MuiTab, Typography } from '@mui/material'
-import { styled } from '@mui/system'
 import PropTypes from 'prop-types'
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Grid,
+  IconButton,
+  Tab as MuiTab,
+  Typography,
+} from '@mui/material'
+import { styled } from '@mui/system'
+import DeleteIcon from '@mui/icons-material/Delete'
+import AddIcon from '@mui/icons-material/Add'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Button from 'ui/Button'
 
 export const Tab = styled(MuiTab)(({ theme }) => ({
   textTransform: 'none',
@@ -16,20 +29,24 @@ export const TabLabel = styled(Typography)`
   line-height: 24px;
 `
 
-export const LineContent = ({ label, value }) => (
+export const LineContent = ({ label, value, labelStyle = {}, bodyStyle = {}, sx = { py: 1.5 } }) => (
   <Grid
     container
     spacing={3}
-    sx={{ py: 1.5, px: 1, borderBottom: '1px solid', borderBottomColor: theme => theme.palette.colors.gray[200] }}
+    sx={{ px: 1, borderBottom: '1px solid', borderBottomColor: theme => theme.palette.colors.gray[200], ...sx }}
   >
     <Grid item xs={12} md={4}>
-      <Typography sx={{ fontSize: '14px', fontWeight: '500', color: theme => theme.palette.colors.gray[500] }}>
+      <Typography
+        sx={{ fontSize: '14px', fontWeight: '500', color: theme => theme.palette.colors.gray[500], ...labelStyle }}
+      >
         {label}
       </Typography>
     </Grid>
     <Grid item xs={12} md={8}>
       {value instanceof String ? (
-        <Typography sx={{ fontSize: '14px', color: theme => theme.palette.colors.gray[900] }}>{value}</Typography>
+        <Typography sx={{ fontSize: '14px', color: theme => theme.palette.colors.gray[900], ...bodyStyle }}>
+          {value}
+        </Typography>
       ) : (
         value
       )}
@@ -40,4 +57,116 @@ export const LineContent = ({ label, value }) => (
 LineContent.propTypes = {
   label: PropTypes.string,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  labelStyle: PropTypes.object,
+  bodyStyle: PropTypes.object,
+  sx: PropTypes.object,
+}
+
+export const ResultCard = ({ title, icon, value }) => (
+  <Grid item xs={12} md={3}>
+    <Typography component="h4" sx={{ fontSize: '16px', color: 'colors.gray.700' }}>
+      {title}
+    </Typography>
+    <Box display="flex" alignItems="center" mt={0.5} className="space-x-2">
+      {icon}{' '}
+      <Typography component="h3" sx={{ fontSize: '32px', color: 'colors.gray.900', fontWeight: '500' }}>
+        {value}
+      </Typography>
+    </Box>
+  </Grid>
+)
+
+ResultCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.node.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+}
+
+export const GroupList = ({
+  header,
+  canDropList,
+  dropList,
+  index,
+  canAddCandidate,
+  addCandidate,
+  children,
+  ...props
+}) => (
+  <Grid item data-cy="election-list-card" {...props}>
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      sx={{
+        bgcolor: 'whiteCorner',
+        px: 1.5,
+        py: 1,
+        borderTopRightRadius: '6px',
+        borderTopLeftRadius: '6px',
+      }}
+    >
+      {header}
+      {canDropList && (
+        <IconButton edge="start" color="inherit" onClick={dropList} aria-label="delete" sx={{ ml: 0.25 }}>
+          <DeleteIcon sx={{ color: theme => theme.palette.form.error.color, fontSize: '20px' }} />
+        </IconButton>
+      )}
+    </Box>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls={`panel-content-${index}`}
+        id={`panel-header-${index}`}
+      >
+        <Box
+          sx={{ display: 'flex', flex: '1 1 0%', justifyContent: 'space-between', alignItems: 'center' }}
+          className="space-x-4"
+        >
+          <Typography>Les candidats</Typography>
+        </Box>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Box>{children}</Box>
+        {canAddCandidate && (
+          <Box
+            display="flex"
+            alignItems="center"
+            sx={{
+              borderTop: '1px solid',
+              borderTopColor: theme => theme.palette.colors.gray[200],
+              pt: 1.5,
+              mt: 2,
+            }}
+          >
+            <Button
+              onClick={addCandidate}
+              aria-label="add"
+              isMainButton
+              rootProps={{ sx: { fontSize: '12px', px: 1.5 } }}
+            >
+              <AddIcon sx={{ color: theme => theme.palette.colors.blue[500], fontSize: '20px' }} />
+              Ajouter un candidat
+            </Button>
+          </Box>
+        )}
+      </AccordionDetails>
+    </Accordion>
+  </Grid>
+)
+
+GroupList.defaultProps = {
+  canDropList: false,
+  dropList: () => {},
+  canAddCandidate: false,
+  addCandidate: () => {},
+}
+
+GroupList.propTypes = {
+  header: PropTypes.node.isRequired,
+  index: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  canDropList: PropTypes.bool.isRequired,
+  dropList: PropTypes.func,
+  canAddCandidate: PropTypes.bool.isRequired,
+  addCandidate: PropTypes.func,
+  children: PropTypes.node,
 }
