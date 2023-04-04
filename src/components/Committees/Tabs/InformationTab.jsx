88cd400e18a36eceed76af5e3ca89f982028a9ec
Arthur Monney <monneylobe@gmail.com>
2023-04-04 16:51:25 +0200

    Add animator modal form to committee (#2097)

--- src/components/Committees/Tabs/InformationTab.jsx
+++ src/components/Committees/Tabs/InformationTab.jsx
@@ -1,61 +1,90 @@
+import { useState } from 'react'
 import { Box, Grid, Typography, Alert } from '@mui/material'
 import PropTypes from 'prop-types'
 import { format } from 'date-fns'
 import UICard from 'ui/Card'
 import pluralize from 'components/shared/pluralize/pluralize'
 import ZoneContext from 'providers/context'
+import { getFullName } from 'shared/helpers'
+import Button from 'ui/Button/Button'
 import Map from '../Map'
 import { LineContent } from '../styles'
 import ZonesAccordion from '../Zone/Accordions'
+import UpdateAnimatorModal from './UpdateAnimatorModal'
 
-const InformationTab = ({ committee }) => (
-  <ZoneContext.Provider value={{ zones: committee.zones }}>
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={6}>
-        <UICard
-          rootProps={{ sx: { overflow: 'hidden', pt: 1, pr: 0 } }}
-          content={
-            <>
-              <LineContent label="Nom du comité" value={committee.name} />
-              <LineContent
-                label="Date de création"
-                value={format(new Date(committee.created_at), 'dd/MM/yyyy à HH:mm:ss')}
-              />
-              <LineContent label="Description" value={committee.description} />
-              <LineContent
-                label="Membres"
-                value={`${committee.members_count} ${pluralize(committee.members_count, 'adhérent')} et ${
-                  committee.sympathizers_count
-                } ${pluralize(committee.sympathizers_count, 'sympathisant')}`}
-              />
-              <Box sx={{ px: 3, py: 2 }} className="space-y-3">
-                <Typography
-                  sx={{ fontSize: '14px', fontWeight: '500', color: theme => theme.palette.colors.gray[500] }}
-                >
-                  Zones
-                </Typography>
-                <Alert severity="info">Les zones en vert foncé sont les zones appartenant à ce comité</Alert>
-                <Box
-                  sx={{
-                    border: '1px solid',
-                    color: theme => theme.palette.colors.gray[200],
-                    borderRadius: '8px',
-                    overflow: 'hidden',
-                  }}
-                >
-                  <ZonesAccordion selectedZones={committee.zones} />
+const InformationTab = ({ committee }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  return (
+    <ZoneContext.Provider value={{ zones: committee.zones }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <UICard
+            rootProps={{ sx: { overflow: 'hidden', pt: 1, pr: 0 } }}
+            content={
+              <>
+                <LineContent label="Nom du comité" value={committee.name} />
+                <LineContent
+                  label="Date de création"
+                  value={format(new Date(committee.created_at), 'dd/MM/yyyy à HH:mm:ss')}
+                />
+                <LineContent label="Description" value={committee.description} />
+                <LineContent
+                  label="Membres"
+                  value={`${committee.members_count} ${pluralize(committee.members_count, 'adhérent')} et ${
+                    committee.sympathizers_count
+                  } ${pluralize(committee.sympathizers_count, 'sympathisant')}`}
+                />
+                <LineContent
+                  label="Président du comité"
+                  value={
+                    <Box display="flex" alignItems="center" justifyContent="space-between">
+                      <Typography sx={{ fontSize: '14px', color: 'colors.gray.900' }}>
+                        {committee.animator ? getFullName(committee.animator) : 'Aucun'}
+                      </Typography>
+                      <Button isMainButton onClick={() => setIsModalOpen(true)}>
+                        {committee.animator ? 'Modifier' : 'Ajouter'}
+                      </Button>
+                    </Box>
+                  }
+                />
+                <Box sx={{ pr: 2.5, pl: 1, py: 2 }} className="space-y-3">
+                  <Typography
+                    sx={{ fontSize: '14px', fontWeight: '500', color: theme => theme.palette.colors.gray[500] }}
+                  >
+                    Zones
+                  </Typography>
+                  <Alert severity="info">Les zones en vert foncé sont les zones appartenant à ce comité</Alert>
+                  <Box
+                    sx={{
+                      border: '1px solid',
+                      color: theme => theme.palette.colors.gray[200],
+                      borderRadius: '8px',
+                      overflow: 'hidden',
+                    }}
+                  >
+                    <ZonesAccordion selectedZones={committee.zones} />
+                  </Box>
                 </Box>
-              </Box>
-            </>
-          }
+              </>
+            }
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Map />
+        </Grid>
+      </Grid>
+
+      {isModalOpen && (
+        <UpdateAnimatorModal
+          committeeId={committee.uuid}
+          animatorId={committee.animator?.uuid}
+          handleClose={() => setIsModalOpen(false)}
         />
-      </Grid>
-      <Grid item xs={12} md={6}>
-        <Map />
-      </Grid>
-    </Grid>
-  </ZoneContext.Provider>
-)
+      )}
+    </ZoneContext.Provider>
+  )
+}
 
 export default InformationTab
 
