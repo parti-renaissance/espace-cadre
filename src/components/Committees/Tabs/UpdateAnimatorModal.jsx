a6566b3c99af7848c7054e0ae78a6e14ca253291
Arthur Monney <monneylobe@gmail.com>
2023-04-05 13:34:22 +0200

    add committee and renaissance membership on animator modal (#2098)

--- src/components/Committees/Tabs/UpdateAnimatorModal.jsx
+++ src/components/Committees/Tabs/UpdateAnimatorModal.jsx
@@ -46,6 +46,7 @@ const UpdateAnimatorModal = ({ committeeId, animatorId, handleClose }) => {
             customStyle={{ bgcolor: 'colors.gray.50' }}
             value={selectedAdherent}
             onChange={setSelectedAdherent}
+            initialParams={{ committee: committeeId, renaissance_membership: 'adherent_re' }}
           />
         </Box>
       </Box>
