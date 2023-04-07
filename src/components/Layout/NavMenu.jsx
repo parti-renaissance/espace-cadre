22fadd2fcb73a7820e5e6ad0d2e1e74e7273244f
Arthur Monney <monneylobe@gmail.com>
2023-04-07 11:27:14 +0200

    Add button to toggle menu visibility (#2112)

--- src/components/Layout/NavMenu.jsx
+++ src/components/Layout/NavMenu.jsx
@@ -1,4 +1,7 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
+import AddIcon from '@mui/icons-material/Add'
+import RemoveIcon from '@mui/icons-material/Remove'
 import paths from 'shared/paths'
 import { featuresLabels } from 'shared/features'
 import NavItem from 'ui/NavItem/NavItem'
@@ -6,23 +9,31 @@ import { useUserScope } from '../../redux/user/hooks'
 
 export const NavMenu = ({ handleItemClick, group }) => {
   const [currentScope] = useUserScope()
+  const [show, setShow] = useState(true)
 
   return (
     <div className="menu-list mt-6">
-      <h5 className="menu-list__header">{group.label}</h5>
-      <nav className="menu-list__links">
-        {group.features.map(
-          featureKey =>
-            currentScope.hasFeature(featureKey) && (
-              <NavItem
-                key={featureKey}
-                path={paths[featureKey]}
-                label={featuresLabels[featureKey]}
-                handleClick={handleItemClick}
-              />
-            )
-        )}
-      </nav>
+      <div className="menu-list-header">
+        <h5 className="menu-list-header__title">{group.label}</h5>
+        <button onClick={() => setShow(!show)} type="button" className="no-style button">
+          {show ? <RemoveIcon sx={{ fontSize: '18px' }} /> : <AddIcon sx={{ fontSize: '18px' }} />}
+        </button>
+      </div>
+      {show && (
+        <nav className="menu-list__links">
+          {group.features.map(
+            featureKey =>
+              currentScope.hasFeature(featureKey) && (
+                <NavItem
+                  key={featureKey}
+                  path={paths[featureKey]}
+                  label={featuresLabels[featureKey]}
+                  handleClick={handleItemClick}
+                />
+              )
+          )}
+        </nav>
+      )}
     </div>
   )
 }
