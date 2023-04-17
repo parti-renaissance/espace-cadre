3d0c16b9fd5d56bd8aded82f46591897a7b78d43
Arthur Monney <monneylobe@gmail.com>
2023-04-17 10:59:55 +0200

    update zone selection when updating elected and remove vote_place zone

--- src/components/Filters/Element/ZoneAutocomplete.jsx
+++ src/components/Filters/Element/ZoneAutocomplete.jsx
@@ -1,20 +1,36 @@
 import PropTypes from 'prop-types'
+import qs from 'qs'
+import { Box, Typography } from '@mui/material'
 import Autocomplete from 'components/Filters/Element/Autocomplete'
+import { zoneLabels, zoneTypeColors } from 'domain/zone'
+import { UIChip } from 'ui/Card'
 
 export const ZONE_AUTOCOMPLETE_URI = '/api/v3/zone/autocomplete'
 
-const ZoneAutocomplete = ({ onChange, value, customStyle, ...props }) => (
-  <Autocomplete
-    onChange={onChange}
-    value={value}
-    customStyle={customStyle}
-    uri={ZONE_AUTOCOMPLETE_URI}
-    queryParam="q"
-    valueParam="uuid"
-    getOptionLabel={option => `${option.name} (${option.code})`}
-    {...props}
-  />
-)
+const ZoneAutocomplete = ({ onChange, value, customStyle, initialParams = {}, ...props }) => {
+  const fullUri = `${ZONE_AUTOCOMPLETE_URI}?${qs.stringify(initialParams)}`
+
+  return (
+    <Autocomplete
+      onChange={onChange}
+      value={value}
+      customStyle={customStyle}
+      uri={fullUri}
+      queryParam="q"
+      valueParam="uuid"
+      getOptionLabel={option => `${option.name} (${option.code})`}
+      renderOption={(props, option) => (
+        <li {...props} key={option.uuid}>
+          <Box className="space-x-4">
+            <Typography size="small">{`${option.name} (${option.code})`}</Typography>
+            <UIChip color="whiteCorner" bgcolor={zoneTypeColors[option.type]} label={zoneLabels[option.type]} />
+          </Box>
+        </li>
+      )}
+      {...props}
+    />
+  )
+}
 
 export default ZoneAutocomplete
 
@@ -22,4 +38,5 @@ ZoneAutocomplete.propTypes = {
   onChange: PropTypes.func.isRequired,
   value: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   customStyle: PropTypes.object,
+  initialParams: PropTypes.object,
 }
