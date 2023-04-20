a90a04479bbd5c4de857f85954f351142b95495a
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-04-20 15:24:05 +0200

    Update url for leaving impersonnification

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { userLogout } from '../../redux/auth'
-import { API_HOST, OAUTH_HOST } from 'shared/environments'
+import { RENAISSANCE_HOST, OAUTH_HOST } from 'shared/environments'
 import { isSwitchUser } from '../../redux/user/selectors'
 
 const Logout = () => {
@@ -10,7 +10,7 @@ const Logout = () => {
 
   useEffect(() => {
     const logOutUrl = isSwitchedUser
-      ? `${API_HOST}/admin/app/adherent/list?_switch_user=_exit`
+      ? `${RENAISSANCE_HOST}/admin/app/adherent/list?_switch_user=_exit`
       : `${OAUTH_HOST}/deconnexion`
     dispatch(userLogout(isSwitchedUser))
     window.location.href = logOutUrl
