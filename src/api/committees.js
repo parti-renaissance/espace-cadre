6ef305a0eca17d81e5819a5a82a2fc26581361af
Arthur Monney <monneylobe@gmail.com>
2023-04-28 11:41:45 +0200

    Add delete committee feature (#2190)

--- src/api/committees.js
+++ src/api/committees.js
@@ -22,3 +22,4 @@ export const updateAnimator = async ({ committeeId, animatorId }) =>
 export const getUsedZones = async () => await apiClient.get('/v3/committees/used-zones')
 export const createCommittee = async committee => await apiClient.post('/v3/committees', committee)
 export const updateCommittee = async committee => await apiClient.put(`/v3/committees/${committee.uuid}`, committee)
+export const deleteCommittee = async committeeId => await apiClient.delete(`/v3/committees/${committeeId}`)
