c9b1d3a16067aa3710b36c9b2a02c4c2fb3e0c5b
Arthur Monney <monneylobe@gmail.com>
2023-06-22 18:03:30 +0200

    Add committee election status color (#2348)
    * Add committee election status color
    
    * Update wording

--- src/components/Committees/Status.jsx
+++ src/components/Committees/Status.jsx
@@ -10,7 +10,40 @@ const values = {
   [electionStatus.canceled]: 'Annulée',
 }
 
-const Status = ({ status }) => <UIChip label={values[status] || status} color="teal700" bgcolor="activeLabel" />
+const Status = ({ status, ...props }) => {
+  let defaultProperties = {
+    label: typeof values[status] !== 'undefined' ? values[status] : 'Statut inconnu',
+    color: 'colors.gray.800',
+    bgcolor: 'colors.gray.100',
+  }
+  let properties = defaultProperties
+
+  if (electionStatus.canceled === status) {
+    properties = Object.assign(defaultProperties, {
+      label: values[status],
+      color: 'red600',
+      bgcolor: 'inactiveLabel',
+    })
+  }
+
+  if (electionStatus.closed === status) {
+    properties = Object.assign(defaultProperties, {
+      label: values[status],
+      color: 'teal700',
+      bgcolor: 'activeLabel',
+    })
+  }
+
+  if ([electionStatus.scheduled, electionStatus.in_progress].includes(status)) {
+    properties = Object.assign(defaultProperties, {
+      label: values[status],
+      color: 'yellow800',
+      bgcolor: 'pendingLabel',
+    })
+  }
+
+  return <UIChip {...properties} {...props} />
+}
 
 export default Status
 
