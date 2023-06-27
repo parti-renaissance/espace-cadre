6f79f173324c21b15075f495a941650d12fce307
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-06-27 12:01:42 +0200

    Use createSelector for currentScope and prevent rerendering (#2360)

--- src/redux/user/selectors.js
+++ src/redux/user/selectors.js
@@ -1,11 +1,15 @@
 import { APP_VERSION } from 'shared/environments'
 import { Scope } from 'domain/scope'
+import { createSelector } from '@reduxjs/toolkit'
 
 export const getCurrentUser = state => state.auth.user
 
 export const getUserScopes = state => state.auth.scopes || []
 
-export const getCurrentScope = state => (state.auth.currentScope ? new Scope(state.auth.currentScope) : null)
+export const getCurrentScope = createSelector(
+  state => (state.auth.currentScope ? new Scope(state.auth.currentScope) : null),
+  scope => scope
+)
 
 export const isUserLogged = state => state.auth.isUserLogged && state.auth.appVersion === APP_VERSION
 
