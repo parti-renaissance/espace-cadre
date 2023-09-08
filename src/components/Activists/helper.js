3de501b13425eb13d7f9f1c7776c72e1ba2efe42
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-09-08 09:28:01 +0200

    Update mandate badge color

--- src/components/Activists/helper.js
+++ src/components/Activists/helper.js
@@ -5,14 +5,22 @@ export const parseMandates = (mandates, declaredMandates) => {
     return mandates.map(mandate => {
       const [type, zone] = mandate.split('|')
 
-      return { label: mandateTypes[type] ?? type, help: zone, badgeOptions: { variant: 'filled' } }
+      return {
+        label: mandateTypes[type] ?? type,
+        help: zone,
+        badgeOptions: {
+          color: 'colors.green.800',
+          bgcolor: 'colors.green.300',
+          sx: { border: 2 },
+        },
+      }
     })
   }
 
   if (Array.isArray(declaredMandates) && declaredMandates.length) {
     return declaredMandates.map(mandate => ({
       label: mandateTypes[mandate] ?? mandate,
-      badgeOptions: { variant: 'outlined' },
+      badgeOptions: { color: 'colors.green.800', bgcolor: '#fff', sx: { border: 2 } },
     }))
   }
 
--- src/ui/Badge/Badge.jsx
+++ src/ui/Badge/Badge.jsx
@@ -1,17 +1,21 @@
 import { UIChip } from 'ui/Card'
 import PropTypes from 'prop-types'
 
-const Badge = ({ badge }) => (
-  <UIChip
-    label={badge.label}
-    help={badge.help}
-    color="#0369a1"
-    bgcolor="#f0f9ff"
-    labelStyle={{ fontSize: '12px', fontWeight: '600' }}
-    sx={{ display: 'flex', width: 'fit-content', my: 0.5 }}
-    {...(badge.badgeOptions ?? {})}
-  />
-)
+const Badge = ({ badge }) => {
+  const { sx = {}, ...badgeOptions } = badge.badgeOptions ?? {}
+
+  return (
+    <UIChip
+      label={badge.label}
+      help={badge.help}
+      color="#0369a1"
+      bgcolor="#f0f9ff"
+      labelStyle={{ fontSize: '12px', fontWeight: '600' }}
+      sx={{ display: 'flex', width: 'fit-content', py: 1, my: 0.5, ...sx }}
+      {...badgeOptions}
+    />
+  )
+}
 
 export default Badge
 
