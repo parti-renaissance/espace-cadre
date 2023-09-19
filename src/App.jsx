590b0df093f0258739da4220212421a942eb72cd
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2023-09-19 13:14:29 +0200

    [Sentry] configure user context

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,26 @@ import { persistor, store } from './redux/store'
 import { NotifyProvider, QueryProvider, ThemeProvider } from './providers'
 import AppRoutes from './components/AppRoutes'
 import PrivateRoutes from './providers/routes'
+import ErrorBoundary from 'providers/errorboundary'
+
 import './style/index.scss'
 
 const App = () => (
   <StorageProvider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Router>
-        <QueryProvider>
-          <ThemeProvider>
-            <NotifyProvider>
-              <AppRoutes>
-                <PrivateRoutes />
-              </AppRoutes>
-            </NotifyProvider>
-          </ThemeProvider>
-        </QueryProvider>
-      </Router>
+      <ErrorBoundary>
+        <Router>
+          <QueryProvider>
+            <ThemeProvider>
+              <NotifyProvider>
+                <AppRoutes>
+                  <PrivateRoutes />
+                </AppRoutes>
+              </NotifyProvider>
+            </ThemeProvider>
+          </QueryProvider>
+        </Router>
+      </ErrorBoundary>
     </PersistGate>
   </StorageProvider>
 )
--- src/providers/errorboundary.jsx
+++ src/providers/errorboundary.jsx
@@ -2,17 +2,35 @@ import ErrorComponent from 'components/ErrorComponent'
 import * as Sentry from '@sentry/react'
 import PropTypes from 'prop-types'
 import { APP_ENVIRONMENT } from 'shared/environments'
+import { useSelector } from 'react-redux'
+import { getCurrentScope, getCurrentUser } from '../redux/user/selectors'
+import { configureScope } from '@sentry/react'
 
-const ErrorBoundary = ({ children }) => (
-  <Sentry.ErrorBoundary
-    fallback={
-      <ErrorComponent errorMessage={{ message: "Une erreur est survenue, merci de relancer l'application." }} />
-    }
-    showDialog={APP_ENVIRONMENT === 'staging'}
-  >
-    {children}
-  </Sentry.ErrorBoundary>
-)
+const ErrorBoundary = ({ children }) => {
+  const currentScope = useSelector(getCurrentScope)
+  const currentUser = useSelector(getCurrentUser)
+
+  if (currentUser) {
+    configureScope(scope => {
+      scope.setUser({
+        email: currentUser.emailAddress,
+        username: `${currentUser.firstName} ${currentUser.lastName}`,
+        scope: currentScope?.code,
+      })
+    })
+  }
+
+  return (
+    <Sentry.ErrorBoundary
+      fallback={
+        <ErrorComponent errorMessage={{ message: "Une erreur est survenue, merci de relancer l'application." }} />
+      }
+      showDialog={APP_ENVIRONMENT === 'staging'}
+    >
+      {children}
+    </Sentry.ErrorBoundary>
+  )
+}
 
 ErrorBoundary.propTypes = {
   children: PropTypes.node.isRequired,
