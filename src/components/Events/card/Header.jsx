a4e281ad6c58befcfd043b27b87493ff3000d086
Antonin Carlin <antonin.carlin@icloud.com>
2024-01-22 18:10:33 +0100

    fix: EventsCard

--- src/components/Events/card/Header.jsx
+++ src/components/Events/card/Header.jsx
@@ -11,6 +11,8 @@ import noImage from 'assets/no-image.png'
 import EventChip from './EventChip'
 import PropTypes from 'prop-types'
 import { formatDate } from 'shared/helpers'
+import CardMedia from '@mui/material/CardMedia'
+import { UIChip } from 'src/ui/Card'
 
 const CalendarTodayIcon = styled(MuiCalendarTodayIcon)(
   ({ theme }) => `
@@ -24,40 +26,7 @@ const CalendarTodayIcon = styled(MuiCalendarTodayIcon)(
 const HorizontalContainer = styled('div')`
   display: flex;
   flex: 1;
-  align-items: center;
-`
-
-const AttendeesIcon = styled('div')(
-  ({ theme }) => `
-  display: flex;
-  align-items: center;
-  color: ${theme.palette.gray600};
-  font-size: 10px;
-  font-weight: 500;
-  border-radius: 19px;
-  height: 18px;
-  padding: ${theme.spacing(0, 0.75)};
-  border-color: ${theme.palette.gray100};
-  border: ${`1px solid ${theme.palette.gray200}`};
-  margin: ${theme.spacing(0.25, 1, 0, 1)};
-`
-)
-
-const Image = styled('img')`
-  object-fit: cover;
-  margin: ${({ theme }) => theme.spacing(-2.75, -2, 2, -2)};
-  border-top-right-radius: 8px;
-  border-top-left-radius: 8px;
-  height: 130px;
-`
-
-const NoImageContainer = styled('div')`
-  margin: ${({ theme }) => theme.spacing(-2.75, -2, 2, -2)};
-  border-top-right-radius: 8px;
-  border-top-left-radius: 8px;
-  height: 130px;
-  display: flex;
-  justify-content: center;
+  gap: ${({ theme }) => theme.spacing(1)};
   align-items: center;
 `
 
@@ -67,23 +36,33 @@ const LabelTypography = styled(Typography)`
   align-self: center;
 `
 
+const CardMediaPlaceholderContainer = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
 export const formatAddress = ({ route, postalCode, locality }) =>
   [route, route && ', ', postalCode, postalCode && ' ', locality].filter(Boolean).join('')
 const Header = ({ event, categoryNameByCategoryId }) => (
   <>
-    {event.image ? (
-      <Image src={event.image} />
-    ) : (
-      <NoImageContainer>
-        <img src={noImage} height="100px" width="100px" alt="no image" />
-      </NoImageContainer>
-    )}
+    <CardMediaPlaceholderContainer>
+      {event.image ? (
+        <CardMedia sx={{ borderRadius: '4px' }} component="img" height={130} image={event.image} />
+      ) : (
+        <img height={130} src={noImage} />
+      )}
+    </CardMediaPlaceholderContainer>
+    <br />
     <HorizontalContainer>
       <EventChip event={event} />
-      <AttendeesIcon>
-        <GroupIcon sx={{ mr: 1, fontSize: '16px', fontWeight: '500', color: 'gray500' }} />
-        {event.attendees}
-      </AttendeesIcon>
+      <UIChip
+        sx={{ pl: 1 }}
+        icon={<GroupIcon sx={{ fontWeight: '500', color: 'gray500' }} />}
+        variant="outlined"
+        label={event.attendees}
+        size="small"
+      />
     </HorizontalContainer>
     <Title subject={event.name} author={`Par ${event.organizer}`} lines={2} sx={{ pt: 1 }} />
     <Box component="div" sx={{ display: 'flex', mt: 1 }}>
@@ -93,12 +72,7 @@ const Header = ({ event, categoryNameByCategoryId }) => (
     {event.address && !event.address.isEmpty() && (
       <Box component="div" sx={{ display: 'flex', mt: 1 }}>
         <RoomIcon sx={{ mr: 1, fontSize: '16px', fontWeight: '500', color: 'gray500' }} />
-        <TruncatedText
-          lines={2}
-          variant="subtitle2"
-          sx={{ height: '35px', color: 'gray600' }}
-          title={formatAddress(event.address)}
-        >
+        <TruncatedText lines={2} variant="subtitle2" sx={{ color: 'gray600' }} title={formatAddress(event.address)}>
           {formatAddress(event.address)}
         </TruncatedText>
       </Box>
