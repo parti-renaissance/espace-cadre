acdfdc8a033a2ab6f3454f0b9225d5dec817409d
Antonin Carlin <antonin.carlin@icloud.com>
2024-02-15 12:07:27 +0100

    feat: recommended way of creating router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import { Provider as StorageProvider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor, store } from './redux/store'
@@ -10,32 +10,30 @@ import ErrorBoundary from '~/providers/errorboundary'
 
 import './style/index.scss'
 
+const Router = AppRoutes({ children: <PrivateRoutes /> })
+
 const App = () => (
   <StorageProvider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <ErrorBoundary>
-        <Router>
-          <QueryProvider>
-            <SettingsProvider
-              defaultSettings={{
-                themeMode: 'light', // 'light' | 'dark'
-                themeDirection: 'ltr', //  'rtl' | 'ltr'
-                themeContrast: 'default', // 'default' | 'bold'
-                themeLayout: 'vertical', // 'vertical' | 'horizontal' | 'mini'
-                themeColorPresets: 'default', // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
-                themeStretch: false,
-              }}
-            >
-              <ThemeProvider>
-                <NotifyProvider>
-                  <AppRoutes>
-                    <PrivateRoutes />
-                  </AppRoutes>
-                </NotifyProvider>
-              </ThemeProvider>
-            </SettingsProvider>
-          </QueryProvider>
-        </Router>
+        <QueryProvider>
+          <SettingsProvider
+            defaultSettings={{
+              themeMode: 'light', // 'light' | 'dark'
+              themeDirection: 'ltr', //  'rtl' | 'ltr'
+              themeContrast: 'default', // 'default' | 'bold'
+              themeLayout: 'vertical', // 'vertical' | 'horizontal' | 'mini'
+              themeColorPresets: 'default', // 'default' | 'cyan' | 'purple' | 'blue' | 'orange' | 'red'
+              themeStretch: false,
+            }}
+          >
+            <ThemeProvider>
+              <NotifyProvider>
+                <RouterProvider router={Router} />
+              </NotifyProvider>
+            </ThemeProvider>
+          </SettingsProvider>
+        </QueryProvider>
       </ErrorBoundary>
     </PersistGate>
   </StorageProvider>
