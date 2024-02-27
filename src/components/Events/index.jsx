c070c3c6ff377c9979d96c67662ddd800a798204
Brandon Sueur <gynidark@gmail.com>
2024-02-27 23:06:08 +0100

    feat(event): new page detail event with static informations

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
-import { ListEvents, CreateEvent } from '~/components/Events/pages'
+import { ListEvents, CreateEvent, DetailEvent } from '~/components/Events/pages'
 import { paths } from '~/components/Events/shared/paths'
 
 const EventRoute = () => (
   <Routes>
     <Route path="*" element={<ListEvents />} />
-    {/*<Route path=":eventId" element={<DetailEvent />} />*/}
+    <Route path="/:eventId" element={<DetailEvent />} />
 
     {/* Create an event */}
     <Route path={paths.create} element={<CreateEvent />} />
