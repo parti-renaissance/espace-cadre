aabb6889c44cec39244d03553328175352ba347b
Brandon Sueur <gynidark@gmail.com>
2024-02-28 13:46:15 +0100

    fix(event): move the returned value immediately after the `=>`

--- src/components/Events/pages/detail/components/ShareLink/ShareLink.tsx
+++ src/components/Events/pages/detail/components/ShareLink/ShareLink.tsx
@@ -7,47 +7,45 @@ interface ShareLinkProps {
   link: string
 }
 
-const ShareLink = ({ link }: ShareLinkProps) => {
-  return (
-    <Card sx={{ p: '24px' }}>
-      <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ mb: '16px' }}>
-        <Button variant="outlined" size="large" fullWidth={true}>
-          Copier le lien
-        </Button>
+const ShareLink = ({ link }: ShareLinkProps) => (
+  <Card sx={{ p: '24px' }}>
+    <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ mb: '16px' }}>
+      <Button variant="outlined" size="large" fullWidth={true}>
+        Copier le lien
+      </Button>
 
-        <Button startIcon={<ShareOutlined />} variant="contained" fullWidth={true} size="large">
-          Partager
-        </Button>
-      </Stack>
+      <Button startIcon={<ShareOutlined />} variant="contained" fullWidth={true} size="large">
+        Partager
+      </Button>
+    </Stack>
 
-      <Box
+    <Box
+      sx={{
+        display: 'flex',
+        border: 1,
+        borderRadius: '8px',
+        borderColor: 'grey.300',
+        p: '14px',
+      }}
+    >
+      <Link
+        color="primary"
+        variant="body2"
+        underline="hover"
+        component={RouterLink}
+        to={link}
         sx={{
-          display: 'flex',
-          border: 1,
-          borderRadius: '8px',
-          borderColor: 'grey.300',
-          p: '14px',
+          textOverflow: 'ellipsis',
+          overflow: 'hidden',
         }}
+        referrerPolicy="no-referrer"
+        target="_blank"
+        rel="noopener noreferrer"
       >
-        <Link
-          color="primary"
-          variant="body2"
-          underline="hover"
-          component={RouterLink}
-          to={link}
-          sx={{
-            textOverflow: 'ellipsis',
-            overflow: 'hidden',
-          }}
-          referrerPolicy="no-referrer"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {link}
-        </Link>
-      </Box>
-    </Card>
-  )
-}
+        {link}
+      </Link>
+    </Box>
+  </Card>
+)
 
 export default ShareLink
