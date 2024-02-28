51b68a9a5cf81b4ed3746e367bafb00eb0df750a
Brandon Sueur <gynidark@gmail.com>
2024-02-28 20:05:54 +0100

    feat(event): sharelink

--- src/components/Events/pages/detail/components/ShareLink/ShareLink.tsx
+++ src/components/Events/pages/detail/components/ShareLink/ShareLink.tsx
@@ -7,53 +7,66 @@ interface ShareLinkProps {
   link: string
 }
 
-const ShareLink = ({ link }: ShareLinkProps) => (
-  <Card sx={{ p: '24px' }}>
-    <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ mb: '16px' }}>
-      <Button variant="outlined" size="large" fullWidth={true} onClick={() => navigator.clipboard.writeText(link)}>
-        Copier le lien
-      </Button>
+const openNewTab = (url: string) => {
+  const win = window.open(url, '_blank')
+  win?.focus()
+}
 
-      <Button
-        startIcon={<ShareOutlined />}
-        variant="contained"
-        fullWidth={true}
-        size="large"
-        onClick={() => {
-          throw new Error('Not implemented')
-        }}
-      >
-        Partager
-      </Button>
-    </Stack>
+const ShareLink = ({ link }: ShareLinkProps) => {
+  const handleShare = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: 'Partager le lien',
+          url: link!,
+        })
+      } catch (error) {
+        openNewTab(link!)
+      }
+    }
+    openNewTab(link!)
+  }
 
-    <Box
-      sx={{
-        display: 'flex',
-        border: 1,
-        borderRadius: '8px',
-        borderColor: 'grey.300',
-        p: '14px',
-      }}
-    >
-      <Link
-        color="primary"
-        variant="body2"
-        underline="hover"
-        component={RouterLink}
-        to={link}
+  return (
+    <Card sx={{ p: '24px' }}>
+      <Stack direction="row" spacing={2} justifyContent="space-between" sx={{ mb: '16px' }}>
+        <Button variant="outlined" size="large" fullWidth={true} onClick={() => navigator.clipboard.writeText(link)}>
+          Copier le lien
+        </Button>
+
+        <Button startIcon={<ShareOutlined />} variant="contained" fullWidth={true} size="large" onClick={handleShare}>
+          Partager
+        </Button>
+      </Stack>
+
+      <Box
         sx={{
-          textOverflow: 'ellipsis',
-          overflow: 'hidden',
+          display: 'flex',
+          border: 1,
+          borderRadius: '8px',
+          borderColor: 'grey.300',
+          p: '14px',
         }}
-        referrerPolicy="no-referrer"
-        target="_blank"
-        rel="noopener noreferrer"
       >
-        {link}
-      </Link>
-    </Box>
-  </Card>
-)
+        <Link
+          color="primary"
+          variant="body2"
+          underline="hover"
+          component={RouterLink}
+          to={link}
+          sx={{
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+          }}
+          referrerPolicy="no-referrer"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {link}
+        </Link>
+      </Box>
+    </Card>
+  )
+}
 
 export default ShareLink
