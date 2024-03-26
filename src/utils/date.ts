d9b4a9097a50e75a9143b1efca86901506db489b
Nicolas Guillard <nicolas@nitrique.fr>
2024-03-26 10:46:07 +0100

    fix: #RE-1511 populate data on mandant (#2930)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,6 @@
-import { differenceInCalendarYears, format } from 'date-fns'
-import { fr } from 'date-fns/locale'
+import { differenceInCalendarYears } from 'date-fns'
 import pluralize from '~/components/shared/pluralize/pluralize'
+import { formatDate } from '~/shared/helpers'
 
 export const dateFormat = 'P'
 export const timeFormat = 'p'
@@ -11,19 +11,17 @@ export const dateValueFormat = 'd' // 1, 2, 3
 
 export const agendaDateFormat = 'EE d'
 
-const localeOptions = { locale: fr }
+export const getFormattedDate = (date: Date | string) => formatDate(date, dateFormat)
 
-export const getFormattedDate = (date: Date) => format(date, dateFormat, localeOptions)
+export const getHumanFormattedDate = (date: Date | string) => formatDate(date, humanReadableDate)
+export const getHumanFormattedTime = (date: Date | string) => formatDate(date, timeFormat)
 
-export const getHumanFormattedDate = (date: Date) => format(date, humanReadableDate, localeOptions)
-export const getHumanFormattedTime = (date: Date) => format(date, timeFormat, localeOptions)
+export const getAgendaDate = (date: Date | string) => formatDate(date, agendaDateFormat)
 
-export const getAgendaDate = (date: Date) => format(date, agendaDateFormat, localeOptions)
+export const getNowFormattedDate = () => formatDate(new Date(), humanReadableDate)
 
-export const getNowFormattedDate = () => format(new Date(), humanReadableDate, localeOptions)
-
-export const getShortDayName = (date: Date) => format(date, dateShortFormat, localeOptions)
-export const getDayNumber = (date: Date) => format(date, dateValueFormat, localeOptions)
+export const getShortDayName = (date: Date) => formatDate(date, dateShortFormat)
+export const getDayNumber = (date: Date) => formatDate(date, dateValueFormat)
 
 export const getAge = (date: Date) => {
   const difference = differenceInCalendarYears(new Date(), date)
