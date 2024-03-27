d873ce134fd23a14769a5a183812d48a71cfe09a
Nicolas Guillard <nicolas@nitrique.fr>
2024-03-27 18:04:14 +0100

    #RE-1514 Modal when procuration done (#2935)
    * feature: #RE-1514 Modal when procuration done
    
    * doc: #RE-1514 modal base documentation

---
+++ src/components/ModalBase/ModalBase.tsx
@@ -0,0 +1,41 @@
+import { ModalProps } from '@mui/base'
+import { Box } from '@mui/system'
+import { Modal } from '@mui/material'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { useTheme } from '@mui/material/styles'
+
+interface ModalBaseProps extends Omit<ModalProps, 'open'> {
+  // True if you want to display a full page instead on modal on XS viewport.
+  pageOnMobile?: boolean
+}
+
+export default function ModalBase({ children, pageOnMobile = false, ...props }: ModalBaseProps) {
+  const theme = useTheme()
+  const isXs = useMediaQuery(theme.breakpoints.only('xs'))
+
+  return (
+    <Modal open={true} data-testid="modal" {...props}>
+      <Box sx={isXs && pageOnMobile ? xsStyle : style}>{children}</Box>
+    </Modal>
+  )
+}
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '70%',
+  minWidth: 400,
+  maxWidth: 1000,
+  bgcolor: 'background.paper',
+  p: 4,
+  borderRadius: 2,
+}
+
+const xsStyle = {
+  bgcolor: 'background.paper',
+  height: '100vh',
+  width: '100vw',
+  p: 4,
+}
