cce701318bacf65d5b147ef9cab222437eeee7ab
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-02 13:05:28 +0200

    feature(procurations): #RE-1615 Update texts on mandate (#2944)

--- src/components/Mandates/Components/MandantTab/Components/MandateDoneIntroduction.tsx
+++ src/components/Mandates/Components/MandantTab/Components/MandateDoneIntroduction.tsx
@@ -9,6 +9,23 @@ function MandateDoneIntroduction() {
         <Typography fontWeight={fontWeight.medium} color={'text.secondary'}>
           Mandants traités
         </Typography>
+        <br />
+        <Typography color={'text.secondary'} fontSize={14}>
+          Retrouvez dans cet onglet l’ensemble des mandants déjà traités.
+        </Typography>
+      </p>
+
+      <p>
+        <Typography color={'text.secondary'} fontSize={14}>
+          En usant des filtres, vous pouvez dissocier les mandants liés à un mandataire de ceux que vous avez exclu ou
+          traité manuellement.
+        </Typography>
+      </p>
+
+      <p>
+        <Typography color={'text.secondary'} fontSize={14}>
+          Vous pouvez également voir les associations précédemment faites et les annuler si nécessaire.
+        </Typography>
       </p>
     </div>
   )
