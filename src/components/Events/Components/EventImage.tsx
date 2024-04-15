5cd91497c308006ad90188b524e69b2daf9cb930
Antonin <antonin.carlin@icloud.com>
2024-04-15 18:23:05 +0200

    fix(EventImage): types

--- src/components/Events/Components/EventImage.tsx
+++ src/components/Events/Components/EventImage.tsx
@@ -1,8 +1,11 @@
-import { CardMedia } from '@mui/material'
+import { CardMedia, CardMediaProps } from '@mui/material'
 import { ReactComponentElement } from 'react'
 import placeholder from '~/assets/image/placeholder.png'
 
-export default function EventImage({ image, ...props }: { image: string | ReactComponentElement<any> }) {
+export default function EventImage({
+  image,
+  ...props
+}: CardMediaProps & { image: string | ReactComponentElement<any> }) {
   if (typeof image === 'string') {
     return <CardMedia component="img" image={image} {...props} />
   }
