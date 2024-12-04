e9d6213c9a6d64d1e210461d16f9fc768aaf9246
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-12-04 09:51:18 +0100

    [Election] add ballots and voters download buttons

--- src/api/designations.ts
+++ src/api/designations.ts
@@ -1,5 +1,6 @@
 import { apiClient } from '~/services/networking/client'
 import { Designation, DesignationTypeEnum } from '~/domain/designation'
+import { downloadFile } from '~/api/upload'
 
 export const getDesignations = async (types: DesignationTypeEnum[]): Promise<Designation[]> => {
   const data = await apiClient.get(`/v3/designations?${types.map(type => `type[]=${type}`).join('&')}`)
@@ -17,3 +18,5 @@ export const updateDesignation = (designation: Designation) =>
 export const cancelDesignation = async (uuid: string) => await apiClient.put(`/v3/designations/${uuid}/cancel`)
 export const resultsDesignation = async (uuid: string) => await apiClient.get(`/v3/designations/${uuid}/results`)
 export const getVoters = async (uuid: string) => await apiClient.get(`/v3/designations/${uuid}/voters`)
+export const downloadVoters = uuid => downloadFile(`/v3/designations/${uuid}/voters.xlsx`)
+export const downloadBallots = uuid => downloadFile(`/v3/designations/${uuid}/ballots.xlsx`)
